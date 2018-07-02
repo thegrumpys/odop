4d764398dc4c3926c5fcdf31f8d2a5dc91fd1ef9
Brian Watt <bwatt@1fifoto.com>
2018-07-01 22:53:37 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 8080, function() {
+    console.log('Server started');
+});
