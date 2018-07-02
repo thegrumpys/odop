92c9f117235fe6ac9958e2823e8f881db43294c1
Brian Watt <bwatt@1fifoto.com>
2018-07-02 00:53:07 -0500

    Add React via create-react-app

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+var {createServer} = require('http'); 
 var express = require('express');
+var compression = require('compression');
+var morgan = require('morgan');
+var path = require('path');
+
 var app = express();
+var dev = app.get('env') !== 'production';
 
-app.use(express.static('public'));
+if (!dev) {
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
 
-app.listen(process.env.PORT || 8080, function() {
-    console.log('Server started');
+    app.use(express.static(path.resolve(__dirname, 'build')));
+    
+    app.get('*', function(req,res){
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+} else {
+    app.use(morgan('dev'));
+}
+
+var server = createServer(app);
+
+server.listen(process.env.PORT || 5000, function(err) {
+    if (err) throw err;
+    console.log('Server started!');
 });
