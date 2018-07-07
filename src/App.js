8d88e1f5249b32801aaabd50c697fb27c09d6b59
Brian Watt <bwatt@1fifoto.com>
2018-07-07 17:33:32 -0500

    Add in reactstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    Jumbotron,
+    Button
+} from 'reactstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="inverse" light expand="md">
+                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+                <Jumbotron>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1>Welcome to React</h1>
+                                <p>
+                                    <Button
+                                        tag="a"
+                                        color="success"
+                                        size="large"
+                                        href="http://reactstrap.github.io"
+                                        target="_blank"
+                                    >
+                                        View Reactstrap Docs
+                                    </Button>
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
