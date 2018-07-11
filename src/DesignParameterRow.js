97081fa88d2e4838f6333bd211e41f878d7a4937
Brian Watt <bwatt@1fifoto.com>
2018-07-11 13:30:27 -0500

    1 Screen capture from SS_II. Possible web page layout.

--- src/DesignParameterRow.js
+++ src/DesignParameterRow.js
@@ -6,36 +6,68 @@ export class DesignParameterRow extends React.Component {
     render() {
         var vmin = this.props.design_parameter.vmin > 0.0 ? 'bg-danger align-middle' : 'align-middle';
         var vmax = this.props.design_parameter.vmax > 0.0 ? 'bg-danger align-middle' : 'align-middle';
-        var fixed = this.props.design_parameter.lmin === global.FIXEDSTAT ? <Input type="checkbox" aria-label="Checkbox for fixed value" checked/> : <Input type="checkbox" aria-label="Checkbox for fixed value"/>;
-        return (
-                <tr key={this.props.design_parameter.name}>
-                <td className="align-middle">{this.props.design_parameter.name}</td>
-                <td className="pull-right align-middle"><Input type="number" defaultValue={this.props.design_parameter.value} /></td>
-                <td className="text-nowrap align-middle">{this.props.design_parameter.units}</td>
-
-                <td className="text-center align-middle">{fixed}</td>
-
-                <td className={vmin}>
-                <InputGroup>
-                    <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                            <Input addon type="checkbox" aria-label="Checkbox for minimum value" />
-                        </InputGroupText>
-                    </InputGroupAddon>
-                    <Input className="pull-right" type="number" defaultValue={this.props.design_parameter.cmin} />
-                </InputGroup>
-                </td>
-                
-                <td className={vmax}>
-                <InputGroup>
+        var fixed;
+        if (this.props.design_parameter.lmin === global.FIXEDSTAT) {
+            fixed = (<Input type="checkbox" aria-label="Checkbox for fixed value" checked />);
+        } else {
+            fixed = (<Input type="checkbox" aria-label="Checkbox for fixed value" />);
+        }
+        var cmin;
+        if (this.props.design_parameter.lmin === global.SETSTAT) {
+            cmin = (
+              <InputGroup>
                 <InputGroupAddon addonType="prepend">
                   <InputGroupText>
-                    <Input addon type="checkbox" aria-label="Checkbox for maximum value" />
+                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" checked />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input className="pull-right" type="number" defaultValue={this.props.design_parameter.cmin} />
+              </InputGroup>
+            );
+        } else {
+            cmin = (
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input className="pull-right" type="number" defaultValue={this.props.design_parameter.cmin} disabled />
+              </InputGroup>
+            );
+        }
+        var cmax;
+        if (this.props.design_parameter.lmax === global.SETSTAT) {
+            cmax = (
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" checked />
                   </InputGroupText>
                 </InputGroupAddon>
                 <Input className="pull-right" type="number" defaultValue={this.props.design_parameter.cmax} />
-                </InputGroup>
-                </td>
+              </InputGroup>
+            );
+        } else {
+            cmax = (
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input className="pull-right" type="number" defaultValue={this.props.design_parameter.cmax} disabled />
+              </InputGroup>
+            );
+        }
+        return (
+                <tr key={this.props.design_parameter.name}>
+                  <td className="align-middle">{this.props.design_parameter.name}</td>
+                  <td className="pull-right align-middle"><Input type="number" defaultValue={this.props.design_parameter.value} /></td>
+                  <td className="text-nowrap align-middle">{this.props.design_parameter.units}</td>
+                  <td className="text-center align-middle">{fixed}</td>
+                  <td className={vmin}>{cmin}</td>
+                  <td className={vmax}>{cmax}</td>
                 </tr>
         );
     }
--- src/StateVariableRow.js
+++ src/StateVariableRow.js
@@ -6,36 +6,68 @@ export class StateVariableRow extends React.Component {
     render() {
         var vmin = this.props.state_variable.vmin > 0.0 ? 'bg-danger align-middle' : 'align-middle';
         var vmax = this.props.state_variable.vmax > 0.0 ? 'bg-danger align-middle' : 'align-middle';
-        var fixed = this.props.state_variable.lmin === global.FIXEDSTAT ? <Input type="checkbox" aria-label="Checkbox for fixed value" checked/> : <Input type="checkbox" aria-label="Checkbox for fixed value"/>;
-       return (
-                <tr key={this.props.state_variable.name}>
-                <td className="align-middle">{this.props.state_variable.name}</td>
-                <td className="pull-right align-middle"><Input type="number" defaultValue={this.props.state_variable.value} /></td>
-                <td className="text-nowrap align-middle">{this.props.state_variable.units}</td>
-
-                <td className="text-center align-middle">{fixed}</td>
-
-                <td className={vmin}>
-                <InputGroup>
+        var fixed;
+        if (this.props.state_variable.lmin === global.FIXEDSTAT) {
+            fixed = (<Input type="checkbox" aria-label="Checkbox for fixed value" checked />);
+        } else {
+            fixed = (<Input type="checkbox" aria-label="Checkbox for fixed value" />);
+        }
+        var cmin;
+        if (this.props.state_variable.lmin === global.SETSTAT) {
+            cmin = (
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" checked />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input className="pull-right" type="number" defaultValue={this.props.state_variable.cmin} />
+              </InputGroup>
+            );
+        } else {
+            cmin = (
+              <InputGroup>
                 <InputGroupAddon addonType="prepend">
                   <InputGroupText>
                     <Input addon type="checkbox" aria-label="Checkbox for minimum value" />
                   </InputGroupText>
                 </InputGroupAddon>
-                <Input className="pull-right" type="number" defaultValue={this.props.state_variable.cmin} />
-                </InputGroup>
-                </td>
-                
-                <td className={vmax}>
-                <InputGroup>
+                <Input className="pull-right" type="number" defaultValue={this.props.state_variable.cmin} disabled />
+              </InputGroup>
+            );
+        }
+        var cmax;
+        if (this.props.state_variable.lmax === global.SETSTAT) {
+            cmax = (
+              <InputGroup>
                 <InputGroupAddon addonType="prepend">
                   <InputGroupText>
-                    <Input addon type="checkbox" aria-label="Checkbox for maximum value" />
+                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" checked />
                   </InputGroupText>
                 </InputGroupAddon>
                 <Input className="pull-right" type="number" defaultValue={this.props.state_variable.cmax} />
-                </InputGroup>
-                </td>
+              </InputGroup>
+            );
+        } else {
+            cmax = (
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input className="pull-right" type="number" defaultValue={this.props.state_variable.cmax} disabled />
+              </InputGroup>
+            );
+        }
+        return (
+                <tr key={this.props.state_variable.name}>
+                  <td className="align-middle">{this.props.state_variable.name}</td>
+                  <td className="pull-right align-middle"><Input type="number" defaultValue={this.props.state_variable.value} disabled /></td>
+                  <td className="text-nowrap align-middle">{this.props.state_variable.units}</td>
+                  <td className="text-center align-middle">{fixed}</td>
+                  <td className={vmin}>{cmin}</td>
+                  <td className={vmax}>{cmax}</td>
                 </tr>
         );
     }
