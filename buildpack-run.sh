44b15a962f0ac0d5e203d17338b206930145a8b1
Brian Watt <bwatt@1fifoto.com>
2018-07-13 19:34:24 -0500

    Add Heroku buildpack-run.sh to create version.js file

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This is buildpack-run.sh
-echo "Entering buildpack-run.sh"
-./scripts/createVersion.js > src/version.js
-echo "Exiting buildpack-run.sh code=" $?
+echo "In buildpack-run.sh"
+node ./scripts/createVersion.js > src/version.js
+echo $?
