2406494ceb2a6dc990134e2e54626db3642051be
Brian Watt <bwatt@1fifoto.com>
2018-07-16 20:19:53 -0500

    24 Add store test cases

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -4,7 +4,7 @@ import App from '../App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 import { pcylWebApp } from '../reducers';
-import { startup } from '../actionCreators';
+import { startup, changeDesignParameterValue } from '../actionCreators';
 import { initialState } from '../initialState';
 
 it('renders without crashing', () => {
@@ -15,6 +15,20 @@ it('renders without crashing', () => {
     store.dispatch(startup());
     const div = document.createElement('div');
     div.setAttribute("id", "root");
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders with different design parameter and state variable constraint violations', () => {
+    const store = createStore(
+            pcylWebApp,
+            initialState
+            );
+    store.dispatch(startup());
+    const div = document.createElement('div');
+    div.setAttribute("id", "root");
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    store.dispatch(changeDesignParameterValue("PRESSURE", 5000));
     ReactDOM.render(<Provider store={store}><App /></Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
