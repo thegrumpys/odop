8fb05b53f4cc00b95eefc681467b8321ce97a121
Brian Watt <bwatt@1fifoto.com>
2018-07-17 21:44:01 -0500

    24 Add store test cases

--- src/__test__/equationsMiddleware.test.js
+++ src/__test__/equationsMiddleware.test.js
@@ -12,18 +12,103 @@ import { MIN, MAX } from '../actionTypes';
 import { CONSTRAINED, FIXED } from '../globals';
 
 //=====================================================================
-// DESIGN PARAMETERS
+// STARTUP
 //=====================================================================
 
-it('change pressure design parameter value', () => {
+it('middleware with startup', () => {
     const store = createStore(
         pcylWebApp,
         initialState,
         applyMiddleware(equationsMiddleware));
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[0].name).toEqual("PRESSURE");
-    expect(design.design_parameters[0].value).toEqual(500); // before
+    expect(design.design_parameters[0].cmin).toEqual(0);
+    expect(design.design_parameters[0].cmax).toEqual(1500);
+    expect(design.design_parameters[0].smin).toEqual(0.06666666666666667);
+    expect(design.design_parameters[0].smax).toEqual(1500);
+    expect(design.design_parameters[1].name).toEqual("RADIUS");
+    expect(design.design_parameters[1].cmin).toEqual(0);
+    expect(design.design_parameters[1].cmax).toEqual(0.5);
+    expect(design.design_parameters[1].smin).toEqual(0.4);
+    expect(design.design_parameters[1].smax).toEqual(0.5);
+    expect(design.design_parameters[2].name).toEqual("THICKNESS");
+    expect(design.design_parameters[2].cmin).toEqual(0.0);
+    expect(design.design_parameters[2].cmax).toEqual(0.05);
+    expect(design.design_parameters[2].smin).toEqual(0.04);
+    expect(design.design_parameters[2].smax).toEqual(0.05);
+    expect(design.state_variables[0].name).toEqual("FORCE");
+    expect(design.state_variables[0].cmin).toEqual(1000);
+    expect(design.state_variables[0].cmax).toEqual(0);
+    expect(design.state_variables[0].smin).toEqual(1000);
+    expect(design.state_variables[0].smax).toEqual(0.06666666666666667);
+    expect(design.state_variables[1].name).toEqual("AREA");
+    expect(design.state_variables[1].cmin).toEqual(0);
+    expect(design.state_variables[1].cmax).toEqual(0);
+    expect(design.state_variables[1].smin).toEqual(0.06666666666666667);
+    expect(design.state_variables[1].smax).toEqual(0.06666666666666667);
+    expect(design.state_variables[2].name).toEqual("STRESS");
+    expect(design.state_variables[2].cmin).toEqual(0);
+    expect(design.state_variables[2].cmax).toEqual(3000);
+    expect(design.state_variables[2].smin).toEqual(0.06666666666666667);
+    expect(design.state_variables[2].smax).toEqual(3000);
+    
+    store.dispatch(startup());
+    
+    
+    var design = store.getState(); // after
+//  value=500, level=1500, sdlimit=0, status=1, stemp=1500
+    expect(design.design_parameters[0].name).toEqual("PRESSURE");
+    expect(design.design_parameters[0].cmin).toEqual(0);
+    expect(design.design_parameters[0].cmax).toEqual(1500);
+    expect(design.design_parameters[0].smin).toEqual(500); // updated
+    expect(design.design_parameters[0].smax).toEqual(1500);
+//  value=0.4, level=0, sdlimit=0, status=1, stemp=0.4
+//  value=0.4, level=0.5, sdlimit=0, status=1, stemp=0.5
+    expect(design.design_parameters[1].name).toEqual("RADIUS");
+    expect(design.design_parameters[1].cmin).toEqual(0);
+    expect(design.design_parameters[1].cmax).toEqual(0.5);
+    expect(design.design_parameters[1].smin).toEqual(0.4);
+    expect(design.design_parameters[1].smax).toEqual(0.5);
+//  value=0.04, level=0, sdlimit=0, status=1, stemp=0.04
+//  value=0.04, level=0.05, sdlimit=0, status=1, stemp=0.05
+    expect(design.design_parameters[2].name).toEqual("THICKNESS");
+    expect(design.design_parameters[2].cmin).toEqual(0.0);
+    expect(design.design_parameters[2].cmax).toEqual(0.05);
+    expect(design.design_parameters[2].smin).toEqual(0.04);
+    expect(design.design_parameters[2].smax).toEqual(0.05);
+//  value=251.32741228718348, level=1000, sdlimit=0, status=1, stemp=1000
+    expect(design.state_variables[0].name).toEqual("FORCE");
+    expect(design.state_variables[0].cmin).toEqual(1000);
+    expect(design.state_variables[0].cmax).toEqual(0);
+    expect(design.state_variables[0].smin).toEqual(1000);
+    expect(design.state_variables[0].smax).toEqual(251.32741228718348); // updated
+    expect(design.state_variables[1].name).toEqual("AREA");
+    expect(design.state_variables[1].cmin).toEqual(0);
+    expect(design.state_variables[1].cmax).toEqual(0);
+    expect(design.state_variables[1].smin).toEqual(0.5026548245743669); // updated
+    expect(design.state_variables[1].smax).toEqual(0.5026548245743669); // updated
+//  value=2500, level=3000, sdlimit=0, status=1, stemp=3000
+    expect(design.state_variables[2].name).toEqual("STRESS");
+    expect(design.state_variables[2].cmin).toEqual(0);
+    expect(design.state_variables[2].cmax).toEqual(3000);
+    expect(design.state_variables[2].smin).toEqual(2500); // updated
+    expect(design.state_variables[2].smax).toEqual(3000);
+});
+
+//=====================================================================
+// DESIGN PARAMETERS
+//=====================================================================
+
+it('middleware change pressure design parameter value', () => {
+    const store = createStore(
+        pcylWebApp,
+        initialState,
+        applyMiddleware(equationsMiddleware));
+    
+    var design = store.getState(); // before
+    expect(design.design_parameters[0].name).toEqual("PRESSURE");
+    expect(design.design_parameters[0].value).toEqual(500);
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].value).toEqual(251.32741228718348);
     expect(design.state_variables[1].name).toEqual("AREA");
@@ -33,9 +118,9 @@ it('change pressure design parameter value', () => {
 
     store.dispatch(changeDesignParameterValue("PRESSURE", 5000));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[0].name).toEqual("PRESSURE");
-    expect(design.design_parameters[0].value).toEqual(5000); // after
+    expect(design.design_parameters[0].value).toEqual(5000);
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].value).toEqual(2513.2741228718348);
     expect(design.state_variables[1].name).toEqual("AREA");
@@ -44,15 +129,15 @@ it('change pressure design parameter value', () => {
     expect(design.state_variables[2].value).toEqual(25000);
 });
 
-it('change radius design parameter value', () => {
+it('middleware change radius design parameter value', () => {
     const store = createStore(
         pcylWebApp,
         initialState,
         applyMiddleware(equationsMiddleware));
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].value).toEqual(0.4); // before
+    expect(design.design_parameters[1].value).toEqual(0.4);
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].value).toEqual(251.32741228718348);
     expect(design.state_variables[1].name).toEqual("AREA");
@@ -62,9 +147,9 @@ it('change radius design parameter value', () => {
 
     store.dispatch(changeDesignParameterValue("RADIUS", 0.5));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].value).toEqual(0.5); // after
+    expect(design.design_parameters[1].value).toEqual(0.5);
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].value).toEqual(251.32741228718348);
     expect(design.state_variables[1].name).toEqual("AREA");
@@ -73,15 +158,15 @@ it('change radius design parameter value', () => {
     expect(design.state_variables[2].value).toEqual(3125);
 });
 
-it('change thickness design parameter value', () => {
+it('middleware change thickness design parameter value', () => {
     const store = createStore(
         pcylWebApp,
         initialState,
         applyMiddleware(equationsMiddleware));
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].value).toEqual(0.04); // before
+    expect(design.design_parameters[2].value).toEqual(0.04);
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].value).toEqual(251.32741228718348);
     expect(design.state_variables[1].name).toEqual("AREA");
@@ -91,9 +176,9 @@ it('change thickness design parameter value', () => {
 
     store.dispatch(changeDesignParameterValue("THICKNESS", 0.05));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].value).toEqual(0.05); // after
+    expect(design.design_parameters[2].value).toEqual(0.05);
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].value).toEqual(251.32741228718348);
     expect(design.state_variables[1].name).toEqual("AREA");
--- src/__test__/reducers.test.js
+++ src/__test__/reducers.test.js
@@ -15,13 +15,12 @@ import { CONSTRAINED, FIXED } from '../globals';
 // STARTUP
 //=====================================================================
 
-it('without startup', () => {
+it('reducers without startup', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    // Nothing changed, subscribe not possible, so just use the store directly
-    var design = store.getState();
+    var design = store.getState(); // after
     expect(design.name).toEqual("Piston-Cylinder");
     expect(design.version).toEqual("1.2");
     expect(design.design_parameters[1].name).toEqual("RADIUS");
@@ -30,13 +29,12 @@ it('without startup', () => {
     expect(design.state_variables[1].value).toEqual(0.5026548245743669);
 });
 
-it('with startup', () => {
+it('reducers with startup', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    // Nothing changed, subscribe not possible, so just use the store directly
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.name).toEqual("Piston-Cylinder");
     expect(design.version).toEqual("1.2");
     expect(design.design_parameters[1].name).toEqual("RADIUS");
@@ -46,7 +44,7 @@ it('with startup', () => {
     
     store.dispatch(startup());
     
-    var design = store.getState();
+    var design = store.getState(); // after
     expect(design.name).toEqual("Piston-Cylinder");
     expect(design.version).toEqual("1.2");
     expect(design.design_parameters[1].name).toEqual("RADIUS");
@@ -59,260 +57,260 @@ it('with startup', () => {
 // DESIGN PARAMETERS
 //=====================================================================
 
-it('change design parameter value', () => {
+it('reducers change design parameter value', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].value).toEqual(0.4); // before
+    expect(design.design_parameters[1].value).toEqual(0.4);
     expect(design.state_variables[1].name).toEqual("AREA");
     expect(design.state_variables[1].value).toEqual(0.5026548245743669);
 
     store.dispatch(changeDesignParameterValue("RADIUS", 0.5));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].value).toEqual(0.5); // after
+    expect(design.design_parameters[1].value).toEqual(0.5);
     expect(design.state_variables[1].name).toEqual("AREA");
     expect(design.state_variables[1].value).toEqual(0.5026548245743669);
 });
 
-it('change design parameter constraint min', () => {
+it('reducers change design parameter constraint min', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].cmin).toEqual(0.0); // before
+    expect(design.design_parameters[1].cmin).toEqual(0.0);
     expect(design.design_parameters[1].smin).toEqual(0.4);
 
     store.dispatch(changeDesignParameterConstraint("RADIUS", MIN, 0.1));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].cmin).toEqual(0.1); // after
+    expect(design.design_parameters[1].cmin).toEqual(0.1);
     expect(design.design_parameters[1].smin).toEqual(0.1);
 });
 
-it('change design parameter constraint max', () => {
+it('reducers change design parameter constraint max', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].cmax).toEqual(0.05); // before
+    expect(design.design_parameters[2].cmax).toEqual(0.05);
     expect(design.design_parameters[2].smax).toEqual(0.05);
 
     store.dispatch(changeDesignParameterConstraint("THICKNESS", MAX, 0.06));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].cmax).toEqual(0.06); // after
+    expect(design.design_parameters[2].cmax).toEqual(0.06);
     expect(design.design_parameters[2].smax).toEqual(0.06);
 });
 
-it('set design parameter flag min', () => {
+it('reducers set design parameter flag min', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].lmin).toEqual(CONSTRAINED); // before
+    expect(design.design_parameters[1].lmin).toEqual(CONSTRAINED);
 
     store.dispatch(setDesignParameterFlag("RADIUS", MIN, FIXED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].lmin).toEqual(CONSTRAINED|FIXED); // after
+    expect(design.design_parameters[1].lmin).toEqual(CONSTRAINED|FIXED);
 });
 
-it('reset design parameter flag min', () => {
+it('reducers reset design parameter flag min', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].lmin).toEqual(CONSTRAINED); // before
+    expect(design.design_parameters[1].lmin).toEqual(CONSTRAINED);
 
     store.dispatch(resetDesignParameterFlag("RADIUS", MIN, CONSTRAINED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[1].name).toEqual("RADIUS");
-    expect(design.design_parameters[1].lmin).toEqual(0); // after
+    expect(design.design_parameters[1].lmin).toEqual(0);
 });
 
-it('set design parameter flag max', () => {
+it('reducers set design parameter flag max', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].lmax).toEqual(CONSTRAINED); // before
+    expect(design.design_parameters[2].lmax).toEqual(CONSTRAINED);
 
     store.dispatch(setDesignParameterFlag("THICKNESS", MAX, FIXED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].lmax).toEqual(CONSTRAINED|FIXED); // after
+    expect(design.design_parameters[2].lmax).toEqual(CONSTRAINED|FIXED);
 });
 
-it('reset design parameter flag max', () => {
+it('reducers reset design parameter flag max', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].lmax).toEqual(CONSTRAINED); // before
+    expect(design.design_parameters[2].lmax).toEqual(CONSTRAINED);
 
     store.dispatch(resetDesignParameterFlag("THICKNESS", MAX, CONSTRAINED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.design_parameters[2].name).toEqual("THICKNESS");
-    expect(design.design_parameters[2].lmax).toEqual(0); // after
+    expect(design.design_parameters[2].lmax).toEqual(0);
 });
 
 //=====================================================================
 // STATE VARIABLES
 //=====================================================================
 
-it('change state variable value', () => {
+it('reducers change state variable value', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.state_variables[1].name).toEqual("AREA");
     expect(design.state_variables[1].value).toEqual(0.5026548245743669);
 
     store.dispatch(changeStateVariableValue("AREA", 0.7853981633974483));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.state_variables[1].name).toEqual("AREA");
     expect(design.state_variables[1].value).toEqual(0.7853981633974483);
 });
 
-it('change state variable constraint min', () => {
+it('reducers change state variable constraint min', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].cmin).toEqual(1000);
     expect(design.state_variables[0].smin).toEqual(1000);
 
     store.dispatch(changeStateVariableConstraint("FORCE", MIN, 10000));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.state_variables[0].name).toEqual("FORCE");
     expect(design.state_variables[0].cmin).toEqual(10000);
     expect(design.state_variables[0].smin).toEqual(10000);
 });
 
-it('change state variable constraint max', () => {
+it('reducers change state variable constraint max', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.state_variables[2].name).toEqual("STRESS");
     expect(design.state_variables[2].cmax).toEqual(3000);
     expect(design.state_variables[2].smax).toEqual(3000);
 
     store.dispatch(changeStateVariableConstraint("STRESS", MAX, 4000));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.state_variables[2].name).toEqual("STRESS");
     expect(design.state_variables[2].cmax).toEqual(4000);
     expect(design.state_variables[2].smax).toEqual(4000);
 });
 
-it('set state variable flag min', () => {
+it('reducers set state variable flag min', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.state_variables[0].name).toEqual("FORCE");
-    expect(design.state_variables[0].lmin).toEqual(CONSTRAINED); // before
+    expect(design.state_variables[0].lmin).toEqual(CONSTRAINED);
 
     store.dispatch(setStateVariableFlag("FORCE", MIN, FIXED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.state_variables[0].name).toEqual("FORCE");
-    expect(design.state_variables[0].lmin).toEqual(CONSTRAINED|FIXED); // after
+    expect(design.state_variables[0].lmin).toEqual(CONSTRAINED|FIXED);
 });
 
-it('reset state variable flag min', () => {
+it('reducers reset state variable flag min', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.state_variables[0].name).toEqual("FORCE");
-    expect(design.state_variables[0].lmin).toEqual(CONSTRAINED); // before
+    expect(design.state_variables[0].lmin).toEqual(CONSTRAINED);
 
     store.dispatch(resetStateVariableFlag("FORCE", MIN, CONSTRAINED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.state_variables[0].name).toEqual("FORCE");
-    expect(design.state_variables[0].lmin).toEqual(0); // after
+    expect(design.state_variables[0].lmin).toEqual(0);
 });
 
-it('set state variable flag max', () => {
+it('reducers set state variable flag max', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.state_variables[2].name).toEqual("STRESS");
-    expect(design.state_variables[2].lmax).toEqual(CONSTRAINED); // before
+    expect(design.state_variables[2].lmax).toEqual(CONSTRAINED);
 
     store.dispatch(setStateVariableFlag("STRESS", MAX, FIXED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.state_variables[2].name).toEqual("STRESS");
-    expect(design.state_variables[2].lmax).toEqual(CONSTRAINED|FIXED); // after
+    expect(design.state_variables[2].lmax).toEqual(CONSTRAINED|FIXED);
 });
 
-it('reset state variable flag max', () => {
+it('reducers reset state variable flag max', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.state_variables[2].name).toEqual("STRESS");
-    expect(design.state_variables[2].lmax).toEqual(CONSTRAINED); // before
+    expect(design.state_variables[2].lmax).toEqual(CONSTRAINED);
 
     store.dispatch(resetStateVariableFlag("STRESS", MAX, CONSTRAINED));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.state_variables[2].name).toEqual("STRESS");
-    expect(design.state_variables[2].lmax).toEqual(0); // after
+    expect(design.state_variables[2].lmax).toEqual(0);
 });
 
 //=====================================================================
 // OBJECTIVE FUNCTION
 //=====================================================================
 
-it('change objective value', () => {
+it('reducers change objective value', () => {
     const store = createStore(
         pcylWebApp,
         initialState);
     
-    var design = store.getState();
+    var design = store.getState(); // before
     expect(design.objective_value).toEqual(0.5605106435926049);
 
     store.dispatch(changeObjectiveValue(0.987654321));
     
-    design = store.getState();
+    design = store.getState(); // after
     expect(design.objective_value).toEqual(0.987654321);
 });
