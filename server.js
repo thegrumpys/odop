9a8c743e255f86f3e909ffc7fc1cedc4436070f5
Brian Watt <bwatt@1fifoto.com>
2018-07-26 22:41:16 -0500

    48 Add api/designs/:name GET, PUT, and DELETE to Server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 const app = express();
 app.use(bodyParser.json({ type: 'application/json' }));
@@ -11,14 +12,13 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // Put all API endpoints under '/api'
 app.get('/api/v1/designs', (req, res) => {
     console.log('===========================================================');
-    console.log('In GET /api/designs');
-    var mysql = require('mysql');
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    console.log('In GET /api/v1/designs');
+    var connection = .createConnection(process.env.JAWSDB_URL);
     connection.connect();
     connection.query('SELECT name FROM design', function(err, rows, fields) {
-        console.log(err);
-        console.log(rows);
-        console.log(fields);
+//        console.log(err);
+//        console.log(rows);
+//        console.log(fields);
         if (err) throw err;
         res.json(rows.map((row,index) => {return row.name}));
     });
@@ -27,16 +27,15 @@ app.get('/api/v1/designs', (req, res) => {
 
 app.get('/api/v1/designs/:name', (req, res) => {
     console.log('===========================================================');
-    console.log('In GET /api/designs/:name');
+    console.log('In GET /api/v1/designs/:name');
     var name = req.params['name'];
     console.log('name='+name);
-    var mysql = require('mysql');
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    var connection = .createConnection(process.env.JAWSDB_URL);
     connection.connect();
     connection.query('SELECT value FROM design where name = \''+name+'\'', function(err, rows, fields) {
-        console.log(err);
-        console.log(rows);
-        console.log(fields);
+//        console.log(err);
+//        console.log(rows);
+//        console.log(fields);
         if (err) throw err;
         if (rows.length == 1) res.json(JSON.parse(rows[0].value));
     });
@@ -50,15 +49,14 @@ app.post('/api/v1/designs/:name', (req, res) => {
     var name = req.params['name'];
     var value = JSON.stringify(req.body.value);
     console.log('name='+name+' value='+value);
-    var mysql = require('mysql');
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    var connection = .createConnection(process.env.JAWSDB_URL);
     connection.connect();
 //    INSERT INTO table_name (column1, column2, column3, ...)
 //    VALUES (value1, value2, value3, ...);
     connection.query('INSERT INTO design (name, value) VALUES (\''+name+'\',\''+value+'\')', function(err, rows, fields) {
-        console.log(err);
-        console.log(rows);
-        console.log(fields);
+//        console.log(err);
+//        console.log(rows);
+//        console.log(fields);
         if (err) throw err;
         res.end();
     });
@@ -71,16 +69,15 @@ app.put('/api/v1/designs/:name', (req, res) => {
     var name = req.params['name'];
     var value = req.params['value'];
     console.log('name='+name+' value='+value);
-    var mysql = require('mysql');
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    var connection = .createConnection(process.env.JAWSDB_URL);
     connection.connect();
 //    UPDATE table_name
 //    SET column1 = value1, column2 = value2, ...
 //    WHERE condition;
     connection.query('UPDATE design SET value = \''+value+'\' WHERE name = \''+name+'\'', function(err, rows, fields) {
-        console.log(err);
-        console.log(rows);
-        console.log(fields);
+//        console.log(err);
+//        console.log(rows);
+//        console.log(fields);
         if (err) throw err;
         res.end();
     });
@@ -92,15 +89,14 @@ app.delete('/api/v1/designs/:name', (req, res) => {
     console.log('In DELETE /api/v1/designs/:name');
     var name = req.params['name'];
     console.log('name='+name);
-    var mysql = require('mysql');
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    var connection = .createConnection(process.env.JAWSDB_URL);
     connection.connect();
 //    DELETE FROM table_name
 //    WHERE condition;
     connection.query('DELETE FROM design WHERE name = \''+name+'\'', function(err, rows, fields) {
-        console.log(err);
-        console.log(rows);
-        console.log(fields);
+//        console.log(err);
+//        console.log(rows);
+//        console.log(fields);
         if (err) throw err;
         res.end();
     });
