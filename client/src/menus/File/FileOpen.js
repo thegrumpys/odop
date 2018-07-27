0837c38c8c9ecae324d096b6defdbcf07c4e62e4
Grumpy In CA <WinTenTester@outlook.com>
2018-07-26 17:32:29 -0700

    49 Implement client side of File : Open, Save, Save As, & Delete

--- client/src/menus/File/FileOpen.js
+++ client/src/menus/File/FileOpen.js
@@ -1,24 +1,71 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, DropdownItem } from 'reactstrap';
-import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Label, Input } from 'reactstrap';
 import { connect } from 'react-redux';
+import { load } from '../../actionCreators';
 
 class FileOpen extends React.Component {
     constructor(props) {
         super(props);
         this.toggle = this.toggle.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+        this.onOpen = this.onOpen.bind(this);
+        this.onSelect = this.onSelect.bind(this);
         this.state = {
-            modal: false
+            modal: false,
+            designs: []
         };
     }
 
+    getDesigns() {
+        // Get the designs and store them in state
+        fetch('/api/v1/designs')
+          .then(res => res.json())
+          .then(designs => this.setState({ designs }));
+    }
+    
+    getDesign(name) {
+        fetch('/api/v1/designs/' + name)
+        .then(res => res.json())
+        .then(design => this.setState({ design }));
+    }
+    
     toggle() {
+        this.getDesigns();
         this.setState({
             modal: !this.state.modal
         });
     }
+    
+    onSelect(event) {
+        console.log(event.target.value)
+        this.setState({
+            choice: event.target.value 
+        });
+    }
+    
+    onOpen() {
+        this.setState({
+            modal: !this.state.modal
+        });
+        console.log(this.state.choice);
+        // Load the model
+        // Step 1 - Get design blob (this.state.choice)
+        this.getDesign(this.state.choice);
+        // Step 2 - Put blob in store
+        this.props.load(this.state.design);
+        
+    }
+    
+    onCancel() {
+        this.setState({
+            modal: !this.state.modal
+        });
+        // Noop - all done
+    }
 
     render() {
+        const { designs } = this.state;
         return (
             <React.Fragment>
                 <DropdownItem onClick={this.toggle}>
@@ -29,55 +76,16 @@ class FileOpen extends React.Component {
                     <ModalBody>
                         Implementation in progress for software version 0.4. <br />
                         <br />
-                        <FormGroup>
                         <Label for="fileOpenSelect">Select design to open:</Label>
-                        <Input type="select" name="select" id="fileOpenSelect">
-                          <option>design 1</option>
-                          <option>design 2</option>
-                          <option>design 3</option>
-                          <option>design 4</option>
-                          <option>design 5</option>
-                          <option>design 6</option>
-                          <option>design 7</option>
-                          <option>design 8</option>
-                          <option>design 9</option>
-                          <option>design 10</option>
-                          <option>design 11</option>
-                          <option>design 12</option>
-                          <option>design 13</option>
-                          <option>design 14</option>
-                          <option>design 15</option>
-                          <option>design 16</option>
-                          <option>design 17</option>
-                          <option>design 18</option>
-                          <option>design 19</option>
-                          <option>design 20</option>
-                          <option>design 21</option>
-                          <option>design 22</option>
-                          <option>design 23</option>
-                          <option>design 24</option>
-                          <option>design 25</option>
-                          <option>design 26</option>
-                          <option>design 27</option>
-                          <option>design 28</option>
-                          <option>design 29</option>
-                          <option>design 30</option>
-                          <option>design 31</option>
-                          <option>design 32</option>
-                          <option>design 33</option>
-                          <option>design 34</option>
-                          <option>design 35</option>
-                          <option>design 36</option>
-                          <option>design 37</option>
-                          <option>design 38</option>
-                          <option>design 39</option>
-                          <option>design 40</option>
+                        <Input type="select" name="select" id="fileOpenSelect" onClick={this.onSelect}>
+                            {designs.map((design, index) =>
+                                <option key={index}>{design}</option>
+                            )}
                         </Input>
-                      </FormGroup>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="primary" onClick={this.toggle}>Open</Button>{' '}
-                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                        <Button color="secondary" onClick={this.onCancel}>Cancel</Button>{' '}
+                        <Button color="primary" onClick={this.onOpen}>Open</Button>
                     </ModalFooter>
                 </Modal>
             </React.Fragment>
@@ -85,9 +93,8 @@ class FileOpen extends React.Component {
     }
 }  
 
-const mapStateToProps = state => ({
-    name: state.name, 
-    version: state.version
-  });
+const mapDispatchToProps = {
+        load: load
+};
 
-export default connect(mapStateToProps)(FileOpen);
+export default connect(null, mapDispatchToProps)(FileOpen);
