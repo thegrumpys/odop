8e5807af1787ace647e6a2be24a8be2f2ce3fb43
Grumpy In CA <WinTenTester@outlook.com>
2018-07-27 17:48:25 -0700

    49 Implement client side of File : Open, Save, Save As, & Delete
    Early attempt at FileSaveAs.js

--- client/src/menus/File/FileSaveAs.js
+++ client/src/menus/File/FileSaveAs.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, DropdownItem } from 'reactstrap';
+import { Label, Input } from 'reactstrap';
 import { connect } from 'react-redux';
+import { unload } from '../../actionCreators';
 
 class FileSaveAs extends React.Component {
     constructor(props) {
         super(props);
         this.toggle = this.toggle.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+        this.onSaveAs = this.onSaveAs.bind(this);
         this.state = {
             modal: false
         };
+    }
+    
+    putDesign(name) {
+//        fetch('/api/v1/designs/' + name)
+//        .then(res => res.json())
+//        .then(design => this.props.post(design));
     }
 
     toggle() {
         this.setState({
             modal: !this.state.modal
         });
+    }
+        
+    onSaveAs() {
+        this.setState({
+            modal: !this.state.modal
+        });
+        // Save the model
+        var name;
+//      var name = inputText;
+        console.log("name=", name);
+        if (name === undefined) name = 'undef_startup';
+        this.putDesign(name);
+    }
+    
+    onCancel() {
+        this.setState({
+            modal: !this.state.modal
+        });
+        // Noop - all done
     }
 
     render() {
@@ -26,10 +55,15 @@ class FileSaveAs extends React.Component {
                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
                     <ModalHeader toggle={this.toggle}><img src="favicon.ico" alt="The Grumpys"/> &nbsp; File : Save As </ModalHeader>
                     <ModalBody>
-                        This menu item will be implemented in software version 0.4.
+                    Implementation in progress for software version 0.4. <br />
+                    <br />
+                    <Label for="fileSaveAsText">Save As:</Label>
+                    <Input type="text" name="inputText" id="fileSaveAsText" placeholder="Enter design name here" >
+                    </Input>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="primary" onClick={this.toggle}>Close</Button>
+                    <Button color="secondary" onClick={this.onCancel}>Cancel</Button>{' '}
+                    <Button color="primary" onClick={this.onSaveAs}>Save As</Button>
                     </ModalFooter>
                 </Modal>
             </React.Fragment>
@@ -37,9 +71,8 @@ class FileSaveAs extends React.Component {
     }
 }  
 
-const mapStateToProps = state => ({
-    name: state.name, 
-    version: state.version
-  });
+    const mapDispatchToProps = {
+            unload: unload
+    };
 
-export default connect(mapStateToProps)(FileSaveAs);
+    export default connect(null, mapDispatchToProps)(FileSaveAs);
---
+++ client/src/menus/File/FileSaveAs_X.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, DropdownItem } from 'reactstrap';
+import { connect } from 'react-redux';
+
+class FileSaveAs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            modal: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <DropdownItem onClick={this.toggle}>
+                    Save As
+                </DropdownItem>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}><img src="favicon.ico" alt="The Grumpys"/> &nbsp; File : Save As </ModalHeader>
+                    <ModalBody>
+                        This menu item will be implemented in software version 0.4.
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}  
+
+const mapStateToProps = state => ({
+    name: state.name, 
+    version: state.version
+  });
+
+export default connect(mapStateToProps)(FileSaveAs);
