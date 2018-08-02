b5a02e6d95b8d1b2936c442b4f31a8e093e57a45
Brian Watt <bwatt@1fifoto.com>
2018-08-02 14:44:53 -0500

    73 Add File : Preferences

---
+++ client/src/menus/File/FilePreferences.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, DropdownItem } from 'reactstrap';
+import { connect } from 'react-redux';
+
+class FilePreferences extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            modal: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <DropdownItem onClick={this.toggle}>
+                    Preferences
+                </DropdownItem>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}><img src="favicon.ico" alt="The Grumpys"/> &nbsp; Select Size</ModalHeader>
+                    <ModalBody>
+                        Not implemented.
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}  
+
+const mapStateToProps = state => ({
+});
+
+export default connect(mapStateToProps)(FilePreferences);
