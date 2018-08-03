dc545e9ad617fbcf21178e6731187a14e37dc266
Grumpy In CA <WinTenTester@outlook.com>
2018-08-02 21:39:51 -0700

    23 Idea for web page initialization sequence
    Before removing commented code.

---
+++ client/src/components/PromptForDesign.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Label, Input } from 'reactstrap';
+//import { connect } from 'react-redux';
+//import { load } from '../store/actionCreators';
+//import { displayError } from './ErrorModal';
+//import { displaySpinner } from './Spinner';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'
+import { initialState } from '../initialState';
+import App from './App';
+import { displaySpinner } from './Spinner';
+import { displayError } from './ErrorModal';
+import { pcylWebApp } from '../store/reducers';
+import { dispatcher } from '../store/middleware/dispatcher';
+
+export class PromptForDesign extends React.Component {
+    constructor(props) {
+        super(props);
+//        this.toggle = this.toggle.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+        this.onOpen = this.onOpen.bind(this);
+        this.onSelect = this.onSelect.bind(this);
+        this.state = {
+//            modal: false,
+            modal: true,
+            designs: [],
+            name: "startup"
+        };
+        this.getDesigns();
+    }
+
+    getDesigns() {
+        // Get the designs and store them in state
+        displaySpinner(true);
+        fetch('/api/v1/designs')
+            .then(res => {
+                displaySpinner(false);
+                if (!res.ok) {
+                   throw Error(res.statusText);
+                }
+                return res.json()
+            })
+            .then(designs => this.setState({ designs }))
+            .catch(error => {
+                displayError('GET of design names failed: '+error.message);
+            });
+    }
+    
+//    getDesign(name) {
+////        console.log('In getDesign name=', name);
+//        displaySpinner(true);
+//        fetch('/api/v1/designs/' + name)
+//            .then(res => {
+//                displaySpinner(false);
+//                if (!res.ok) {
+//                    throw Error(res.statusText);
+//                }
+//                return res.json()
+//            })
+//            .then(design => this.props.load(design))
+//            .catch(error => {
+//                displayError('GET of \''+name+'\' design failed: '+error.message);
+//            });
+//    }
+    
+    getDesign(name) {
+        
+//    function loggerMiddleware({ getState }) {
+//      return next => action => {
+//        console.log('will dispatch', action)
+//        
+//        // Call the next dispatch method in the middleware chain.
+//        const returnValue = next(action)
+//        
+//        console.log('state after dispatch', getState())
+//        
+//        // This will likely be the action itself, unless
+//        // a middleware further in chain changed it.
+//        return returnValue
+//      }
+//    }
+
+        /* eslint-disable no-underscore-dangle */
+        const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+        /* eslint-enable */
+
+        const middleware = composeEnhancers(applyMiddleware(/*loggerMiddleware,*/dispatcher));
+
+        displaySpinner(true);
+        fetch('/api/v1/designs/'+name)
+            .then(res => {
+                displaySpinner(false);
+                if (!res.ok) {
+                    throw Error(res.statusText);
+                }
+                return res.json()
+            })
+            .then(design => {
+                const store = createStore(pcylWebApp, design, middleware);
+                ReactDOM.render(<Provider store={store}><App store={store} /></Provider>, document.getElementById('root2'));
+            })
+            .catch(error => {
+                displayError('GET of \'startup\' design failed with message: \''+error.message+'\'. Using builtin initialState instead. You may continue in "demo mode" but you will be unable to save your work.');
+                const store = createStore(pcylWebApp, initialState, middleware);
+                ReactDOM.render(<Provider store={store}><App store={store} /></Provider>, document.getElementById('root2'));
+            });
+    }
+//    
+//    toggle() {
+//        this.getDesigns();
+//        this.setState({
+//            modal: !this.state.modal,
+//            name: this.props.name
+//        });
+//    }
+//    
+    onSelect(event) {
+//        console.log(event.target.value)
+        this.setState({
+            name: event.target.value 
+        });
+    }
+    
+    onOpen() {
+        this.setState({
+            modal: !this.state.modal
+        });
+//        console.log(this.state.name);
+        // Load the model
+        var name = this.state.name;
+        if (name === undefined) name = 'startup';
+        this.getDesign(name);
+    }
+    
+    onCancel() {
+        this.setState({
+            modal: !this.state.modal
+        });
+        // Noop - all done
+    }
+
+    render() {
+        const { designs } = this.state;
+        return (
+            <React.Fragment>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}><img src="favicon.ico" alt="The Grumpys"/> &nbsp; Select starting design </ModalHeader>
+                    <ModalBody>
+                        <br />
+                        <Label for="fileOpenSelect">Select design to open:</Label>
+                        <Input type="select" id="fileOpenSelect" onChange={this.onSelect} value={this.state.name}>
+                            {designs.map((design, index) =>
+                                <option key={index} value={design}>{design}</option>
+                            )}
+                        </Input>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={this.onCancel}>Cancel</Button>{' '}
+                        <Button color="primary" onClick={this.onOpen}>Open</Button>
+                    </ModalFooter>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}  
+
+//const mapStateToProps = state => ({
+//    name: state.name, 
+//});
+//
+//const mapDispatchToProps = {
+//    load: load
+//};
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(PromptForDesign);
