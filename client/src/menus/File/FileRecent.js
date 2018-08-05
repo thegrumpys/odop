606581e8ed449c5b0be0bd06aacdd8cddef068d2
Grumpy In CA <WinTenTester@outlook.com>
2018-08-04 18:42:20 -0700

    80 File : Recently opened menu item

--- client/src/menus/File/FileRecent.js
+++ client/src/menus/File/FileRecent.js
@@ -1,113 +1,35 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, DropdownItem } from 'reactstrap';
-import { Label, Input } from 'reactstrap';
 import { connect } from 'react-redux';
-import { load } from '../../store/actionCreators';
-import { displayError } from '../../components/ErrorModal';
-import { displaySpinner } from '../../components/Spinner';
 
 class FileRecent extends React.Component {
     constructor(props) {
         super(props);
         this.toggle = this.toggle.bind(this);
-        this.onCancel = this.onCancel.bind(this);
-        this.onOpen = this.onOpen.bind(this);
-        this.onSelect = this.onSelect.bind(this);
         this.state = {
-            modal: false,
-            designs: [],
-            name: this.props.name
+            modal: false
         };
     }
 
-    getDesigns() {
-        // Get the designs and store them in state
-        displaySpinner(true);
-        fetch('/api/v1/designs')
-            .then(res => {
-                displaySpinner(false);
-                if (!res.ok) {
-                   throw Error(res.statusText);
-                }
-                return res.json()
-            })
-            .then(designs => this.setState({ designs }))
-            .catch(error => {
-                displayError('GET of design names failed: '+error.message);
-            });
-    }
-    
-    getDesign(name) {
-//        console.log('In getDesign name=', name);
-        displaySpinner(true);
-        fetch('/api/v1/designs/' + name)
-            .then(res => {
-                displaySpinner(false);
-                if (!res.ok) {
-                    throw Error(res.statusText);
-                }
-                return res.json()
-            })
-            .then(design => this.props.load(design))
-            .catch(error => {
-                displayError('GET of \''+name+'\' design failed: '+error.message);
-            });
-    }
-    
     toggle() {
-        this.getDesigns();
-        this.setState({
-            modal: !this.state.modal,
-            name: this.props.name
-        });
-    }
-    
-    onSelect(event) {
-//        console.log(event.target.value)
-        this.setState({
-            name: event.target.value 
-        });
-    }
-    
-    onOpen() {
         this.setState({
             modal: !this.state.modal
         });
-//        console.log(this.state.name);
-        // Load the model
-        var name = this.state.name;
-        if (name === undefined) name = 'startup';
-        this.getDesign(name);
     }
     
-    onCancel() {
-        this.setState({
-            modal: !this.state.modal
-        });
-        // Noop - all done
-    }
-
     render() {
-        const { designs } = this.state;
         return (
             <React.Fragment>
                 <DropdownItem onClick={this.toggle}>
                     Recent
                 </DropdownItem>
                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}><img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon"/> &nbsp; File : Open </ModalHeader>
+                    <ModalHeader toggle={this.toggle}><img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon"/> &nbsp; File : Recent </ModalHeader>
                     <ModalBody>
-                        <br />
-                        <Label for="fileOpenSelect">Select design to open:</Label>
-                        <Input type="select" id="fileOpenSelect" onChange={this.onSelect} value={this.state.name}>
-                            {designs.map((design, index) =>
-                                <option key={index} value={design}>{design}</option>
-                            )}
-                        </Input>
+                        Not implemented
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="secondary" onClick={this.onCancel}>Cancel</Button>{' '}
-                        <Button color="primary" onClick={this.onOpen}>Open</Button>
+                        <Button color="primary" onClick={this.toggle}>Close</Button>
                     </ModalFooter>
                 </Modal>
             </React.Fragment>
@@ -116,11 +38,9 @@ class FileRecent extends React.Component {
 }  
 
 const mapStateToProps = state => ({
-    name: state.name, 
 });
 
 const mapDispatchToProps = {
-    load: load
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(FileRecent);
