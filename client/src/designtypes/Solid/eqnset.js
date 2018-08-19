d2649161b72d43101bc6d765f986ddd3cbd92387
Grumpy In CA <WinTenTester@outlook.com>
2018-08-18 18:23:22 -0700

    141 Modify template files to create rectangular Solid design type
    Work in progress

--- client/src/designtypes/Solid/eqnset.js
+++ client/src/designtypes/Solid/eqnset.js
@@ -1,4 +1,25 @@
 export function eqnset(d, p) {
     var x = [];
+
+    const Density = 0;
+    const Length = 0;
+    const Width = 1;
+    const Height = 2;
+    const Volume = 0;
+    const SurfaceArea = 1;
+    const VolToSurfArea = 2;
+    const Girth = 3;
+    const LengthPlusGirth = 4;
+    const Diagonal = 5;
+    const Weight = 6;
+
+    x[Volume] = p[Length] * p[Width] * p[Height];
+    x[SurfaceArea] = 2.0 * p[Length] * p[Width] + 2.0 * p[Length] * p[Height] + 2.0 * p[Width] * p[Height];
+    x[VolToSurfArea] = x[Volume] / x[SurfaceArea];
+    x[Girth] = 2.0 * p[Width] + 2.0 * p[Height];
+    x[LengthPlusGirth] = p[Length] + x[Girth];
+    x[Diagonal] = Math.sqrt(p[Length] * p[Length] + p[Width] * p[Width] + p[Height] * p[Height]);
+    x[Weight] = x[Volume] * d[Density];
+    
     return x;
 }
