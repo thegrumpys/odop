a0a44d624f612da3ce72c288d1ed00b206ad467e
Grumpy In CA <WinTenTester@outlook.com>
2018-08-18 18:36:27 -0700

    141 Modify template files to create rectangular Solid design type
    First draft of initialState and eqnset complete.
    load.sql is next

--- client/src/designtypes/Solid/initialState.js
+++ client/src/designtypes/Solid/initialState.js
@@ -183,21 +183,5 @@ export const initialState = {
             "objective_value": 0.0,
             "termination_condition": "None",
             "violated_constraint_count": 0
-        },
-        "system_controls": {
-            "ioopt": 3,
-            "maxit": 100,
-            "weapon": 1,
-            "nmerit": 1,
-            "fix_wt": 1.5,
-            "con_wt": 1.0,
-            "zero_wt": 10.0,
-            "viol_wt": 1.0,
-            "mfn_wt": 0.01,
-            "objmin": 0.00005,
-            "del": 1.0,
-            "delmin": 0.0001,
-            "tol": 0.0001,
-            "smallnum": 1.0e-07
         }
     };
