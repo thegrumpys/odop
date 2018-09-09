d933e98b2e7c18a1f6050316075cf6b16cafec2b
Brian Watt <bwatt@1fifoto.com>
2018-09-09 10:01:00 -0500

    196 Add View Offset menu item (only for development)

---
+++ client/src/menus/View/ViewOffsets.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { DropdownItem, Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { connect } from 'react-redux';
+
+class ViewOffsets extends React.Component {
+    constructor(props) {
+        console.log('In ViewOffsets.constructor');
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            modal: false
+        };
+    }
+    
+    toggle() {
+        console.log('In ViewOffsets.toggle');
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+//    var p = [];
+//    var ip = 0;
+//    for (let i = 0; i < design.symbol_table.length; i++) {
+//        element = design.symbol_table[i];
+//        if (element.input) {
+//            output_offest_flag && console.log('export const',element.name,'=',ip++,';');
+//            p.push(element.value);
+//        }
+//    }
+//    var x = [];
+//    var ix = 0;
+//    for (let i = 0; i < design.symbol_table.length; i++) {
+//        element = design.symbol_table[i];
+//        if (!element.input) {
+//            output_offest_flag && console.log('export const',element.name,'=',ix++,';');
+//            x.push(element.value);
+//        }
+//    }
+    
+    render() {
+        console.log('In ViewOffsets.render');
+        var ip = 0;
+        var ix = 0;
+        return (
+            <React.Fragment>
+                <DropdownItem onClick={this.toggle}>
+                    Offsets
+                </DropdownItem>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}><img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon"/> &nbsp; View : Offsets </ModalHeader>
+                    <ModalBody>
+                        <pre>
+                        {'// Independent Variables (input-only)\n'}
+                        {this.props.symbol_table.map((element) => {return element.input ? 'export const ' + element.name.replace('%','PC').replace('/','_').replace('@','_') + ' = ' + (ip++) + ';\n' : ''})}
+                        {'\n// Dependent Variables (input-output)\n'}
+                        {this.props.symbol_table.map((element) => {return !element.input ? 'export const ' + element.name.replace('%','PC').replace('/','_').replace('@','_') + ' = ' + (ix++) + ';\n' : ''})}
+                        </pre>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}  
+
+const mapStateToProps = state => ({
+    symbol_table: state.symbol_table,
+});
+
+export default connect(mapStateToProps)(ViewOffsets);
