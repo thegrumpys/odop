a07cc00f4c38f970e348fc356981a46cc8a8acd7
Brian Watt <bwatt@1fifoto.com>
2018-09-12 13:42:09 -0500

    176 Create spring design report

--- client/src/designtypes/Spring/report.js
+++ client/src/designtypes/Spring/report.js
@@ -2,7 +2,7 @@ import React from 'react';
 import * as o from './offsets';
 
 export function report(p, x) {
-    console.log('In report p=',p,' x=',x);
+//    console.log('In report p=',p,' x=',x);
     return (
         <React.Fragment>
             <table>
