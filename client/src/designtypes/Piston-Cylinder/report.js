105a56c303ea87b7dde4bd9dc40b2ff4a8e1ede7
Brian Watt <bwatt@1fifoto.com>
2018-09-12 13:23:41 -0500

    176 Create spring design report

---
+++ client/src/designtypes/Piston-Cylinder/report.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as o from './offsets';
+
+export function report(p, x) {
+    console.log('In report p=',p,' x=',x);
+    return (
+        <span>Not Implemented</span>
+    );
+}
