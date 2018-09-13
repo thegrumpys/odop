9ab2dcc543c2286dd9133f93513681165bf64fb7
Grumpy In CA <WinTenTester@outlook.com>
2018-09-12 17:10:47 -0700

    177 Investigate unexpected result with Spring problem Seek

--- client/src/store/reducers.js
+++ client/src/store/reducers.js
@@ -47,7 +47,8 @@ export function reducers(state, action) {
         return Object.assign({}, state, {
             symbol_table: state.symbol_table.map((element) => {
                 if (element.name === action.payload.name) {
-//                    console.log('CHANGE_SYMBOL_VALUE element=',element.name,' old value=',element.value,' new value=',action.payload.value);
+//                    if (element.name === 'Force_2')
+//                        console.log('CHANGE_SYMBOL_VALUE element=',element.name,' old value=',element.value,' new value=',action.payload.value);
                     return Object.assign({}, element, {
                         value: action.payload.value
                     });
@@ -165,7 +166,8 @@ export function reducers(state, action) {
                 if (element.input) {
                     value = action.payload.values[i++]
                     if (value !== undefined) {
-//                        console.log('CHANGE_INPUT_SYMBOL_VALUES i=',i-1,' element=',element.name,' old value=',element.value,' new value=',value);
+//                        if (element.name === 'Force_2')
+//                            console.log('CHANGE_INPUT_SYMBOL_VALUES i=',i-1,' element=',element.name,' old value=',element.value,' new value=',value);
                         return Object.assign({}, element, {
                             value: value
                         });
