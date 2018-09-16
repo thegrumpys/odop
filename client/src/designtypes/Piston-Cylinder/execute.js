59e8d0acc399bfbff332b847e90fcbafee0b475c
Brian Watt <bwatt@1fifoto.com>
2018-09-16 11:57:41 -0500

    202 Implement Demo/Tutorial Capability

---
+++ client/src/designtypes/Piston-Cylinder/execute.js
@@ -0,0 +1,8 @@
+export function getExecuteNames() {
+    console.log('In getExecuteNames');
+    return [];
+}
+
+export function execute(execute_name) {
+    console.log('In execute execute_name=',execute_name);
+}
