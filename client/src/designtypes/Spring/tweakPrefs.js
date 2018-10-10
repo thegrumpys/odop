9827d2eb366863ff15e05d3c3fd382c65c73e571
Grumpy In CA <WinTenTester@outlook.com>
2018-10-09 17:31:50 -0700

    137 Implement generic tutorial content
    tweakPrefs is complete.

--- client/src/designtypes/Spring/tweakPrefs.js
+++ client/src/designtypes/Spring/tweakPrefs.js
@@ -6,15 +6,19 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    Click Next to tweak Preference values for improved Search precision.
+                    Click Next to tweak these Preference values for improved Search precision.
                     </p>
                     
                     <p>
-                    Note: these changes may result in longer execution times for Search.
+                    maxit = 190<br />
+                    objmin = 0.000005<br />
+                    delmin = 0.00001<br />
+                    tol = 0.00001<br />
+                    smallnum = 1e-8<br />
                     </p>
                     
                     <p>
-                    &nbsp;
+                    Note: these changes may result in longer execution times for some Searches.
                     </p>
                 </React.Fragment>
             )
@@ -22,12 +26,19 @@ export const execute = {
         {
             text: (
                 <React.Fragment>
-                    Changes imposed are:<br />
+                    <b>New values:</b><br />
                     maxit = 190<br />
+                    objmin = 0.000005<br />
+                    delmin = 0.00001<br />
+                    tol = 0.00001<br />
+                    smallnum = 1e-8<br />
+                    <br />
+                    Reset original values with <b>File : Preferences</b> Restore Defaults<br />
+                    Click Next to close.
                 </React.Fragment>
             ),
             actions: [
-                changeSystemControlsValue({maxit: 190})
+                changeSystemControlsValue({maxit: 190, objmin: 0.000005, delmin: 0.00001, tol: 0.00001, smallnum: 1e-8})
             ]
         }
     ]
