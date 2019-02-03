c1d1356726c1e3ce0a5ff2f0018452dd50e2905c
Brian Watt <bwatt@1fifoto.com>
2019-02-02 18:31:05 -0600

    180 Implement Functionally Determined Constraint Levels

--- client/src/components/ConstraintsMinRowDependentVariable.js
+++ client/src/components/ConstraintsMinRowDependentVariable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InputGroup, InputGroupAddon, InputGroupText, Input, UncontrolledTooltip } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, ButtonGroup, UncontrolledTooltip, Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 import { connect } from 'react-redux';
 import { MIN, MAX, FIXED, CONSTRAINED, FUNCTION } from '../store/actionTypes';
 import { changeSymbolConstraint, setSymbolFlag, resetSymbolFlag } from '../store/actionCreators';
@@ -12,6 +12,14 @@ class ConstraintsMinRowDependentVariable extends React.Component {
         this.onChangeDependentVariableConstraint = this.onChangeDependentVariableConstraint.bind(this);
         this.onSetDependentVariableFlagConstrained = this.onSetDependentVariableFlagConstrained.bind(this)
         this.onResetSymbolFlagConstrained = this.onResetSymbolFlagConstrained.bind(this)
+        this.onClick = this.onClick.bind(this);
+        this.onChangeValue = this.onChangeValue.bind(this);
+        this.onEnterValue = this.onEnterValue.bind(this);
+        this.onSelectVariable = this.onSelectVariable.bind(this);
+        this.onCancel = this.onCancel.bind(this);
+        this.state = {
+            modal: false, // Default: do not display
+        };
     }
     
     onSetDependentVariableFlagConstrained(event) {
@@ -29,6 +37,67 @@ class ConstraintsMinRowDependentVariable extends React.Component {
         }
     }
     
+    onClick(event) {
+//        console.log("In onClick event=",event);
+        // Show modal only if there are cminchoices
+        if (this.props.element.cminchoices !== undefined && this.props.element.cminchoices.length > 0) {
+            this.setState({
+                modal: !this.state.modal,
+                value: this.props.element.lmin & CONSTRAINED ? evaluateConstraintValue(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin) : 0
+            });
+        }
+    }
+    
+    onChangeValue(event) {
+//      console.log("In onChangeValue event=",event);
+      this.setState({
+          value: event.target.value
+      });
+    }
+    
+    onEnterValue(event) {
+//        console.log("In onEnterValue event=",event);
+        this.setState({
+            modal: !this.state.modal
+        });
+        this.props.resetSymbolFlag(this.props.element.name, MIN, FUNCTION);
+        this.props.changeSymbolConstraint(this.props.element.name, MIN, parseFloat(this.state.value));
+        if (this.props.element.lmin & FIXED) {
+            this.props.resetSymbolFlag(this.props.element.name, MAX, FUNCTION);
+            this.props.changeSymbolConstraint(this.props.element.name, MAX, parseFloat(this.state.value));
+        }
+    }
+      
+    onSelectVariable(event, name) {
+//        console.log("In onSelectVariable event=",event," name=",name);
+        this.setState({
+            modal: !this.state.modal
+        });
+        this.props.setSymbolFlag(this.props.element.name, MIN, FUNCTION);
+        this.props.symbol_table.forEach((element, i) => {
+            if (element.name === name) {
+//                console.log('@@@ element=',element,' i=',i);
+                this.props.changeSymbolConstraint(this.props.element.name, MIN, i);
+            }
+        })
+        if (this.props.element.lmin & FIXED) {
+            this.props.setSymbolFlag(this.props.element.name, MAX, FUNCTION);
+            this.props.symbol_table.forEach((element, i) => {
+                if (element.name === name) {
+//                    console.log('@@@ element=',element,' i=',i);
+                    this.props.changeSymbolConstraint(this.props.element.name, MAX, i);
+                }
+            })
+        }
+    }
+    
+    onCancel(event) {
+//        console.log("In onCancel event=",event);
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
     render() {
         // =======================================
         // Constraint Minimum Column
@@ -47,22 +116,58 @@ class ConstraintsMinRowDependentVariable extends React.Component {
         // Table Row
         // =======================================
         return (
-            <tr key={this.props.element.name}>
-                <td className="align-middle" colSpan="2">
-                    <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                            <InputGroupText>
-                                <Input addon type="checkbox" aria-label="Checkbox for minimum value" checked={this.props.element.lmin & CONSTRAINED} onChange={this.props.element.lmin & CONSTRAINED ? this.onResetSymbolFlagConstrained : this.onSetDependentVariableFlagConstrained} disabled={this.props.element.lmin & FIXED ? true : false} />
-                            </InputGroupText>
-                        </InputGroupAddon>
-                        <Input id={this.props.element.name + "_cmin"} className={cmin_class} type="number" value={this.props.element.lmin & CONSTRAINED ? evaluateConstraintValue(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin) : ''} onChange={this.onChangeDependentVariableConstraint} disabled={this.props.element.lmin & FIXED || this.props.element.lmin & CONSTRAINED ? false : true} />
-                        {this.props.element.lmin & FUNCTION ? <UncontrolledTooltip placement="top" target={this.props.element.name + "_cmin"}>{evaluateConstraintName(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin)}</UncontrolledTooltip> : ''}
-                    </InputGroup>
-                </td>
-                <td className="text-right align-middle" colSpan="1">
-                    {this.props.element.lmin & FIXED ? (this.props.element.vmin*100.0).toFixed(1) : (this.props.element.lmin & CONSTRAINED ? (this.props.element.vmin*100.0).toFixed(1) + '%' : '')}
-                </td>
-            </tr>
+            <React.Fragment>
+                <tr key={this.props.element.name}>
+                    <td className="align-middle" colSpan="2">
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>
+                                    <Input addon type="checkbox" aria-label="Checkbox for minimum value" checked={this.props.element.lmin & CONSTRAINED} onChange={this.props.element.lmin & CONSTRAINED ? this.onResetSymbolFlagConstrained : this.onSetDependentVariableFlagConstrained} disabled={this.props.element.lmin & FIXED ? true : false} />
+                                </InputGroupText>
+                            </InputGroupAddon>
+                            <Input id={this.props.element.name + "_cmin"} className={cmin_class} type="number" value={this.props.element.lmin & CONSTRAINED ? evaluateConstraintValue(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin) : ''} onChange={this.onChangeDependentVariableConstraint} disabled={this.props.element.lmin & FIXED || this.props.element.lmin & CONSTRAINED ? false : true} />
+                            {this.props.element.lmin & FUNCTION ? <UncontrolledTooltip placement="top" target={this.props.element.name + "_cmin"}>{evaluateConstraintName(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin)}</UncontrolledTooltip> : ''}
+                        </InputGroup>
+                    </td>
+                    <td className="text-right align-middle" colSpan="1">
+                        {this.props.element.lmin & FIXED ? (this.props.element.vmin*100.0).toFixed(1) : (this.props.element.lmin & CONSTRAINED ? (this.props.element.vmin*100.0).toFixed(1) + '%' : '')}
+                    </td>
+                </tr>
+                {this.props.element.cminchoices !== undefined && this.props.element.cminchoices.length > 0 ? <Modal isOpen={this.state.modal} className={this.props.className} size="lg">
+                    <ModalHeader>
+                        Set {this.props.element.name} Min Constraint
+                    </ModalHeader>
+                    <ModalBody>
+                        Select constraint variable or enter constraint value.
+                        <table>
+                            <tr>
+                                <td>Variable:&nbsp;</td>
+                                <td>
+                                    <InputGroup>
+                                        <ButtonGroup>
+                                            {this.props.element.cminchoices.map((e) => {return (
+                                                <Button key={e} color="primary" onClick={(event) => {this.onSelectVariable(event,e)}} style={{marginBotton: '5px'}} active={evaluateConstraintName(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin) == e}>{e}</Button>
+                                            );})}
+                                        </ButtonGroup>
+                                    </InputGroup>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Value:&nbsp;</td>
+                                <td>
+                                    <InputGroup>
+                                        <Input id={this.props.element.name + "_cmin"} className="text-right" type="number" value={this.state.value} onChange={this.onChangeValue} />
+                                        <Button color="primary" onClick={this.onEnterValue}>Enter</Button>
+                                    </InputGroup>
+                                </td>
+                            </tr>
+                        </table>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={this.onCancel}>Cancel</Button>
+                    </ModalFooter>
+                </Modal> : ''}
+            </React.Fragment>
         );
     }
 }
@@ -73,10 +178,10 @@ const mapStateToProps = state => ({
     objective_value: state.result.objective_value
 });
 
-const mapDispatchToDependentVariableProps = {
+const mapDispatchToProps = {
     changeSymbolConstraint: changeSymbolConstraint,
     setSymbolFlag: setSymbolFlag,
     resetSymbolFlag: resetSymbolFlag
 };
 
-export default connect(mapStateToProps, mapDispatchToDependentVariableProps)(ConstraintsMinRowDependentVariable);
+export default connect(mapStateToProps, mapDispatchToProps)(ConstraintsMinRowDependentVariable);
