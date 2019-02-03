cee37f3500042a06b4fccd90a37925c881899395
Brian Watt <bwatt@1fifoto.com>
2019-02-03 16:07:01 -0600

    180 Implement Functionally Determined Constraint Levels

--- client/src/designtypes/Spring/Extension/demo7.js
+++ client/src/designtypes/Spring/Extension/demo7.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { changeSymbolValue, changeSymbolConstraint, fixSymbolValue, freeSymbolValue, loadInitialState, setSymbolFlag, saveOutputSymbolConstraints, changeLabelsValue, search } from '../../../store/actionCreators';
-import { MIN, MAX, CONSTRAINED } from '../../../store/actionTypes';
+import { MIN, MAX, CONSTRAINED, VARIABLE } from '../../../store/actionTypes';
 import { changeSystemControlsValue } from '../../../store/actionCreators';
 export const execute = {
     "name": "demo7",
@@ -146,6 +146,7 @@ export const execute = {
                 setSymbolFlag('OD_Free', MAX, CONSTRAINED),
                 changeSymbolConstraint('OD_Free', MAX, 0.645),
                 fixSymbolValue('L_Free', 5.125),
+                resetSymbolFlag('Force_1', MIN, VARIABLE),
                 setSymbolFlag('Force_1', MIN, CONSTRAINED),
                 changeSymbolConstraint('Force_1', MIN, 45.0),
                 setSymbolFlag('Force_1', MAX, CONSTRAINED),
