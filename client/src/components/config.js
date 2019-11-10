529b2bba371194ec67837618062b2f13c93c1880
Brian Watt <bwatt@1fifoto.com>
2019-11-10 16:53:18 -0600

    Merge client's .env.dist into server's .env.dist & delete client's
    Undo all these changes!

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,5 +1,7 @@
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || '{clientId}';
-const ISSUER = process.env.REACT_APP_ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
+//const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || '{clientId}';
+//const ISSUER = process.env.REACT_APP_ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
+const CLIENT_ID = '0oa1itosqdQvfGNMD357';
+const ISSUER = 'https://dev-729070.okta.com/oauth2/default';
 
 export default {
   oidc: {
