7956b36dd99bfc704e430c9ea8d977e2b268dae0
Brian Watt <bwatt@1fifoto.com>
2019-11-18 22:07:56 -0600

    Remove /x prefix - oops!

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -3,13 +3,13 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 const env = runtimeEnv(); // Load the env object.
 
 console.log('In config: env.REACT_APP_ISSUER=', env.REACT_APP_ISSUER, 'env.REACT_APP_CLIENT_ID=', env.REACT_APP_CLIENT_ID);
-const ISSUER = env.REACT_APP_ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
-const CLIENT_ID = env.REACT_APP_CLIENT_ID || '{clientId}';
+const issuer = env.REACT_APP_ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
+const clientId = env.REACT_APP_CLIENT_ID || '{clientId}';
 
 export default {
   oidc: {
-    issuer: ISSUER,
-    clientId: CLIENT_ID,
+    issuer: issuer,
+    clientId: clientId,
     redirectUri: window.location.origin + '/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
