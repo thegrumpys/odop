4d90edda5ac09581d96a1dfb82348179d3e610cf
Brian Watt <bwatt@1fifoto.com>
2019-11-18 21:22:47 -0600

    Add debug to config.js

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -2,14 +2,7 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const env = runtimeEnv(); // Load the env object.
 
-//const ISSUER = process.env.REACT_APP_ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
-//const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || '{clientId}';
-
-//const ISSUER = 'https://dev-729070.okta.com/oauth2/default';
-//const CLIENT_ID = '0oa1s8kbaxJR4O7z7357'; // For heroku production
-//const CLIENT_ID = '0oa1s9izxgX3DHecg357'; // For heroku-staging
-//const CLIENT_ID = '0oa1itosqdQvfGNMD357'; // For localhost development
-
+console.log('In config: env.REACT_APP_ISSUER=', env.REACT_APP_ISSUER, 'env.REACT_APP_CLIENT_ID=', env.REACT_APP_CLIENT_ID);
 const ISSUER = env.REACT_APP_ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
 const CLIENT_ID = env.REACT_APP_CLIENT_ID || '{clientId}';
 
