5162e975d4c5a12d8c0754a9bb96a86450b85b6a
Brian Watt <bwatt@1fifoto.com>
2019-12-07 20:34:31 -0600

    288 Production Heroku with Okta integration
    Delete FEImplicitCallback

--- client/src/components/FEApp.js
+++ client/src/components/FEApp.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security } from '@okta/okta-react';
+import { Security, ImplicitCallback } from '@okta/okta-react';
 import config from './config';
 import FEHome from './FEHome';
 import FELogin from './FELogin';
-import FEImplicitCallback from './FEImplicitCallback';
 
 function onAuthRequired({history}) {
   history.push('/login');
@@ -23,7 +22,7 @@ class FEApp extends Component {
         <Security {...config.oidc} onAuthRequired={onAuthRequired}>
           <Route path='/' exact={true} component={FEHome} />
           <Route path='/login' component={FELogin} />
-          <Route path='/implicit/callback' component={FEImplicitCallback} />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
         </Security>
       </Router>
     );
