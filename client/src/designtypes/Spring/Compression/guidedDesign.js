3c2c0643cbe6e2bdd68240382c27ae2d666bb90e
Grumpy In CA <WinTenTester@outlook.com>
2020-01-26 11:08:25 -0800

    299  Create "Guided Design" tutorial session
    update link

--- client/src/designtypes/Spring/Compression/guidedDesign.js
+++ client/src/designtypes/Spring/Compression/guidedDesign.js
@@ -289,7 +289,7 @@ export const execute = {
                     You can follow the same browser window configuration to work with two
                     ODOP design sessions at the same time.
                     For additional details see:  
-                    <a href="https://thegrumpys.github.io/odop/Help/wideScreen" target="_blank"> Side-by-side sessions on a widescreen monitor </a>
+                    <a href="https://thegrumpys.github.io/odop/Help/wideScreen" target="_blank" rel="noopener noreferrer"> Side-by-side sessions on a widescreen monitor </a>
                     <br /><br />
                     <b>Note to developers:</b> The link below fails in the local dev environment because it
                     appends the link path to the local path in the browser address line.
