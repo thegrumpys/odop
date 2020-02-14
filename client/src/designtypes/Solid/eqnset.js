076150c67f797e4283b944f865db7e51a3f5a6ad
Grumpy In CA <WinTenTester@outlook.com>
2020-02-13 17:11:42 -0800

    331  Repair density calc in Rectangular Solid design type
    long overdue change from p to x

--- client/src/designtypes/Solid/eqnset.js
+++ client/src/designtypes/Solid/eqnset.js
@@ -6,7 +6,7 @@ export function eqnset(p, x) {
     x[o.Girth] = 2.0 * p[o.Width] + 2.0 * p[o.Height];
     x[o.Length_Girth] = p[o.Length] + x[o.Girth];
     x[o.Diagonal] = Math.sqrt(p[o.Length] * p[o.Length] + p[o.Width] * p[o.Width] + p[o.Height] * p[o.Height]);
-    x[o.Weight] = x[o.Volume] * p[o.Density];
+    x[o.Weight] = x[o.Volume] * x[o.Density];
 //    console.log('In eqnset p=',p,' x=',x);
     return x;
 }
