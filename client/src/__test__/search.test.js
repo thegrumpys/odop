47c3f53e194e02c1249bb4130a817780b40c954e
Brian Watt <bwatt@1fifoto.com>
2020-04-18 15:22:42 -0500

    359 Seek fails test. Results differ from previous release

--- client/src/__test__/search.test.js
+++ client/src/__test__/search.test.js
@@ -2,9 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { initialState } from '../designtypes/Piston-Cylinder/initialState';
 import * as sto from '../designtypes/Piston-Cylinder/symbol_table_offsets';
 import { initialSystemControls } from '../initialSystemControls';
-import { changeSymbolValue, changeResultObjectiveValue } from '../store/actionCreators';
+import { changeSymbolValue, changeResultObjectiveValue, startup } from '../store/actionCreators';
 import { reducers } from '../store/reducers';
 import { dispatcher } from '../store/middleware/dispatcher';
+import { invokeInit } from '../store/middleware/invokeInit';
+import { invokeEquationSet } from '../store/middleware/invokeEquationSet';
+import { setSclDen } from '../store/middleware/setSclDen';
+import { updateViolationsAndObjectiveValue } from '../store/middleware/updateViolationsAndObjectiveValue';
 import { search } from '../store/middleware/search';
 
 //=====================================================================
@@ -24,27 +28,32 @@ it('search without merit', () => {
     store.dispatch(changeResultObjectiveValue(0.560511));
 
     var design = store.getState(); // before
+//    store.dispatch(startup());
+    invokeInit(store);
+    invokeEquationSet(store);
+    setSclDen(store);
+    updateViolationsAndObjectiveValue(store);
     var obj = search(store, design.system_controls.objmin);
 
     var design = store.getState(); // after
-    expect(obj).toEqual(0);
+    expect(obj).toEqual(0.14664192222304165);
 
     expect(design.name).toEqual("initialState");
     expect(design.type).toEqual("Piston-Cylinder");
     expect(design.version).toEqual("3");
 
     expect(design.symbol_table[sto.PRESSURE].name).toEqual("PRESSURE"); // p vector
-    expect(design.symbol_table[sto.PRESSURE].value).toEqual(500);
+    expect(design.symbol_table[sto.PRESSURE].value).toEqual(697.2108757363197);
     expect(design.symbol_table[sto.RADIUS].name).toEqual("RADIUS");
-    expect(design.symbol_table[sto.RADIUS].value).toEqual(0.4);
+    expect(design.symbol_table[sto.RADIUS].value).toEqual(0.5825642374486647);
     expect(design.symbol_table[sto.THICKNESS].name).toEqual("THICKNESS");
-    expect(design.symbol_table[sto.THICKNESS].value).toEqual(0.04);
+    expect(design.symbol_table[sto.THICKNESS].value).toEqual(0.05814850143495808);
     expect(design.symbol_table[sto.FORCE].name).toEqual("FORCE"); // x vector
-    expect(design.symbol_table[sto.FORCE].value).toEqual(251.32741228718348);
+    expect(design.symbol_table[sto.FORCE].value).toEqual(743.3642427191874);
     expect(design.symbol_table[sto.AREA].name).toEqual("AREA");
-    expect(design.symbol_table[sto.AREA].value).toEqual(0.5026548245743669);
+    expect(design.symbol_table[sto.AREA].value).toEqual(1.0661971414805103);
     expect(design.symbol_table[sto.STRESS].name).toEqual("STRESS");
-    expect(design.symbol_table[sto.STRESS].value).toEqual(2500);
+    expect(design.symbol_table[sto.STRESS].value).toEqual(3492.524417147412);
 
     expect(design.system_controls.ioopt).toEqual(3);
     expect(design.system_controls.maxit).toEqual(100);
@@ -63,7 +72,7 @@ it('search without merit', () => {
     expect(design.system_controls.show_units).toEqual(1);
     expect(design.system_controls.show_violations).toEqual(1);
 
-    expect(design.result.objective_value).toEqual(0);
-    expect(design.result.termination_condition).toEqual("OBJMIN - SHORT SEARCH");
-    expect(design.result.violated_constraint_count).toEqual(0);
+    expect(design.result.objective_value).toEqual(0.14664192222304165);
+    expect(design.result.termination_condition).toEqual("DELMIN 12 ITER.");
+    expect(design.result.violated_constraint_count).toEqual(4);
 });
