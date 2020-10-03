8596e48ced941fd8b8e6defa962fc8f70a096b15
Grumpy In CA <WinTenTester@outlook.com>
2020-10-02 20:28:59 -0700

    400  Improve tutorial & demo sessions for 3.6
    Work in progress but nearing completion.

--- client/src/designtypes/Spring/Compression/demo10.js
+++ client/src/designtypes/Spring/Compression/demo10.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { changeSymbolValue, changeSymbolConstraint, fixSymbolValue, freeSymbolValue, loadInitialState, setSymbolFlag, saveOutputSymbolConstraints, changeLabelsValue, search } from '../../../store/actionCreators';
-import { MAX, CONSTRAINED, FREE_SYMBOL_VALUE } from '../../../store/actionTypes';
+import { changeSymbolValue, changeSymbolConstraint, fixSymbolValue, freeSymbolValue, loadInitialState, setSymbolFlag, resetSymbolFlag, saveOutputSymbolConstraints, changeLabelsValue, search } from '../../../store/actionCreators';
+import { MAX, CONSTRAINED } from '../../../store/actionTypes';
 export const execute = {
     steps: [
         {
@@ -10,6 +10,14 @@ export const execute = {
                     <p>
                     The following example provides the ODOP:Spring solution to a reference book 
                     problem illustrating the design of a compression spring .
+                    </p>
+                    
+                    <p>
+                    While this particular problem struggles with ambiguity on 
+                    issues like end type and stress correction factors,
+                    it provides an opportunity to demonstrate how to handle a "design-to-stress" 
+                    approach that is common in traditional manual calculations. 
+                    More detail on this point appears later in this session.
                     </p>
                     
                     <p>
@@ -29,7 +37,7 @@ export const execute = {
             )
         },
         {
-            title: "Page 02 of 13",
+            title: "Page 02 of 12",
             text: (
                 <React.Fragment>
                     <p>
@@ -39,9 +47,9 @@ export const execute = {
                     &nbsp; &nbsp; <b>SPRING DESIGNER'S HANDBOOK</b> &nbsp; &nbsp; by  Harold Carlson<br />
                     Published by  Marcel Dekker, Inc. &nbsp; &nbsp; 270 Madison Avenue &nbsp; &nbsp; New York, &nbsp; NY &nbsp; &nbsp; 10016<br />
                     <br />
-                    Refer to  Problem 1  on page 179  of the 10th printing  (1978).
+                    Refer to  Problem 6  on page 233  of the 10th printing  (1978).
                     </p>
-                    
+                    <br /><br />
                 </React.Fragment>
             ),
             actions: [
@@ -52,17 +60,18 @@ export const execute = {
             ]
         },
         {
-            title: "Page 03 of 13",
+            title: "Page 03 of 12",
             text: (
                 <React.Fragment>
                     <p>
-                    Design a compression spring such that the following specifications are met:<br />
+                    To briefly restate the problem, our challenge is to 
+                    design a compression spring such that the following specifications are met:<br />
                     <br />
                     material              =  302  stainless<br />
                     maximum force (solid) = 90.0  pounds<br />
                     maximum deflection    =  3.0  inches<br />
-                    outside diameter      =  1.00 inch (arbitrary)<br />
-                    design stress (solid) = 90000 psi<br />
+                    mean diameter      =  1.0 inch &nbsp; (arbitrary value utilized in handbook solution)<br />
+                    design stress (solid) = 80,000 psi &nbsp; (uncorrected)<br />
                     <br />
                     </p>
                     
@@ -76,7 +85,7 @@ export const execute = {
             )
         },
         {
-            title: "Page 04 of 13",
+            title: "Page 04 of 12",
             text: (
                 <React.Fragment>
                     <p>
@@ -84,22 +93,27 @@ export const execute = {
                     we will treat this as a "design-to-stress" problem.  
                     You may wish to review the
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/SpringDesign/advancedSpringOperations" target="_blank" rel="noopener noreferrer">Design To Stress</a>&nbsp; 
-                    section of the documentation (on-line Help entry) for additional information.
+                    and 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/SpringDesign/materials" target="_blank" rel="noopener noreferrer">Materials</a>&nbsp; 
+                    sections of the documentation (on-line Help entries) for additional information. 
                     </p>
                     
-                    <p>Next, the demo session will enter everything we know about the problem. </p>
+                    <p>
+                    This is a good time to take a good look at the existing values.
+                    You can scroll the page down to view the complete set of values. 
+                    Scroll back up in order to use the Next button to continue.
+                    </p>
                     
-                    <p>This is a good time to take a good look at the existing values.
-                    Affected values will update immediately as the demo session enters the changes.</p>
-                    
-                    <p>You can scroll the page down to view the complete set of values. 
-                    Scroll back up in order to use the Next button to continue.</p>
+                    <p>
+                    Next, the demo session will enter everything we know about the problem. 
+                    Affected values will update immediately as the demo session enters the changes.                     
+                    </p>
                     <br /><br />
                 </React.Fragment>
             )
         },
         {
-            title: "Page 05 of 13",
+            title: "Page 05 of 12",
             text: (
                 <React.Fragment>
                     <p>
@@ -108,17 +122,18 @@ export const execute = {
                     </p>
                     
                     <p>
-                    CHANGE Material_Type 302_STAINLESS (sets Density & Torsion Modulus but other properties are specified separately)<br />
-                    CHANGE Prop_Calc_Method 3<br /> 
-                    CHANGE Tensile 193920<br /> 
-                    CHANGE Stress_Lim_Stat 96960<br /> 
+                    CHANGE Material_Type 302_STAINLESS &nbsp; (sets Density & Torsion Modulus but other properties are specified separately)<br />
+                    CHANGE Prop_Calc_Method 3 &nbsp; (allows stress limits to be directly specified; not from materials table)<br /> 
+                    CHANGE Stress_Lim_Stat 96960 &nbsp; (80,000 PSI with stress correction applied) <br /> 
+x                    CHANGE Tensile 193920 &nbsp; (figured so that allowable stress is 50%) <br /> 
+                    CHANGE  End_Type Closed<br />
+                    FIX Force_1 0.0<br /> 
                     FIX Force_2 90.0<br /> 
                     FIX Force_Solid 90.0<br /> 
                     FIX L_Stroke 3.0<br /> 
                     FIX Mean_Dia 1.0<br /> 
                     FIX Stress_Solid 96960<br /> 
-                    FIX Force_1 0.0<br /> 
-                    Free FS_2<br /> 
+                    Free FS_2 &nbsp; (as per Design-to-Stress entry in on-line Help) <br /> 
                     Free FS_Solid<br /> 
                     Free %_Avail_Deflect
                     </p>
@@ -131,20 +146,22 @@ export const execute = {
             actions: [
                 changeSymbolValue("Material_Type",7),
                 changeSymbolValue("Prop_Calc_Method",3),
-                changeSymbolValue("Tensile",193920.0),
+//                changeSymbolValue("Tensile",193920.0),
                 changeSymbolValue("Stress_Lim_Stat",96960),
+                changeSymbolValue("End_Type",3),
+                fixSymbolValue('Force_1', 0.0),
                 fixSymbolValue('Force_2', 90.0),
                 fixSymbolValue('Force_Solid', 90.0),
                 fixSymbolValue('L_Stroke', 3.0),
                 fixSymbolValue('Mean_Dia', 1.0),
                 fixSymbolValue('Stress_Solid', 96960.0),
                 saveOutputSymbolConstraints('FS_2'),
-                freeSymbolValue('FS_2'),
-                fixSymbolValue('Force_1', 0.0)
+//                resetSymbolFlag('FS_2')
+//                freeSymbolValue('FS_2')
             ]
         },
         {
-            title: "Page 06 of 13",
+            title: "Page 06 of 12",
             text: (
                 <React.Fragment>
                     <p>
@@ -162,7 +179,7 @@ export const execute = {
             )
         },
         {
-            title: "Page 07 of 13",
+            title: "Page 07 of 12",
             text: (
                 <React.Fragment>
                     <p>Yes,  A feasible solution is available. Please take a moment to scroll through and view the values.</p>
@@ -174,7 +191,11 @@ export const execute = {
                     is produced any time that the second load uses more
                     than 80 % of available deflection.
                     </p>
-                    <p>There is no warning about buckling so that is not a concern for this design.</p>
+                    <p>
+                    Note the warning about buckling. 
+                    Buckling should not be an immediate concern for a compression spring that will be fitted with drawbars
+                    if utilized in a near static application.
+                    </p>
                 </React.Fragment>
             ),
             actions: [
@@ -182,12 +203,12 @@ export const execute = {
             ]
         },
         {
-            title: "Page 08 of 13",
+            title: "Page 08 of 12",
             text: (
                 <React.Fragment>
                     <p>
                     If you are still on the Report tab,
-                    don't forget to switch back to the tab containing the main page ("Design:").
+                    you may want to switch back to the tab containing the main page ("Design:").
                     </p>
                     
                     <p>
@@ -196,29 +217,24 @@ export const execute = {
                     </p>
                     
                     <p>
-                    Now, in order to conclude this problem,
-                    we want to demonstrate the ODOP Select Size feature. 
-                    Go to the <b>Action : Select Size...</b> menu item
-                    and chose the nearest standard wire diameter.
+                    While the difference is quite small, in order to compare with the handbook result, 
+                    the demo session will impose the handbook value of Wire_Dia. 
                     In summary:
                     <br /><br />
-                    SELECT  Wire_Dia 0.120<br />
+                    CHANGE  Wire_Dia 0.142<br />
                     <br />
-                    In any case, to guarantee the expected outcome, the demo session will
-                    to use a FIX to impose the expected value of Wire_Dia.
                     Look for the new value of Wire_Dia on the next page.
                     </p>
                 </React.Fragment>
             )
         },
         {
-            title: "Page 09 of 13",
+            title: "Page 09 of 12",
             text: (
                 <React.Fragment>
                     <p>
                     Now that there has been a slight change in wire diameter, another search
                     will be required to make corresponding adjustments in the other parameters.
-                    For this problem, only the number of coils remains to be determined.
                     </p>
                     
                     <p>
@@ -229,11 +245,11 @@ export const execute = {
                 </React.Fragment>
             ),
             actions: [
-                fixSymbolValue('Wire_Dia', 0.120)
+                fixSymbolValue('Wire_Dia', 0.142)
             ]
         },
         {
-            title: "Page 10 of 13",
+            title: "Page 10 of 12",
             text: (
                 <React.Fragment>
                     <p>
@@ -248,7 +264,6 @@ export const execute = {
                     
                     <p>
                     Next we'll make a detailed comparison with the handbook results.
-                    Click Next to see the handbook inputs entered into the appropriate ODOP:Spring variables.
                     </p>
                     
                     <p>
@@ -260,45 +275,32 @@ export const execute = {
             ]
         },
         {
-            title: "Page 11 of 13",
-            text: (
-                <React.Fragment>
-                    <p>
-                    If you are still on a Report tab,
-                    don't forget to switch back to the tab containing the main page ("Design:").
-                    </p>
-                    
-                    <p>
-                    The demo session has now imposed the values:<br />
-                    <br />
-                    </p>
-                    <br />
-                </React.Fragment>
-            ),
-            actions: [
-            ]
-        },
-        {
-            title: "Page 12 of 13",
+            title: "Page 11 of 12",
             text: (
                 <React.Fragment>
                     <p>
                     The handbook results:<br />
-                    &nbsp; &nbsp;  solid height = 1.57 in.<br />
-                    &nbsp; &nbsp;  stress correction factor = 1.21 &nbsp; (see Report 2)<br />
-                    &nbsp; &nbsp;  stress at solid      = 104000  psi  (corrected)<br />
-                    &nbsp; &nbsp;  stress at 60 lb load =  92900  psi  (corrected)<br />
-                    &nbsp; &nbsp;  spring index = 7.3
+                    &nbsp; &nbsp;  wire diameter = 0.142 in.<br />
+                    &nbsp; &nbsp;  stress at solid = 80,000  psi &nbsp;  (uncorrected)<br />
+                    &nbsp; &nbsp;  stress at solid = 96,960  psi &nbsp;  (corrected)<br />
+                    &nbsp; &nbsp;  number of active coils = 16.95 &nbsp; (rounded to 17)
                     </p>
                     
                     <p>
                     The results are in very good agreement.
                     </p>
+                    
+                    <p>
+                    While the ambiguity over stress correction factor and end type provided a few
+                    challenges in formulating this demonstration session, 
+                    the real objective of demonstrating how ODOP:Spring can handle a "design-to-stess" 
+                    problem has been achieved.
+                    </p>
                 </React.Fragment>
             )
         },
         {
-            title: "Page 13 of 13 (last page)",
+            title: "Page 12 of 12 (last page)",
             text: (
                 <React.Fragment>
                     <p>
