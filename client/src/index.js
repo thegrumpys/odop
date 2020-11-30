6777877224eb8efaedbdc5e2669eef214fb7a461
Brian Watt <bwatt@1fifoto.com>
2020-11-29 20:00:43 -0600

    438 Migrate to latest version of Okta React
    Add in "autosave" restore to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -38,23 +38,42 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = composeEnhancers(applyMiddleware(/* loggerMiddleware, */dispatcher));
 const store = createStore(reducers, {user: null}, middleware);
 if (typeof(Storage) !== "undefined" && localStorage.getItem("redirect") !== null) {
-//    console.log('In index.js restore auto save')
+//    console.log('In index.js restore "redirect" file')
     store.dispatch(restoreAutoSave("redirect"));
     var design = store.getState();
 //    console.log('In index.js design=',design);
     var name = design.name;
-    // Migrate model version here.
+    // Migrate model version
     var { migrate } = require('./designtypes/'+design.model.type+'/migrate.js'); // Dynamically load migrate
     var migrated_design = migrate(design.model);
     if (migrated_design.jsontype === "ODOP") {
         store.dispatch(load({name: name, model: migrated_design}));
         store.dispatch(deleteAutoSave("redirect"));
     } else {
-        displayError('Invalid JSON type, function ignored');
+        console.error('Invalid JSON type, function ignored');
     }
 } else {
-//    console.log('In index.js load initial state');
-    store.dispatch(loadInitialState(config.design.type, 'US'));
-    store.dispatch(changeName(config.design.name));
+    if (typeof(Storage) !== "undefined" && localStorage.getItem("autosave") !== null) {
+//        console.log('In index.js restore "autosave" file')
+        store.dispatch(restoreAutoSave());
+        var design = store.getState();
+//        console.log('In index.js design=',design);
+        var name = design.name;
+        // Migrate model version
+        var { migrate } = require('./designtypes/'+design.model.type+'/migrate.js'); // Dynamically load migrate
+        var migrated_design = migrate(design.model);
+        if (migrated_design.jsontype === "ODOP") {
+            store.dispatch(load({name: name, model: migrated_design}));
+            store.dispatch(deleteAutoSave());
+            console.warn('Autosave file restored after interruption. Use FileSave, FileSaveAs or FileExport to save it permanently');
+        } else {
+            console.error('Invalid JSON type, function ignored');
+        }
+    } else {
+//        console.log('In index.js load initial state');
+        store.dispatch(loadInitialState(config.design.type, 'US'));
+        store.dispatch(changeName(config.design.name));
+    }
 }
+
 ReactDOM.render(<div id="root2"><Spinner /><ErrorModal /><Provider store={store}><Router><Routes /></Router></Provider></div>, document.getElementById('root'));
