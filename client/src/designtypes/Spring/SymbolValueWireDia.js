7c3b8a998e72293a7436695e97e173cc5d91c96c
Brian Watt <bwatt@1fifoto.com>
2020-12-13 23:01:24 -0600

    450 Calculator view - select Wire_Dia from table

---
+++ client/src/designtypes/Spring/SymbolValueWireDia.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { InputGroup, Form, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { MIN, MAX, FIXED, CONSTRAINED, FDCL } from '../../store/actionTypes';
+import { changeSymbolValue, fixSymbolValue } from '../../store/actionCreators';
+import { evaluateConstraintValue } from '../../store/middleware/evaluateConstraint';
+import * as mo from './mat_ips_offsets';
+
+/*eslint no-extend-native: ["error", { "exceptions": ["Number"] }]*/
+Number.prototype.toODOPPrecision = function() {
+    var value = this.valueOf();
+    var odopValue;
+    if (value < 10000.0 || value >= 1000000.0)
+         odopValue = value.toPrecision(4);
+    else odopValue = value.toFixed(0);
+    return odopValue;
+};
+     
+class SymbolValueWireDia extends Component {
+    
+    constructor(props) {
+//        console.log('In SymbolValueWireDia.constructor props=',props);
+        super(props);
+        this.onSelect = this.onSelect.bind(this);
+    }
+    
+    onSelect(event) {
+//        console.log('In SymbolValueWireDia.onSelect event.target.value=',event.target.value);
+        var wire_dia = parseFloat(event.target.value);
+//        console.log('In SymbolValueWireDia.onSelect wire_dia=',wire_dia);
+        this.props.changeSymbolValue(this.props.element.name,wire_dia);
+        this.props.fixSymbolValue(this.props.element.name);
+    }
+  
+    render() {
+//        console.log('In SymbolValueWireDia.render this=', this);
+        
+//        console.log('In SymbolValueWireDia.render ../' + this.props.type + '/symbol_table_offsets.js');
+        var o = require('../'+this.props.type+'/symbol_table_offsets.js'); // Dynamically load table
+//        console.log('In SymbolValueWireDia.render o =', o);
+
+        // Find size name, load size table, and get wire diameter value
+//        console.log('In SymbolValueWireDia.render this.props.symbol_table[o.Material_File].value =', this.props.symbol_table[o.Material_File].value);
+        if (this.props.symbol_table[o.Material_File].value === "mat_SI.json")
+            var m_tab = require('./mat_SI.json');
+        else
+            var m_tab = require('./mat_ips.json');
+//        console.log('In SymbolValueWireDia.render m_tab =', m_tab);
+        var i = this.props.symbol_table[o.Material_Type].value;
+//        console.log('In SymbolValueWireDia.render i=',i);
+        var size_name = m_tab[i][mo.siznam];
+//        console.log('In SymbolValueWireDia.render size_name=',size_name);
+        var size_table = require('./'+size_name+'.json'); // Dynamically load table
+//        console.log('In SymbolValueWireDia.render size_table=',size_table);
+        const needle = this.props.element.value;
+//        console.log('In SymbolValueWireDia.render needle=',needle);
+        const default_value = size_table.find((element,index) => {
+            if (index > 0) {
+                if (element < needle) return false
+                else return true;
+            } else {
+                return false;
+            }
+        });
+//        console.log('In SymbolValueWireDia.render default_value=',default_value);
+
+        var value_class = 'text-right ';
+        var value_tooltip;
+        if ((this.props.element.lmin & CONSTRAINED && this.props.element.vmin > 0.0) || (this.props.element.lmax & CONSTRAINED && this.props.element.vmax > 0.0)) {
+            if (this.props.objective_value > 4*this.props.system_controls.objmin) {
+                value_tooltip = "CONSTRAINT VIOLATION: Value outside the constraint range from "+evaluateConstraintValue(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin).toODOPPrecision()+" to "+evaluateConstraintValue(this.props.symbol_table,this.props.element.lmax,this.props.element.cmax).toODOPPrecision();
+                value_class += "text-not-feasible";
+            } else if (this.props.objective_value > this.props.system_controls.objmin) {
+                value_tooltip = "CONSTRAINT VIOLATION: Value outside the constraint range from "+evaluateConstraintValue(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin).toODOPPrecision()+" to "+evaluateConstraintValue(this.props.symbol_table,this.props.element.lmax,this.props.element.cmax).toODOPPrecision();
+                value_class += "text-approaching-feasible";
+            } else if (this.props.objective_value > 0.0) {
+                value_tooltip = "CONSTRAINT VIOLATION: Value outside the constraint range from "+evaluateConstraintValue(this.props.symbol_table,this.props.element.lmin,this.props.element.cmin).toODOPPrecision()+" to "+evaluateConstraintValue(this.props.symbol_table,this.props.element.lmax,this.props.element.cmax).toODOPPrecision();
+                value_class += "text-feasible";
+            } else {
+                value_class += "text-strictly-feasible";
+            }
+        }
+        return (
+            <React.Fragment>
+                <td className={"align-middle " + this.props.className}>
+                    <InputGroup>
+                        <Form.Control as="select" disabled={!this.props.element.input} value={default_value[0]} onChange={this.onSelect}>
+                            {size_table.map((value, index) =>
+                                index > 0 ? <option key={index} value={value[0]}>{value[0]}</option> : ''
+                            )}
+                        </Form.Control>
+                    </InputGroup>
+                </td>
+            </React.Fragment>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    type: state.model.type,
+    symbol_table: state.model.symbol_table,
+    system_controls: state.model.system_controls,
+    objective_value: state.model.result.objective_value
+});
+
+const mapDispatchToProps = {
+    changeSymbolValue: changeSymbolValue,
+    fixSymbolValue: fixSymbolValue,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SymbolValueWireDia);
