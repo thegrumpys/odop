8bb200128bea0af3e7d2818f0b594fa91b052135
Brian Watt <bwatt@1fifoto.com>
2020-12-15 12:38:15 -0600

    449 Add Graphical Feasibility Indicator to Results Table

--- client/src/designtypes/Spring/Compression/report.js
+++ client/src/designtypes/Spring/Compression/report.js
@@ -5,6 +5,7 @@ import Calculator from "./Calculator"
 import Report1 from "./Report1"
 import Report2 from "./Report2"
 import Report3 from "./Report3"
+import Graph from "../../../components/Graph"
 
 export function getReportNames() {
     // Note: report names must match cases in switch statement below
@@ -13,5 +14,6 @@ export function getReportNames() {
         { name: 'Report 1 (mini)', component: <Report1 /> },
         { name: 'Report 2 (pre-set)', component: <Report2 /> },
         { name: 'Report 3 (maxi)', component: <Report3 /> },
+        { name: 'Graph', component: <Graph /> },
     ];
 }
