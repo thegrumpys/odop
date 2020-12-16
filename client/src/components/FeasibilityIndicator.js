19c6e6c275c0f3d812bd7c9681e9366375a5bad1
Brian Watt <bwatt@1fifoto.com>
2020-12-16 17:25:59 -0600

    449 Add Graphical Feasibility Indicator to Results Table
    Adjust display based upon comments in Github issue.

--- client/src/components/FeasibilityIndicator.js
+++ client/src/components/FeasibilityIndicator.js
@@ -3,24 +3,35 @@ import { Row, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 class FeasibilityIndicator extends Component {
-    static defaultProps = { width: 222, height: 32 };
+    static defaultProps = { width: 230, height: 32 };
     
     render() {
 //        console.log('In FeasibilityIndicator.render this=', this);
-        const xOrigin = 8;
+        const triangleWidth = 16;
+        const triangleHeight = 16;
+        const blackWidth = 4;
+        const greenWidth = 55;
+        const orangeWidth = 55;
+        const redWidth = 100;
+        const boxY = 8;
+        const boxHeight = 16;
+        const xOrigin = triangleWidth+blackWidth;
+        const yOrigin = 10;
         var x;
         if (this.props.objective_value <= 0) { // Black
-            x = -2; // Center of black
+            x = -blackWidth/2; // Center of black
         } else if (this.props.objective_value > 4*this.props.system_controls.objmin) { // Red
             x = this.props.objective_value-4*this.props.system_controls.objmin; // Shift to Zero
-            x = Math.log10(x); // log10 
-            if (x < 0) x = 0; // set low limit 10**0
-            else if (x > 15) x = 15; // set high limit 10**15
-            x = (x/15)*70+140; // Scale and shift it
+            x = Math.log10(x); // log10
+            var lowBound = Math.log10(4*this.props.system_controls.objmin); // 0.000200 is approx. 10**(-3.7)
+            var highBound = 8 // 10**8
+            if (x < lowBound) x = lowBound;
+            else if (x > highBound) x = highBound;
+            x = ((x-lowBound)/(highBound-lowBound))*redWidth + greenWidth + orangeWidth; // Scale and shift it
         } else if (this.props.objective_value > this.props.system_controls.objmin) { // Orange
-            x = ((this.props.objective_value-this.props.system_controls.objmin)/(4*this.props.system_controls.objmin-this.props.system_controls.objmin))*70+70;
+            x = ((this.props.objective_value-this.props.system_controls.objmin)/(4*this.props.system_controls.objmin-this.props.system_controls.objmin))*orangeWidth + greenWidth;
         } else { // Green
-            x = (this.props.objective_value/this.props.system_controls.objmin)*70+0;
+            x = (this.props.objective_value/this.props.system_controls.objmin)*greenWidth;
         }
 //        console.log('In FeasibilityIndicator.render this.props.objective_value=', this.props.objective_value,'x=',x);
         return (
@@ -29,11 +40,11 @@ class FeasibilityIndicator extends Component {
                     <b>Status</b>
                 </OverlayTrigger>
                 <svg width={this.props.width} height={this.props.height}>
-                    <polygon points={[[xOrigin+x,8], [xOrigin+x+4,0], [xOrigin+x-4,0]]} fill="#05a4e8" />
-                    <rect x={xOrigin-4} y={8} width={4} height={16} fill="black" />
-                    <rect x={xOrigin} y={8} width={70} height={16} fill="#28a745" />
-                    <rect x={xOrigin+70} y={8} width={70} height={16} fill="#fd7e14" />
-                    <rect x={xOrigin+140} y={8} width={70} height={16} fill="#dc3545" />
+                    <rect x={xOrigin-blackWidth} y={yOrigin+boxY} width={blackWidth} height={boxHeight} fill="black" />
+                    <rect x={xOrigin} y={yOrigin+boxY} width={greenWidth} height={boxHeight} fill="#28a745" />
+                    <rect x={xOrigin+greenWidth} y={yOrigin+boxY} width={orangeWidth} height={boxHeight} fill="#fd7e14" />
+                    <rect x={xOrigin+greenWidth+orangeWidth} y={yOrigin+boxY} width={redWidth} height={boxHeight} fill="#dc3545" />
+                    <polygon points={[[xOrigin+x, yOrigin+triangleHeight], [xOrigin+triangleWidth/2+x, yOrigin], [xOrigin-triangleWidth/2+x, yOrigin]]} fill="#05a4e8" />
                 </svg>
             </React.Fragment>
         );
