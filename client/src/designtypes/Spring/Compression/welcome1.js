a0addaab59eb36e328100319cf4fabc8924e81c9
Grumpy In CA <WinTenTester@outlook.com>
2020-12-17 21:09:08 -0800

    459  Develop "Welcome" execute sessions
    Work in progress.
    Merged master into this branch.

--- client/src/designtypes/Spring/Compression/welcome1.js
+++ client/src/designtypes/Spring/Compression/welcome1.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { changeSymbolValue, changeSymbolConstraint, fixSymbolValue, loadInitialState, setSymbolFlag, saveOutputSymbolConstraints, changeLabelsValue, search } from '../../../store/actionCreators';
-import { MAX, CONSTRAINED } from '../../../store/actionTypes';
+import { changeSymbolValue, loadInitialState, changeLabelsValue } from '../../../store/actionCreators';
 export const execute = {
     steps: [
         {
@@ -8,27 +7,19 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    Welcome !
+                    Welcome to the ODOP:Spring web app!
+                    The compression spring Calculator view is below. 
                     </p>
                     
                     <p>
+                    The inputs to the Calculator are the white entry fields.
+                    Once input values are in place, calculation results will appear.
                     </p>
-                    
+
                     <p>
-                    Setting variables to Number.NaN is next.
+                    Push the blue "Next" button to show the next page of instructions, hints and tips. 
+                    Push the darker gray "Exit" button in the upper right to continue without this light green panel. 
                     </p>
-                    <br />
-                </React.Fragment>
-            )
-        },
-        {
-            title: "Page 02 of 07",
-            text: (
-                <React.Fragment>
-                    <p>
-                    Please review the test setting Independent Variables to Number.NaN.
-                    </p>
-                    
                 </React.Fragment>
             ),
             actions: [
@@ -39,71 +30,57 @@ export const execute = {
                 changeSymbolValue("Coils_T", Number.NaN),
                 changeSymbolValue("Force_1", Number.NaN),
                 changeSymbolValue("Force_2", Number.NaN),
-                changeLabelsValue([{name: 'COMMENT', value: 'Welcome script test'}])
+                changeLabelsValue([{name: 'COMMENT', value: 'Welcome - Compression spring - Calculator view'}])
             ]
         },
         {
-            title: "Page 03 of 07",
+            title: "Page 02 of 04",
             text: (
                 <React.Fragment>
                     <p>
-                    Page 3
-                    <br />
+                    While the ODOP:Spring Calculator view does not provide direct control over constraints,
+                    it does display the multi-colored Feasibility Status indicator as feedback on the viability of the current design.
+                    For more information about design Feasibility and about the Feasibility Status indicator, follow these links: 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/feasibility" target="_blank" rel="noopener noreferrer">Feasiblity</a>,&nbsp; 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/designSituations" target="_blank" rel="noopener noreferrer">Design Situations</a>&nbsp; and 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#feasibleRegion" target="_blank" rel="noopener noreferrer">Feasible Region</a>.&nbsp; 
+                    </p>
+                    
+                    <p>
+                    Calculator view provides tables of standard wire diameters that depend on the selected Material_Type and units (U.S., metric). 
+                    Choose a standard wire diameter by selecting the Wire_Dia field and scrolling to the desired value. 
+                    This will leave the selected value in FIXed status.
+                    FIXed status causes the Search to achieve a specified fixed value for the associated variable.
+                    FREE status allows the search to manipulate the associated variable to achieve a feasible design.
+                    For more information, see: 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#fix" target="_blank" rel="noopener noreferrer">FIX</a>&nbsp; 
+                    and various tutorial (tutorTour) and demo (demo, demo1) sessions.
+                    For more information on these sessions, see:
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/tutordemo" target="_blank" rel="noopener noreferrer">Tutorial and Demo</a>&nbsp; 
                     </p>
                 </React.Fragment>
             )
         },
         {
-            title: "Page 04 of 07",
+            title: "Page 03 of 04",
             text: (
                 <React.Fragment>
-                Page 4
-                    <br /><br />
+                    <p>
+                    ODOP:Spring's Advanced view provides access to the full set of features including
+                    constraints and FIX / Free.
+                    Use the <b>View : Advanced</b> menu to switch into Advanced view.
+                    </p>
+                    
+                    <p>
+                    The <b>File : Open</b> menu provides the ability to open and operate with existing designs of 
+                    other spring types (extension, torsion) and units (U.S., metric).
+                   </p>
+                    <br />
                 </React.Fragment>
             )
         },
         {
-            title: "Page 05 of 07",
-            text: (
-                <React.Fragment>
-                    <p>
-                    Page 5 has set a bunch of stuff.
-                    <br />
-                    </p>
-                    <p>
-                    </p>
-                </React.Fragment>
-            ),
-            actions: [
-                fixSymbolValue('OD_Free', 1.0),
-                fixSymbolValue('L_Free', 3.25),
-                fixSymbolValue('L_2', 1.75),
-                fixSymbolValue('Force_1', 0.0),
-                fixSymbolValue('Force_2', 60.0),
-                saveOutputSymbolConstraints('L_Solid'),
-                setSymbolFlag('L_Solid', MAX, CONSTRAINED),
-                changeSymbolConstraint('L_Solid', MAX, 1.625),
-                changeSymbolValue("End_Type",4),
-                changeSymbolValue("Material_Type",3)
-            ]
-        },
-        {
-            title: "Page 06 of 07",
-            text: (
-                <React.Fragment>
-                    <p>
-                    Page 6
-                    </p>
-
-                    <p>
-                    &nbsp;
-                    <br />
-                    </p>
-                </React.Fragment>
-            )
-        },
-        {
-            title: "Page nn of nn (last page)",
+            title: "Page 04 of 04 (last page)",
             text: (
                 <React.Fragment>
                     <p>
