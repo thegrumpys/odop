b72c27417bf20d92c845aa892c9db31eb8965c09
Grumpy In CA <WinTenTester@outlook.com>
2020-12-18 14:13:38 -0800

    459  Develop "Welcome" execute sessions
    Ready for review & discussion.
    Previously merged master into this branch.

--- client/src/designtypes/Spring/Compression/welcome1.js
+++ client/src/designtypes/Spring/Compression/welcome1.js
@@ -12,13 +12,13 @@ export const execute = {
                     </p>
                     
                     <p>
-                    The inputs to the Calculator are the white entry fields.
+                    The white entry fields are inputs to the Calculator.
                     Once input values are in place, calculation results will appear.
                     </p>
 
                     <p>
-                    Push the blue "Next" button to show the next page of instructions, hints and tips. 
-                    Push the darker gray "Exit" button in the upper right to continue without this light green panel. 
+                    Push the blue "Next" button in the upper right to show the next page of instructions, hints and tips. 
+                    Push the darker gray "Exit" button to continue without this light green panel. 
                     </p>
                 </React.Fragment>
             ),
@@ -38,16 +38,27 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    While the ODOP:Spring Calculator view does not provide direct control over constraints,
+                    The ODOP:Spring Calculator view has forces as inputs and deflections as calculation outputs. 
+                    The Advanced view supports a variety of alternative approaches.
+                    </p>
+                    
+                    <p>
+                    While Calculator view does not provide direct control over constraints,
                     it does display the multi-colored Feasibility Status indicator as feedback on the viability of the current design.
                     For more information about design Feasibility and about the Feasibility Status indicator, follow these links: 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/feasibility" target="_blank" rel="noopener noreferrer">Feasiblity</a>,&nbsp; 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/designSituations" target="_blank" rel="noopener noreferrer">Design Situations</a>&nbsp; and 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#feasibleRegion" target="_blank" rel="noopener noreferrer">Feasible Region</a>.&nbsp; 
-                    </p>
-                    
+                   </p>
+                </React.Fragment>
+            )
+        },
+        {
+            title: "Page 03 of 04",
+            text: (
+                <React.Fragment>
                     <p>
-                    Calculator view provides tables of standard wire diameters that depend on the selected Material_Type and units (U.S., metric). 
+                    Calculator view provides a table of standard wire diameters that depends on the selected Material_Type and units (U.S., metric). 
                     Choose a standard wire diameter by selecting the Wire_Dia field and scrolling to the desired value. 
                     This will leave the selected value in FIXed status.
                     FIXed status causes the Search to achieve a specified fixed value for the associated variable.
@@ -58,23 +69,9 @@ export const execute = {
                     For more information on these sessions, see:
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/tutordemo" target="_blank" rel="noopener noreferrer">Tutorial and Demo</a>&nbsp; 
                     </p>
-                </React.Fragment>
-            )
-        },
-        {
-            title: "Page 03 of 04",
-            text: (
-                <React.Fragment>
-                    <p>
-                    ODOP:Spring's Advanced view provides access to the full set of features including
-                    constraints and FIX / Free.
-                    Use the <b>View : Advanced</b> menu to switch into Advanced view.
-                    </p>
                     
                     <p>
-                    The <b>File : Open</b> menu provides the ability to open and operate with existing designs of 
-                    other spring types (extension, torsion) and units (U.S., metric).
-                   </p>
+                    </p>
                     <br />
                 </React.Fragment>
             )
@@ -84,9 +81,27 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    Last page.
+                    ODOP:Spring's Advanced view provides access to the full set of features including
+                    constraints and FIX / FREE.
+                    Use the <b>View : Advanced</b> menu to switch into Advanced view.
                     </p>
                     
+                    <p>
+                    The <b>File : Open</b> menu provides the ability to open and operate with existing designs of 
+                    other spring types (extension, torsion) and units (U.S., metric).
+                   </p>
+                    
+                    <p>
+                    Push the gray "Exit" button in the upper right to continue.
+                    Enter input values in the white fields.
+                    View the corresponding Calculator outputs.
+                    Switch to Advanced mode to utilize the full capabilities of ODOP:Spring.
+                   </p>
+                    
+                    <p>
+                    We welcome your feedback.  
+                    &nbsp;<a href="https://thegrumpys.github.io/odop/About/ContactUs" target="_blank" rel="noopener noreferrer">Contact Us.</a>&nbsp; 
+                    </p>
                     <br />
                 </React.Fragment>
             )
