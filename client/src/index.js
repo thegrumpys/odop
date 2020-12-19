21c9dbc38af10001941ce26c7db8332b37d11daf
Brian Watt <bwatt@1fifoto.com>
2020-12-19 08:34:14 -0600

    461 Rename components in preparation for branch 438

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { dispatcher } from './store/middleware/dispatcher';
 import { reducers } from './store/reducers';
 import { Provider } from 'react-redux'
-import FEApp from './components/FEApp';
+import Routes from './components/Routes';
 import './odop.css';
 
 //function loggerMiddleware({ getState }) {
@@ -35,4 +35,4 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = composeEnhancers(applyMiddleware(/* loggerMiddleware, */dispatcher));
 const store = createStore(reducers, {user: null, name: null, model: null}, middleware);
 
-ReactDOM.render(<div id="root2"><Spinner /><ErrorModal /><Provider store={store}><FEApp /></Provider></div>, document.getElementById('root'));
+ReactDOM.render(<div id="root2"><Spinner /><ErrorModal /><Provider store={store}><Routes /></Provider></div>, document.getElementById('root'));
