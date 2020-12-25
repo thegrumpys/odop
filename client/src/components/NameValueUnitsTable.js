39f493d4d4b1628daf6364a41e3ad2fed60cf225
Brian Watt <bwatt@1fifoto.com>
2020-12-24 22:03:57 -0600

    463 Improve FIX header and tooltip

--- client/src/components/NameValueUnitsTable.js
+++ client/src/components/NameValueUnitsTable.js
@@ -27,8 +27,8 @@ class NameValueUnitsTable extends Component {
                             </OverlayTrigger>
                             </th>
                             <th className="text-center" colSpan="2" id="ValueTitle">
-                                <OverlayTrigger placement="top" overlay={<Tooltip>Current values <br /> (Check box at right to FIX)</Tooltip>}>
-                                    <span>Value (Fix)</span>
+                                <OverlayTrigger placement="top" overlay={<Tooltip>Current values.<br />(Checkbox at right to FIX. Unchecked to FREE)</Tooltip>}>
+                                    <span>Value (&nbsp;<i class="far fa-check-square"></i>&nbsp;Fix&nbsp;-&nbsp;<i class="far fa-square"></i>&nbsp;Free&nbsp;)</span>
                                 </OverlayTrigger>
                             </th>
                             <th className={"text-left " + (this.props.system_controls.show_units ? "" : "d-none")} id="UnitsTitle">
