d158d3b2f486a92431aa94cbb1c2bf90e7c325d2
Grumpy In CA <WinTenTester@outlook.com>
2020-12-30 12:51:07 -0800

    459  Develop "Welcome" execute sessions
    added AutoSave at start
    more wording tweaks

--- client/src/designtypes/Spring/Compression/welcome1.js
+++ client/src/designtypes/Spring/Compression/welcome1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { changeSymbolValue, loadInitialState, changeLabelsValue } from '../../../store/actionCreators';
+import { changeSymbolValue, loadInitialState, changeLabelsValue, saveAutoSave } from '../../../store/actionCreators';
 export const execute = {
     steps: [
         {
@@ -8,22 +8,24 @@ export const execute = {
                 <React.Fragment>
                     <p>
                     Welcome to the ODOP:Spring web app!
-                    The compression spring Calculator view is below. 
+                    Calculator view is below. 
                     </p>
                     
                     <p>
                     The white entry fields below are inputs to the Calculator.
                     Once input values are entered, calculation results will appear
-                    in the currently empty gray fields.
+                    in the light gray fields.
                     </p>
 
                     <p>
                     Push the blue "Next" button in the upper right to show the next page of instructions, hints and tips. 
                     Push the darker gray "Exit" button to close this light green panel. 
                     </p>
+                    <br />
                 </React.Fragment>
             ),
             actions: [
+                saveAutoSave(),
                 loadInitialState('Spring/Compression'),
                 changeSymbolValue("OD_Free", Number.NaN),
                 changeSymbolValue("Wire_Dia", Number.NaN),
@@ -52,6 +54,7 @@ export const execute = {
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/designSituations" target="_blank" rel="noopener noreferrer">Design Situations</a>&nbsp; and 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#feasibleRegion" target="_blank" rel="noopener noreferrer">Feasible Region</a>.&nbsp; 
                    </p>
+                    <br />
                 </React.Fragment>
             )
         },
@@ -60,9 +63,10 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    Calculator view provides a table of commonly available wire diameters that depends on the selected Material_Type and units (U.S., metric). 
-                    Choose a standard wire diameter by selecting the Wire_Dia field and scrolling to the desired value. 
-                    The selected value is marked with FIXed status so that it is not manipulated by the ODOP Search feature (<b>Action : Search</b> menu).
+                    Calculator view provides a list of commonly available wire diameters that depends on the selected Material_Type and units (U.S., metric). 
+                    Choose a standard wire diameter by selecting the Wire_Dia input field and scrolling to the desired value. 
+                    After selection the value is automatically marked with FIXed status so that it is not manipulated by 
+                    the ODOP Search feature (<b>Action : Search</b> menu).
                     </p>
                     
                     <p>
@@ -70,7 +74,6 @@ export const execute = {
                     Hover the cursor (tap a touch screen) to learn about these elements.
                     Additional insights are available in the on-line Help plus Tutorial and Demo sessions.
                     </p>
-                    <br />
                 </React.Fragment>
             )
         },
@@ -79,11 +82,9 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    </p>
-                    
-                    <p>
-                    The <b>File : Open</b> menu provides the ability to open and operate with existing designs of 
-                    other spring types (extension, torsion) and units (U.S., metric).
+                    The <b>File : Open</b> menu above provides the ability to open existing designs of 
+                    supported spring types (compression, extension & torsion) and units (U.S., metric). 
+                    These system-provided designs can be a starting point for new custom designs. 
                    </p>
                     
                     <p>
@@ -99,7 +100,6 @@ export const execute = {
                     We welcome your feedback.  
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/About/ContactUs" target="_blank" rel="noopener noreferrer">Contact Us.</a>&nbsp; 
                     </p>
-                    <br />
                 </React.Fragment>
             )
         }
