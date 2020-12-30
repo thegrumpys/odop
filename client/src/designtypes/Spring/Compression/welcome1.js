e8f17a8ea4f6f2afd07b714a848fad35ee0ccec0
Grumpy In CA <WinTenTester@outlook.com>
2020-12-29 20:51:08 -0800

    459  Develop "Welcome" execute sessions
    Improved words.

--- client/src/designtypes/Spring/Compression/welcome1.js
+++ client/src/designtypes/Spring/Compression/welcome1.js
@@ -12,13 +12,14 @@ export const execute = {
                     </p>
                     
                     <p>
-                    The white entry fields are inputs to the Calculator.
-                    Once input values are in place, calculation results will appear.
+                    The white entry fields below are inputs to the Calculator.
+                    Once input values are entered, calculation results will appear
+                    in the currently empty gray fields.
                     </p>
 
                     <p>
                     Push the blue "Next" button in the upper right to show the next page of instructions, hints and tips. 
-                    Push the darker gray "Exit" button to continue without this light green panel. 
+                    Push the darker gray "Exit" button to close this light green panel. 
                     </p>
                 </React.Fragment>
             ),
@@ -38,14 +39,15 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    The ODOP:Spring Calculator view has forces as inputs and deflections as calculation outputs. 
-                    The Advanced view supports a variety of alternative approaches.
+                    The ODOP:Spring Calculator view is based on applied load.
+                    Forces are calculation inputs. Deflections are outputs. 
+                    Alternative calculation approaches are supported by other views.
+                    Use the <b>View</b> menu above to switch views.
                     </p>
                     
                     <p>
-                    While Calculator view does not provide direct control over constraints,
-                    it does display the multi-colored Feasibility Status indicator as feedback on the viability of the current design.
-                    For more information about design Feasibility and about the Feasibility Status indicator, follow these links: 
+                    ODOP displays the multi-colored Feasibility Status indicator as feedback on the viability of the current design.
+                    For more information about design feasibility and about the Feasibility Status indicator, follow these links: 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/feasibility" target="_blank" rel="noopener noreferrer">Feasiblity</a>,&nbsp; 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/designSituations" target="_blank" rel="noopener noreferrer">Design Situations</a>&nbsp; and 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#feasibleRegion" target="_blank" rel="noopener noreferrer">Feasible Region</a>.&nbsp; 
@@ -58,19 +60,15 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    Calculator view provides a table of standard wire diameters that depends on the selected Material_Type and units (U.S., metric). 
+                    Calculator view provides a table of commonly available wire diameters that depends on the selected Material_Type and units (U.S., metric). 
                     Choose a standard wire diameter by selecting the Wire_Dia field and scrolling to the desired value. 
-                    This will leave the selected value in FIXed status.
-                    FIXed status causes the Search to achieve a specified fixed value for the associated variable.
-                    FREE status allows the search to manipulate the associated variable to achieve a feasible design.
-                    For more information, see: 
-                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#fix" target="_blank" rel="noopener noreferrer">FIX</a>&nbsp; 
-                    and various tutorial (tutorTour) and demo (demo, demo1) sessions.
-                    For more information on these sessions, see:
-                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/tutordemo" target="_blank" rel="noopener noreferrer">Tutorial and Demo</a>&nbsp; 
+                    The selected value is marked with FIXed status so that it is not manipulated by the ODOP Search feature (<b>Action : Search</b> menu).
                     </p>
                     
                     <p>
+                    Tooltips are available for many elements of the Calculator view.
+                    Hover the cursor (tap a touch screen) to learn about these elements.
+                    Additional insights are available in the on-line Help plus Tutorial and Demo sessions.
                     </p>
                     <br />
                 </React.Fragment>
@@ -81,9 +79,6 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    ODOP:Spring's Advanced view provides access to the full set of features including
-                    constraints and FIX / FREE.
-                    Use the <b>View : Advanced</b> menu to switch into Advanced view.
                     </p>
                     
                     <p>
@@ -92,15 +87,15 @@ export const execute = {
                    </p>
                     
                     <p>
-                    Push the gray "Exit" button in the upper right to continue.
+                    Push the gray "Exit" button in the upper right to close this light green panel.
                     Enter input values in the white fields.
                     View the corresponding Calculator outputs.
-                    Switch to Advanced mode to utilize the full capabilities of ODOP:Spring.
+                    Switch to other views to utilize the full capabilities of ODOP:Spring.
                    </p>
                     
                     <p>
                     We welcome your feedback.  
-                    &nbsp;<a href="https://thegrumpys.github.io/odop/About/ContactUs" target="_blank" rel="noopener noreferrer">Contact Us.</a>&nbsp; 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/About/ContactUs" target="_blank" rel="noopener noreferrer">Contact Us.</a>&nbsp; 
                     </p>
                     <br />
                 </React.Fragment>
