b7ca263b3c37c3a468adca7367617b289af0bc8f
Brian Watt <bwatt@1fifoto.com>
2021-01-05 20:59:05 -0600

    481 Create Action Execute files to make Startup, Startup_Metric, ...

---
+++ client/src/designtypes/Spring/Torsion/mkStartup_Metric.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    loadInitialState,
+    changeName,
+    changeLabelsValue,
+    startup,
+} from '../../../store/actionCreators';
+export const execute = {
+    steps: [
+        {
+            title: "Make Startup_Metric model",
+            text: (
+                <React.Fragment>
+                    <p>Setting the following symbol table actions:</p>
+                    <ol>
+                        <li>{'loadInitialState("Spring/Torsion","Metric"),'}</li>
+                        <li>{'changeName("Startup_Metric"),'}</li>
+                        <li>{'changeLabelsValue([\{name\: "COMMENT", value\: "Torsion Spring default start point ... metric units"\}]),'}</li>
+                        <li>{'startup(),'}</li>
+                    </ol>
+                </React.Fragment>
+            ),
+            actions: [
+                loadInitialState("Spring/Torsion","Metric"),
+                changeName("Startup_Metric"),
+                changeLabelsValue([{name: "COMMENT", value: "Torsion Spring default start point ... metric units"}]),
+                startup(),
+            ]
+        }
+    ]
+}
