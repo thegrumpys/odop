0c9a2ae12e91f3c7b97c5fb169ce081c74d0d359
Grumpy In CA <WinTenTester@outlook.com>
2021-02-16 13:48:31 -0800

    459  Develop "Welcome" execute sessions
    Apply latest Advanced tweaks to Calc
    Propagate to Compression & extension

--- client/src/designtypes/Spring/Torsion/welcomeAdv.js
+++ client/src/designtypes/Spring/Torsion/welcomeAdv.js
@@ -8,23 +8,34 @@ export const execute = {
                 <React.Fragment>
                     <p>
                     Welcome to the ODOP:Spring web app!
-                    Advanced view is below. 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/menus#ViewAdvanced" target="_blank" rel="noopener noreferrer">Advanced view </a> 
+                    is below. 
                     The existing values are an arbitrary starting point. 
                     Modify them to meet your requirements. 
                     </p>
                     
                     <p>
-                    Values in the Independent Variables and Calculation Inputs sections below are inputs.
-                    As input values are entered, calculation results will appear immediately. 
-                    Dependent Variables can be controlled by setting constraints or setting FIXed status. 
-                    Use the Search function (<b>Action : Search</b> menu) to find a design that meets constraints and FIXes.
+                    Values in the 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#independentVar" target="_blank" rel="noopener noreferrer">Independent Variables</a>&nbsp; 
+                    and 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#calcInputs" target="_blank" rel="noopener noreferrer">Calculation Inputs</a>&nbsp; 
+                    sections below are inputs.
+                    As input values are entered, calculation results will appear immediately in the 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#dependentVar" target="_blank" rel="noopener noreferrer">Dependent Variables</a>&nbsp; 
+                    section. 
+                    Dependent Variables can be controlled by setting 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#constraints" target="_blank" rel="noopener noreferrer">constraints</a>&nbsp; 
+                    or 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#fix" target="_blank" rel="noopener noreferrer">FIXed status</a>.&nbsp; 
+                    Use the 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/search" target="_blank" rel="noopener noreferrer">Search</a>&nbsp; 
+                    function (<b>Action : Search</b> menu) to find a design that meets constraints and FIXes.
                     </p>
 
                     <p>
                     Push the blue "Next" button in the upper right to show the next page of instructions, hints and tips. 
                     Push the darker gray "Exit" button to close this light green panel. 
                     </p>
-                    <br />
                 </React.Fragment>
             ),
             actions: [
@@ -35,17 +46,20 @@ export const execute = {
             title: "Page 02 of 04",
             text: (
                 <React.Fragment>
-                    <br />
                     <p>
-                    The ODOP:Spring Advanced view allows access to all program features.
-                    Alternative calculation approaches are supported by other views.
-                    Use the <b>View</b> menu above to switch views.
+                    <a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#fix" target="_blank" rel="noopener noreferrer">FIXed status</a>&nbsp; 
+                    causes Search to achieve or hold a specified value.
+                    FREE status allows Search to manipulate the variable to achieve a feasible design. 
+                    The <a href="https://www.springdesignsoftware.org/odop/docs/Help/tutordemo" target="_blank" rel="noopener noreferrer">Tutorial and Demo</a>&nbsp; 
+                    sessions provide additional detail on these concepts as well as more advanced topics 
+                    such as user specified material properties and end types.
                     </p>
-                    
+
                     <p>
-                    The multi-colored Feasibility Status indicator provides feedback on the viability of the current design.
-                    For more information about design feasibility and about the Feasibility Status indicator, follow the links: 
-                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/feasibility" target="_blank" rel="noopener noreferrer">Feasiblity</a>,&nbsp; 
+                    The multi-colored 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/feasibility" target="_blank" rel="noopener noreferrer">Feasibility Status indicator</a>,&nbsp; 
+                    provides feedback on the viability of the current design.
+                    See also: 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/designSituations" target="_blank" rel="noopener noreferrer">Design Situations</a>&nbsp; and 
                     &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#feasibleRegion" target="_blank" rel="noopener noreferrer">Feasible Region</a>.&nbsp; 
                    </p>
@@ -58,20 +72,29 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    FIXed status causes Search to achieve or hold a specified value.
-                    FREE status allows Search to manipulate the variable to achieve a feasible design.
-                    For more information, see: 
-                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/terminology#fix" target="_blank" rel="noopener noreferrer">FIX</a>&nbsp; 
-                    and various tutorial and demo sessions.
-                    For more information on these sessions, see:
-                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/tutordemo" target="_blank" rel="noopener noreferrer">Tutorial and Demo</a>&nbsp; 
+                    The ODOP:Spring Advanced view allows access to all program features.
+                    Alternative presentations are provided by other views.
+                    Use the <b>View</b> menu above to switch views.
                     </p>
                     
                     <p>
                     Tooltips are available for many elements of the Advanced view.
                     Hover the cursor over an item of interest (tap a touch screen) to learn about these elements.
-                    Additional insights are available in the on-line Help plus Tutorial and Demo sessions.
                     </p>
+                    
+                    <p>
+                    The <b>File : Open</b> menu above provides the ability to open existing designs of 
+                    supported spring 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/DesignTypes" target="_blank" rel="noopener noreferrer">types</a>&nbsp; 
+                    (
+                    <a href="https://www.springdesignsoftware.org/odop/docs/Help/DesignTypes/c_spring" target="_blank" rel="noopener noreferrer">compression,</a>&nbsp; 
+                    <a href="https://www.springdesignsoftware.org/odop/docs/Help/DesignTypes/e_spring" target="_blank" rel="noopener noreferrer">extension</a>&nbsp;
+                    &  
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/DesignTypes/t_spring" target="_blank" rel="noopener noreferrer">torsion</a> 
+                    ) and 
+                    &nbsp;<a href="https://www.springdesignsoftware.org/odop/docs/Help/SpringDesign/unitsUSmetric" target="_blank" rel="noopener noreferrer">units (U.S., metric)</a>.&nbsp; 
+                    These system-provided designs can be a starting point for new custom designs. 
+                   </p>
                 </React.Fragment>
             )
         },
@@ -80,19 +103,16 @@ export const execute = {
             text: (
                 <React.Fragment>
                     <p>
-                    The <b>File : Open</b> menu above provides the ability to open existing designs of 
-                    supported spring types (compression, extension & torsion) and units (U.S., metric). 
-                    These system-provided designs can be a starting point for new custom designs. 
-                   </p>
-                    
-                    <p>
                     In summary: 
                     <br />
-                    Enter values and constraints to describe what is known and what the design needs to achieve. 
+                    Enter known values. 
+                    Use constraints and FIX to describe what the design needs to achieve.
                     Be sure to select appropriate values for Material_Type and End_Type.
                     Use the Search function (<b>Action : Search</b> menu) to find a feasible solution.
                     Use the Seek function (<b>Action : Seek</b> menu) to optimize a feasible design.
-                    Switch to other views to utilize the full capabilities of ODOP:Spring.
+                   </p>
+                    
+                    <p>
                     Push the gray "Exit" button in the upper right to close this light green panel.
                    </p>
                     
