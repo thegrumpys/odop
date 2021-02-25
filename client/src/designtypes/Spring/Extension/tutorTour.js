9a80d8f6594e7e27b679698b9fbf94883c4e18bc
Grumpy In CA <WinTenTester@outlook.com>
2021-02-24 20:37:12 -0800

    540  Copy introductory tutor & demo sessions to extension & torsion
    Repair imports line

--- client/src/designtypes/Spring/Extension/tutorTour.js
+++ client/src/designtypes/Spring/Extension/tutorTour.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
-import { changeSymbolValue, changeSymbolConstraint, loadInitialState, changeLabelsValue } from '../../../store/actionCreators';
-import { MAX } from '../../../store/actionTypes';
+// import { changeSymbolValue, changeSymbolConstraint, loadInitialState, changeLabelsValue } from '../../../store/actionCreators';
+// import { MAX } from '../../../store/actionTypes';
 export const execute = {
     steps: [
         {
