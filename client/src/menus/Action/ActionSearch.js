bbfcc6fdb2c5cd069f5f7f4910bb78e456dde369
Grumpy In CA <WinTenTester@outlook.com>
2021-06-06 19:23:24 -0700

    557  Add warning messages prior to Action : Search
    Repair oversight; check both Min & Max constrained

--- client/src/menus/Action/ActionSearch.js
+++ client/src/menus/Action/ActionSearch.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavDropdown } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { FIXED } from '../../store/actionTypes';
+import { CONSTRAINED, FIXED } from '../../store/actionTypes';
 import { search, saveAutoSave } from '../../store/actionCreators';
 import { logUsage } from '../../logUsage';
 import { displayMessage } from '../../components/MessageModal';
@@ -28,7 +28,7 @@ class ActionSearch extends Component {
             warnMsg += 'Objective Value is Not a Number. Check constraint values; ';
         }
         this.props.symbol_table.forEach((element) => { // For each Symbol Table entry
-            if (element.type !== undefined && element.type !== "table" && element.cmin > element.cmax) {
+            if (element.type !== undefined && element.type !== "table" && element.lmin === CONSTRAINED && element.lmax === CONSTRAINED && element.cmin > element.cmax) {
                 warnMsg += (element.name + ' constraints are inconsistent; ');
             }
         });
