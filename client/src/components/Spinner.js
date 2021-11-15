efd596affbc25f1c80697359121d0de1bf784fa7
Brian Watt <bwatt@1fifoto.com>
2021-11-15 11:52:57 -0600

    600 Change Spinner Modal fade="false" to Animation={false}

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -24,7 +24,7 @@ export class Spinner extends Component {
     render() {
 //        console.log('In Spinner.render this=',this);
         return (
-            <Modal show={this.state.modal} style={{zIndex: 1100}} size="sm" fade="false" className={this.props.className} onHide={this.onNoop}>
+            <Modal show={this.state.modal} style={{zIndex: 1100}} size="sm" animation={false} className={this.props.className} onHide={this.onNoop}>
                 <Modal.Body><img src="spinner.gif" alt="Spinning Spinner" style={{"height":"90px"}}/>&nbsp;Running...</Modal.Body>
             </Modal>
         );
