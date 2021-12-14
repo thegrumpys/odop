92b4882821f032c3b9aaeffb1cfb231dd5636ee0
Brian Watt <bwatt@1fifoto.com>
2021-12-13 22:12:15 -0600

    607 Add 3d view

---
+++ client/src/components/GeometryView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Row } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+class GeometryView extends Component {
+    
+    render() {
+//        console.log('In GeometryView.render this=',this);
+        return (
+            <>
+                <Row>
+                  TBD
+                </Row>
+            </>
+        );
+    }
+    
+}
+
+export default connect()(GeometryView);
