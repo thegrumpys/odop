ad340904add96d6cfcad25b33bbb1a31525de2d3
Brian Watt <bwatt@1fifoto.com>
2022-01-17 19:07:28 -0600

    613 Optimize button

---
+++ client/src/designtypes/Spring/Compression/ResultSetOptimize.js
@@ -0,0 +1,110 @@
+import { Component } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { MIN, MAX } from '../store/actionTypes';
+import { seek, saveAutoSave } from '../store/actionCreators';
+import { logUsage } from '../logUsage';
+import { displaySpinner } from './Spinner';
+
+export class ResultTableOptimize extends Component {
+
+    constructor(props) {
+//        console.log('In ResultTableOptimize.ctor'');
+        super(props);
+        this.onOptimizeSeekMINWeight = this.onOptimizeSeekMINWeight.bind(this);
+        this.onOptimizeSeekMAXCycle_Life = this.onOptimizeSeekMAXCycle_Life.bind(this);
+        this.onOptimizeSeekMINRate = this.onOptimizeSeekMINRate.bind(this);
+        this.onOptimizeSeekMINL_Solid = this.onOptimizeSeekMINL_Solid.bind(this);
+    }
+
+    onOptimizeSeekMINWeight(event) {
+//        console.log('In ResultTableOptimize.onOptimizeSeekMINWeight this=',this,'event=',event);
+        logUsage('event', 'ResultTableOptimize', { 'event_label': 'optmize Seek MIN Weight button' });
+        this.setState({
+            optimize_modal: !this.state.optimize_modal,
+        });
+        displaySpinner(true);
+        this.props.saveAutoSave();
+        this.props.seek('Weight', MIN);
+        displaySpinner(false);
+        this.props.onClick(event);
+    }
+
+    onOptimizeSeekMAXCycle_Life(event) {
+//        console.log('In ResultTableOptimize.onOptimizeSeekMAXCycle_Life this=',this,'event=',event);
+        logUsage('event', 'ResultTableOptimize', { 'event_label': 'optmize Seek MAX Cycle_Life button' });
+        this.setState({
+            optimize_modal: !this.state.optimize_modal,
+        });
+        displaySpinner(true);
+        this.props.saveAutoSave();
+        this.props.seek('Cycle_Life', MAX);
+        displaySpinner(false);
+        this.props.onClick(event);
+    }
+
+    onOptimizeSeekMINRate(event) {
+//        console.log('In ResultTableOptimize.onOptimizeSeekMINRate this=',this,'event=',event);
+        logUsage('event', 'ResultTableOptimize', { 'event_label': 'optmize Seek MIN Rate button' });
+        this.setState({
+            optimize_modal: !this.state.optimize_modal,
+        });
+        displaySpinner(true);
+        this.props.saveAutoSave();
+        this.props.seek('Rate', MIN);
+        displaySpinner(false);
+        this.props.onClick(event);
+    }
+
+    onOptimizeSeekMINL_Solid(event) {
+//        console.log('In ResultTableOptimize.onOptimizeSeekMINL_Solid this=',this,'event=',event);
+        logUsage('event', 'ResultTableOptimize', { 'event_label': 'optmize Seek MIN L_Solid button' });
+        this.setState({
+            optimize_modal: !this.state.optimize_modal,
+        });
+        displaySpinner(true);
+        this.props.saveAutoSave();
+        this.props.seek('L_Solid', MIN);
+        displaySpinner(false);
+        this.props.onClick(event);
+    }
+
+  render() {
+//        console.log('In ResultTableOptimize.render this=',this);
+    return (
+      <>
+        <p>Select a pre-configured Seek optimization:</p>
+        <Table borderless="true" size="sm">
+          <tbody>
+            <tr>
+              <td width="50%">
+                <Button variant="primary" onClick={this.onOptimizeSeekMINWeight}>Seek MIN Weight</Button>
+              </td>
+              <td width="50%">
+                <Button variant="primary" onClick={this.onOptimizeSeekMAXCycle_Life}>Seek MAX Cycle_Life</Button>
+              </td>
+            </tr>
+            <tr>
+              <td width="50%">
+                <Button variant="primary" onClick={this.onOptimizeSeekMINRate}>Seek MIN Rate</Button>
+              </td>
+              <td width="50%">
+                <Button variant="primary" onClick={this.onOptimizeSeekMINL_Solid}>Seek MIN L_Solid</Button>
+              </td>
+            </tr>
+         </tbody>
+        </Table>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = {
+    seek: seek,
+    saveAutoSave: saveAutoSave
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ResultTableOptimize);
