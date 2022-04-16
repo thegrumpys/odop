ad4c74f0da32f789285802becd19b12a22f05f5a
Brian Watt <bwatt@1fifoto.com>
2022-04-16 07:30:53 -0500

    643 Possibilities to speed up Patsh operation

--- client/src/store/middleware/propagate.js
+++ client/src/store/middleware/propagate.js
@@ -5,17 +5,20 @@ export function propagate(store) {
     var design = store.getState(); // Re-access store to get latest source values
 //    console.log('In propagate design=',design);
     var value;
+    function nameMatch(entry) {
+      return design.model.symbol_table.find(sink => entry.name === sink.name)
+    }
     for (let i = 0; i < design.model.symbol_table.length; i++) {
         var source = design.model.symbol_table[i];
-        console.log('In propagate source=',source);
+//        console.log('In propagate source=',source);
         value = source.value;
         if (source.propagate !== undefined) {
-            console.log('In propagate source.propagate=',source.propagate);
+//            console.log('In propagate source.propagate=',source.propagate);
             for (let j = 0; j < source.propagate.length; j++) {
                 var entry = source.propagate[j];
-                var sink = design.model.symbol_table.find(sink => entry.name === sink.name);
+                var sink = nameMatch(entry);
 //                console.log('In propagate source=',source,'sink=',sink);
-                console.log('In propagate sink.name=',sink.name,'entry.minmax=',entry.minmax,'value=',value);
+//                console.log('In propagate sink.name=',sink.name,'entry.minmax=',entry.minmax,'value=',value);
                 store.dispatch(changeSymbolConstraint(sink.name, entry.minmax, value));
             }
         }
--- client/src/store/middleware/pxPropagate.js
+++ client/src/store/middleware/pxPropagate.js
@@ -7,21 +7,24 @@ export function pxPropagate(p, x, store) {
     var ip = 0;
     var ix = 0;
     var value;
+    function nameMatch(entry) {
+      return design.model.symbol_table.find(sink => entry.name === sink.name)
+    }
     for (let i = 0; i < design.model.symbol_table.length; i++) {
         var source = design.model.symbol_table[i];
-        console.log('In pxPropagate source=',source);
+//        console.log('In pxPropagate source=',source);
         if (source.type === "equationset" && source.input) {
             value = p[ip++];
         } else {
             value = x[ix++];
         }
         if (source.propagate !== undefined) {
-             console.log('In pxPropagate source.propagate=',source.propagate);
+//             console.log('In pxPropagate source.propagate=',source.propagate);
              for (let j = 0; j < source.propagate.length; j++) {
                  var entry = source.propagate[j];
-                 var sink = design.model.symbol_table.find(sink => entry.name === sink.name);
+                 var sink = nameMatch(entry);
 //                 console.log('In pxPropagate source=',source,'sink=',sink);
-                 console.log('In pxPropagate sink.name=',sink.name,'entry.minmax=',entry.minmax,'value=',value);
+//                 console.log('In pxPropagate sink.name=',sink.name,'entry.minmax=',entry.minmax,'value=',value);
                  store.dispatch(changeSymbolConstraint(sink.name, entry.minmax, value));
              }
         }
