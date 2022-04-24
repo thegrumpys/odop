873029214b06d7597b808e14e3b9e85cce809cbf
Brian Watt <bwatt@1fifoto.com>
2022-04-24 14:30:42 -0500

    676 Full-text Help doc indexing with Lunr

--- client/src/components/SearchDocs.js
+++ client/src/components/SearchDocs.js
@@ -56,8 +56,8 @@ class SearchDocs extends Component {
             return res.json()
         })
         .then((results) => {
-            console.log('In SearchDocs.onButtonPress results=', results);
-            results.forEach((element) => console.log('element.href=',element.href));
+//            console.log('In SearchDocs.onButtonPress results=', results);
+//            results.forEach((element) => console.log('element.href=',element.href));
             this.setState({
                 modal: !this.state.modal,
                 results: results,
