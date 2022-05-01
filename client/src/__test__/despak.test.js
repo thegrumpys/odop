0c3b293b252fe9593a4fa550b4dd4bd725772f86
Brian Watt <bwatt@1fifoto.com>
2022-05-01 14:54:36 -0500

    Repair client test case failures

--- client/src/__test__/despak.test.js
+++ client/src/__test__/despak.test.js
@@ -6,6 +6,10 @@ import { MAX, FIXED } from '../store/actionTypes';
 import { changeSymbolValue, changeResultObjectiveValue, changeSymbolConstraint } from '../store/actionCreators';
 import { reducers } from '../store/reducers';
 import { dispatcher } from '../store/middleware/dispatcher';
+import { invokeInit } from '../store/middleware/invokeInit';
+import { invokeEquationSet } from '../store/middleware/invokeEquationSet';
+import { setSclDen } from '../store/middleware/setSclDen';
+import { updateObjectiveValue } from '../store/middleware/updateObjectiveValue';
 import { despak } from '../store/middleware/despak';
 
 //=====================================================================
@@ -16,6 +20,12 @@ it('despak without merit', () => {
     var state = Object.assign({}, initialState, { system_controls: initialSystemControls }); // Merge initialState and initialSystemControls
     const store = createStore(reducers, {"user": "USERID0123456789", name: "initialState", model: state});
 
+    // These next statements replace store.dispatch(startup());
+    invokeInit(store);
+    invokeEquationSet(store);
+    setSclDen(store);
+    updateObjectiveValue(store);
+    
     store.dispatch(changeSymbolValue("PRESSURE", 500)); // p vector
     store.dispatch(changeSymbolValue("RADIUS", 0.4));
     store.dispatch(changeSymbolValue("THICKNESS", 0.04));
@@ -40,55 +50,57 @@ it('despak without merit', () => {
     var obj = despak(pc, store);
 
     var design = store.getState(); // after
-    expect(obj).toEqual(0.00005);
+    expect(obj).toEqual(0.5605106434805028);
 
-//***************************************
-// DESPAK NO LONGER UPDATES THE MODEL
-// DESPAK PURELY RETURNS THE OBJ VALUE
-//***************************************
-//    expect(design.name).toEqual("initialState");
-//    expect(design.model.type).toEqual("Piston-Cylinder");
-//    expect(design.model.version).toEqual("6");
-//
-//    expect(design.model.symbol_table[sto.PRESSURE].name).toEqual("PRESSURE"); // p vector
-//    expect(design.model.symbol_table[sto.PRESSURE].value).toEqual(500);
-//    expect(design.model.symbol_table[sto.RADIUS].name).toEqual("RADIUS");
-//    expect(design.model.symbol_table[sto.RADIUS].value).toEqual(0.4);
-//    expect(design.model.symbol_table[sto.THICKNESS].name).toEqual("THICKNESS");
-//    expect(design.model.symbol_table[sto.THICKNESS].value).toEqual(0.04);
-//    expect(design.model.symbol_table[sto.FORCE].name).toEqual("FORCE"); // x vector
-//    expect(design.model.symbol_table[sto.FORCE].value).toEqual(123);
-//    expect(design.model.symbol_table[sto.AREA].name).toEqual("AREA");
-//    expect(design.model.symbol_table[sto.AREA].value).toEqual(456);
-//    expect(design.model.symbol_table[sto.STRESS].name).toEqual("STRESS");
-//    expect(design.model.symbol_table[sto.STRESS].value).toEqual(789);
-//
-//    expect(design.model.system_controls.ioopt).toEqual(3);
-//    expect(design.model.system_controls.maxit).toEqual(100);
-//    expect(design.model.system_controls.weapon).toEqual(1);
-//    expect(design.model.system_controls.nmerit).toEqual(1);
-//    expect(design.model.system_controls.fix_wt).toEqual(1.5);
-//    expect(design.model.system_controls.con_wt).toEqual(1.0);
-//    expect(design.model.system_controls.zero_wt).toEqual(10.0);
-//    expect(design.model.system_controls.viol_wt).toEqual(1.0);
-//    expect(design.model.system_controls.mfn_wt).toEqual(0.01);
-//    expect(design.model.system_controls.objmin).toEqual(0.00005);
-//    expect(design.model.system_controls.del).toEqual(1.0);
-//    expect(design.model.system_controls.delmin).toEqual(0.0001);
-//    expect(design.model.system_controls.tol).toEqual(0.0001);
-//    expect(design.model.system_controls.smallnum).toEqual(1.0e-07);
-//    expect(design.model.system_controls.show_units).toEqual(1);
-//    expect(design.model.system_controls.show_violations).toEqual(1);
-//
-//    expect(design.model.result.objective_value).toEqual(0.00005);
-//    expect(design.model.result.termination_condition).toEqual("");
-//    expect(design.model.result.violated_constraint_count).toEqual(0);
+    expect(design.name).toEqual("initialState");
+    expect(design.model.type).toEqual("Piston-Cylinder");
+    expect(design.model.version).toEqual("6");
+
+    expect(design.model.symbol_table[sto.PRESSURE].name).toEqual("PRESSURE"); // p vector
+    expect(design.model.symbol_table[sto.PRESSURE].value).toEqual(500);
+    expect(design.model.symbol_table[sto.RADIUS].name).toEqual("RADIUS");
+    expect(design.model.symbol_table[sto.RADIUS].value).toEqual(0.4);
+    expect(design.model.symbol_table[sto.THICKNESS].name).toEqual("THICKNESS");
+    expect(design.model.symbol_table[sto.THICKNESS].value).toEqual(0.04);
+    expect(design.model.symbol_table[sto.FORCE].name).toEqual("FORCE"); // x vector
+    expect(design.model.symbol_table[sto.FORCE].value).toEqual(123); // DESPAK DOES NOT UPDATE THE MODEL
+    expect(design.model.symbol_table[sto.AREA].name).toEqual("AREA");
+    expect(design.model.symbol_table[sto.AREA].value).toEqual(456); // DESPAK DOES NOT UPDATE THE MODEL
+    expect(design.model.symbol_table[sto.STRESS].name).toEqual("STRESS");
+    expect(design.model.symbol_table[sto.STRESS].value).toEqual(789); // DESPAK DOES NOT UPDATE THE MODEL
+
+    expect(design.model.system_controls.ioopt).toEqual(3);
+    expect(design.model.system_controls.maxit).toEqual(100);
+    expect(design.model.system_controls.weapon).toEqual(1);
+    expect(design.model.system_controls.nmerit).toEqual(1);
+    expect(design.model.system_controls.fix_wt).toEqual(1.5);
+    expect(design.model.system_controls.con_wt).toEqual(1.0);
+    expect(design.model.system_controls.zero_wt).toEqual(10.0);
+    expect(design.model.system_controls.viol_wt).toEqual(1.0);
+    expect(design.model.system_controls.mfn_wt).toEqual(0.01);
+    expect(design.model.system_controls.objmin).toEqual(0.00005);
+    expect(design.model.system_controls.del).toEqual(1.0);
+    expect(design.model.system_controls.delmin).toEqual(0.0001);
+    expect(design.model.system_controls.tol).toEqual(0.0001);
+    expect(design.model.system_controls.smallnum).toEqual(1.0e-07);
+    expect(design.model.system_controls.show_units).toEqual(1);
+    expect(design.model.system_controls.show_violations).toEqual(1);
+
+    expect(design.model.result.objective_value).toEqual(0.00005); // DESPAK DOES NOT UPDATE THE MODEL
+    expect(design.model.result.termination_condition).toEqual("");
+    expect(design.model.result.violated_constraint_count).toEqual(1);
 });
 
 it('despak with merit', () => {
     var state = Object.assign({}, initialState, { system_controls: initialSystemControls }); // Merge initialState and initialSystemControls
     const store = createStore(reducers, {"user": "USERID0123456789", name: "initialState", model: state}, applyMiddleware(dispatcher));
 
+    // These next statements replace store.dispatch(startup());
+    invokeInit(store);
+    invokeEquationSet(store);
+    setSclDen(store);
+    updateObjectiveValue(store);
+    
     store.dispatch(changeSymbolValue("PRESSURE", 500)); // p vector
     store.dispatch(changeSymbolValue("RADIUS", 0.4));
     store.dispatch(changeSymbolValue("THICKNESS", 0.04));
@@ -96,7 +108,7 @@ it('despak with merit', () => {
     store.dispatch(changeSymbolValue("AREA", 456));
     store.dispatch(changeSymbolValue("STRESS", 789));
     store.dispatch(changeResultObjectiveValue(0.560511));
-    store.dispatch(changeSymbolConstraint("STRESS", MAX, 10000));
+    store.dispatch(changeSymbolConstraint("STRESS", MAX, 10000)); // THIS MODIFIES THE DEPENDENT VARIABLES AND OBJECTIVE VALUE
 
     var design = store.getState(); // before
 
@@ -160,47 +172,43 @@ it('despak with merit', () => {
     var obj = despak(pc, store, merit);
 
     var design = store.getState(); // after
-    expect(obj).toEqual(0.0011111111111111111);
+    expect(obj).toEqual(0.5616217545916139);
 
-//***************************************
-// DESPAK NO LONGER UPDATES THE MODEL
-// DESPAK PURELY RETURNS THE OBJ VALUE
-//***************************************
-//    expect(design.name).toEqual("initialState");
-//    expect(design.model.type).toEqual("Piston-Cylinder");
-//    expect(design.model.version).toEqual("6");
-//
-//    expect(design.model.symbol_table[sto.PRESSURE].name).toEqual("PRESSURE"); // p vector
-//    expect(design.model.symbol_table[sto.PRESSURE].value).toEqual(500);
-//    expect(design.model.symbol_table[sto.RADIUS].name).toEqual("RADIUS");
-//    expect(design.model.symbol_table[sto.RADIUS].value).toEqual(0.4);
-//    expect(design.model.symbol_table[sto.THICKNESS].name).toEqual("THICKNESS");
-//    expect(design.model.symbol_table[sto.THICKNESS].value).toEqual(0.04);
-//    expect(design.model.symbol_table[sto.FORCE].name).toEqual("FORCE"); // x vector
-//    expect(design.model.symbol_table[sto.FORCE].value).toEqual(251.32741228718348);
-//    expect(design.model.symbol_table[sto.AREA].name).toEqual("AREA");
-//    expect(design.model.symbol_table[sto.AREA].value).toEqual(0.5026548245743669);
-//    expect(design.model.symbol_table[sto.STRESS].name).toEqual("STRESS");
-//    expect(design.model.symbol_table[sto.STRESS].value).toEqual(2500);
-//
-//    expect(design.model.system_controls.ioopt).toEqual(3);
-//    expect(design.model.system_controls.maxit).toEqual(100);
-//    expect(design.model.system_controls.weapon).toEqual(1);
-//    expect(design.model.system_controls.nmerit).toEqual(1);
-//    expect(design.model.system_controls.fix_wt).toEqual(1.5);
-//    expect(design.model.system_controls.con_wt).toEqual(1.0);
-//    expect(design.model.system_controls.zero_wt).toEqual(10.0);
-//    expect(design.model.system_controls.viol_wt).toEqual(1.0);
-//    expect(design.model.system_controls.mfn_wt).toEqual(0.01);
-//    expect(design.model.system_controls.objmin).toEqual(0.00005);
-//    expect(design.model.system_controls.del).toEqual(1.0);
-//    expect(design.model.system_controls.delmin).toEqual(0.0001);
-//    expect(design.model.system_controls.tol).toEqual(0.0001);
-//    expect(design.model.system_controls.smallnum).toEqual(1.0e-07);
-//    expect(design.model.system_controls.show_units).toEqual(1);
-//    expect(design.model.system_controls.show_violations).toEqual(1);
-//
-//    expect(design.model.result.objective_value).toEqual(0.0011111111111111111);
-//    expect(design.model.result.termination_condition).toEqual("");
-//    expect(design.model.result.violated_constraint_count).toEqual(0);
+    expect(design.name).toEqual("initialState");
+    expect(design.model.type).toEqual("Piston-Cylinder");
+    expect(design.model.version).toEqual("6");
+
+    expect(design.model.symbol_table[sto.PRESSURE].name).toEqual("PRESSURE"); // p vector
+    expect(design.model.symbol_table[sto.PRESSURE].value).toEqual(500);
+    expect(design.model.symbol_table[sto.RADIUS].name).toEqual("RADIUS");
+    expect(design.model.symbol_table[sto.RADIUS].value).toEqual(0.4);
+    expect(design.model.symbol_table[sto.THICKNESS].name).toEqual("THICKNESS");
+    expect(design.model.symbol_table[sto.THICKNESS].value).toEqual(0.04);
+    expect(design.model.symbol_table[sto.FORCE].name).toEqual("FORCE"); // x vector
+    expect(design.model.symbol_table[sto.FORCE].value).toEqual(251.32741228718348); // UPDATED BY changeSymbolConstraint ABOVE
+    expect(design.model.symbol_table[sto.AREA].name).toEqual("AREA");
+    expect(design.model.symbol_table[sto.AREA].value).toEqual(0.5026548245743669); // UPDATED BY changeSymbolConstraint ABOVE
+    expect(design.model.symbol_table[sto.STRESS].name).toEqual("STRESS");
+    expect(design.model.symbol_table[sto.STRESS].value).toEqual(2500); // UPDATED BY changeSymbolConstraint ABOVE
+
+    expect(design.model.system_controls.ioopt).toEqual(3);
+    expect(design.model.system_controls.maxit).toEqual(100);
+    expect(design.model.system_controls.weapon).toEqual(1);
+    expect(design.model.system_controls.nmerit).toEqual(1);
+    expect(design.model.system_controls.fix_wt).toEqual(1.5);
+    expect(design.model.system_controls.con_wt).toEqual(1.0);
+    expect(design.model.system_controls.zero_wt).toEqual(10.0);
+    expect(design.model.system_controls.viol_wt).toEqual(1.0);
+    expect(design.model.system_controls.mfn_wt).toEqual(0.01);
+    expect(design.model.system_controls.objmin).toEqual(0.00005);
+    expect(design.model.system_controls.del).toEqual(1.0);
+    expect(design.model.system_controls.delmin).toEqual(0.0001);
+    expect(design.model.system_controls.tol).toEqual(0.0001);
+    expect(design.model.system_controls.smallnum).toEqual(1.0e-07);
+    expect(design.model.system_controls.show_units).toEqual(1);
+    expect(design.model.system_controls.show_violations).toEqual(1);
+
+    expect(design.model.result.objective_value).toEqual(0.5605106434805028); // UPDATED BY changeSymbolConstraint ABOVE
+    expect(design.model.result.termination_condition).toEqual("");
+    expect(design.model.result.violated_constraint_count).toEqual(1);
 });
