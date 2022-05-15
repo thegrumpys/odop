cb3da6d118349541647b812f4e6dc8638689a63c
Brian Watt <bwatt@1fifoto.com>
2022-05-15 14:04:39 -0500

    692 Improve usage log - add sequence numbering

--- client/src/logUsage.js
+++ client/src/logUsage.js
@@ -54,8 +54,7 @@ function flushBuffer() {
     if (buffer !== '') {
         var tag = 'event';
         var action = 'Values';
-        var sequence_number = sequence++;
-        var sequenced_note = {event_value: sequence_number.toString().padStart(8,'0')+' ', event_label: buffer};
+        var sequenced_note = {event_value: sequence++, event_label: buffer};
         if (process.env.NODE_ENV === 'production') { // Limit G.A. tracking to production
             window.gtag(tag, action, sequenced_note); // Output to Google Analytics
         }
@@ -79,8 +78,7 @@ export function logValue(name,value,suffix='',merge=true) {
 export function logUsage(tag, action, note) {
 //    console.log('In logUsage tag=',tag,'action=',action,'note=',note);
     flushBuffer();
-    var sequence_number = sequence++;
-    var sequenced_note = Object.assign({event_value: sequence_number.toString().padStart(8,'0')+' '}, note);
+    var sequenced_note = Object.assign({event_value: sequence++}, note);
     if (process.env.NODE_ENV === 'production') { // Limit G.A. tracking to production
         window.gtag(tag, action, sequenced_note); // Output to Google Analytics
     }
