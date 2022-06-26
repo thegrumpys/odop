4330db050c2723ac04a643e9c752eb13e0994c8f
Brian Watt <bwatt@1fifoto.com>
2022-06-26 10:25:01 -0500

    569 Implement Alert facility

--- client/src/components/Alerts.js
+++ client/src/components/Alerts.js
@@ -133,7 +133,7 @@ export var commonChecks = function(store) {
                 addAlert({
                     element: element,
                     name: element.name+' MIN',
-                    message: element.lmin & FDCL ? ('FDCL ' + element.name+' MIN = ' + element.cminchoices[element.cminchoice]) : ('non-FDCL ' + element.name+' MIN = ' + element.cmin),
+                    message: element.lmin & FDCL ? ('FDCL: ' + element.name+' MIN = ' + element.cminchoices[element.cminchoice]) : ('non-FDCL: ' + element.name+' MIN = ' + element.cmin),
                     severity: 'Info',
                     help_url: '[Help](/docs/Help/alerts.html#FDCL)'
                 });
@@ -144,7 +144,7 @@ export var commonChecks = function(store) {
                 addAlert({
                     element: element,
                     name: element.name+' MAX',
-                    message: element.lmax & FDCL ? ('FDCL ' + element.name+' MAX = ' + element.cmaxchoices[element.cmaxchoice]) : ('non-FDCL ' + element.name+' MAX = ' + element.cmax),
+                    message: element.lmax & FDCL ? ('FDCL: ' + element.name+' MAX = ' + element.cmaxchoices[element.cmaxchoice]) : ('non-FDCL: ' + element.name+' MAX = ' + element.cmax),
                     severity: 'Info',
                     help_url: '[Help](/docs/Help/alerts.html#FDCL)'
                 });
