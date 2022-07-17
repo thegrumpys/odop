eefa79e59b7c24d5cfce6ea8699fa96500e2ce79
Grumpy In CA <WinTenTester@outlook.com>
2022-07-17 15:01:10 -0700

    703  Post validity-integration tweaks
    rename script to increasePrecision

--- client/src/designtypes/Spring/Torsion/execute.js
+++ client/src/designtypes/Spring/Torsion/execute.js
@@ -5,13 +5,13 @@ export function getExecuteNames() {
       result = [
         'mkStartup',              // Script to make Startup file from Load Initial State
         'mkStartup_Metric',       // Script to make Startup Metric file from Load Initial State Metric
-        'improvePrecision',             // Script to tweak Preference values for greater precision of search result
+        'increasePrecision',             // Script to tweak Preference values for greater precision of search result
         'welcomeAdv',             // Script to guide novice users - Advanced view; spring type neutral
         'welcomeCalc'             // Script to guide novice users - Calculator view; spring type neutral
       ];
     } else {
       result = [
-        'improvePrecision'
+        'increasePrecision'
       ];
     }
 //    console.log('In getExecuteNames result=',result);
