666320199b875ff55c89e46d1a6f1152bc28affd
Grumpy In CA <WinTenTester@outlook.com>
2022-07-21 20:57:03 -0700

    712  Compression - limit deflection and length at solid condition
    work in progress.
    Fails with:
    TypeError: Cannot read properties of undefined (reading '26')

--- client/src/designtypes/Spring/Compression/initialState_metric_units.js
+++ client/src/designtypes/Spring/Compression/initialState_metric_units.js
@@ -467,6 +467,22 @@ export const initialState = {
         },
         {
             "input": false,
+            "name": "Excess_Force",
+            "value": 0.0,
+            "units": "newtons",
+            "lmin": CONSTRAINED,
+            "lmax": 0,
+            "cmin": 0.0,
+            "cmax": 1000,
+            "validmin": Number.MIN_VALUE,
+            "validmax": Number.MAX_VALUE,
+            "sdlim": 0.1,
+            "tooltip": "Hidden.  Provide restoring influence for case where Force_2 >> Force_Solid",
+            "type": "equationset",
+            "hidden": true
+        },
+        {
+            "input": false,
             "name": "Spring_Type",
             "value": "Compression",
             "units": "",
