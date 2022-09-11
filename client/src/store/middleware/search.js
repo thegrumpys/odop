aa827fa0f7d817afeb9ece13b6aef184a88e7893
Brian D. Watt <bwatt@1fifoto.com>
2022-09-11 10:45:23 -0500

    717 Make entire symbol table accessible to init, eqnset ,etc.

--- client/src/store/middleware/search.js
+++ client/src/store/middleware/search.js
@@ -1,6 +1,7 @@
 import { FIXED } from '../actionTypes';
 import { changeInputSymbolValues, changeResultTerminationCondition } from '../actionCreators';
 import { patsh } from './patsh';
+import { createStore, applyMiddleware } from 'redux';
 
 // Search
 export function search(store, objmin, merit) {
@@ -8,24 +9,13 @@ export function search(store, objmin, merit) {
 //    console.trace();
     
     var design = store.getState();
-    
-    // Compress P into PC
-    var element;
-    var pc = [];
-    for (let i = 0; i < design.model.symbol_table.length; i++) {
-        element = design.model.symbol_table[i];
-            if (element.type === "equationset" && element.input) { // Only Independent Variable, skip Dependent and Calc Input
-                if (!(element.lmin & FIXED)) { // Only Free
-//                   console.log('In search i=',i,'element=',element);
-                    pc.push(element.value);
-                }
-            }
-    }
+    var shadow_design = JSON.parse(JSON.stringify(design)); // Deep clone
+    var shadow_store = createStore(reducers, shadow_design, applyMiddleware(middleware));
     
     // Do the pattern search
     var delarg = design.model.system_controls.del;
 //    console.log('In search pc=',pc,'delarg=',delarg,'design.model.system_controls.delmin=',design.model.system_controls.delmin,'objmin=',objmin,'design.model.system_controls.maxit=',design.model.system_controls.maxit,'design.model.system_controls.tol=',design.model.system_controls.tol);
-    var ncode = patsh(pc, delarg, design.model.system_controls.delmin, objmin, design.model.system_controls.maxit, design.model.system_controls.tol, store, merit);
+    var ncode = patsh(shadow_store, delarg, design.model.system_controls.delmin, objmin, design.model.system_controls.maxit, design.model.system_controls.tol, merit);
     
     // Expand PC back into store change actions
     var kd = 0;
