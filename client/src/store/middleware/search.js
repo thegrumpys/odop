671f9f8c8549f761ad62bc4a33576d95570be24a
Brian D. Watt <bwatt@1fifoto.com>
2022-09-11 19:39:22 -0500

    717 Make entire symbol table accessible to init, eqnset ,etc.

--- client/src/store/middleware/search.js
+++ client/src/store/middleware/search.js
@@ -1,40 +1,58 @@
 import { FIXED } from '../actionTypes';
-import { changeInputSymbolValues, changeResultTerminationCondition } from '../actionCreators';
+import { changeInputSymbolValues, changeResultTerminationCondition, load } from '../actionCreators';
 import { patsh } from './patsh';
+import { dispatcher } from './dispatcher';
+import { reducers } from '../reducers';
 import { createStore, applyMiddleware } from 'redux';
 
 // Search
 export function search(store, objmin, merit) {
 //    console.log('Entering search store=',store,'objmin=',objmin,'merit=',merit);
 //    console.trace();
-    
-    var design = store.getState();
-    var shadow_design = JSON.parse(JSON.stringify(design)); // Deep clone
-    var shadow_store = createStore(reducers, shadow_design, applyMiddleware(middleware));
-    
+
+    const local_store = createStore(reducers, JSON.parse(JSON.stringify(store.getState())), applyMiddleware(dispatcher));
+    var local_design = local_store.getState();
+//    console.log('In search local_store=',local_store,'local_design=',local_design);
+
+    // Compress P into PC
+    var element;
+    var pc = [];
+    for (let i = 0; i < local_design.model.symbol_table.length; i++) {
+        element = local_design.model.symbol_table[i];
+        if (element.type === "equationset" && element.input) { // Only Independent Variable, skip Dependent and Calc Input
+            if (!(element.lmin & FIXED)) { // Only Free
+                pc.push(element.value);
+            }
+        }
+    }
+
     // Do the pattern search
-    var delarg = design.model.system_controls.del;
-//    console.log('In search pc=',pc,'delarg=',delarg,'design.model.system_controls.delmin=',design.model.system_controls.delmin,'objmin=',objmin,'design.model.system_controls.maxit=',design.model.system_controls.maxit,'design.model.system_controls.tol=',design.model.system_controls.tol);
-    var ncode = patsh(shadow_store, delarg, design.model.system_controls.delmin, objmin, design.model.system_controls.maxit, design.model.system_controls.tol, merit);
-    
+    var delarg = local_design.model.system_controls.del; // Create copy of delarg because patsh changes delarg internally
+//    console.log('In search pc=',pc,'delarg=',delarg,'local_design.model.system_controls.delmin=',local_design.model.system_controls.delmin,'objmin=',objmin,'local_design.model.system_controls.maxit=',local_design.model.system_controls.maxit,'local_design.model.system_controls.tol=',local_design.model.system_controls.tol);
+    var ncode = patsh(pc, delarg, local_design.model.system_controls.delmin, objmin, local_design.model.system_controls.maxit, local_design.model.system_controls.tol, local_store, merit);
+//    console.log('In search ncode=',ncode);
+    local_store.dispatch(changeResultTerminationCondition(ncode));
+
     // Expand PC back into store change actions
     var kd = 0;
     var p = [];
-    for (let i = 0; i < design.model.symbol_table.length; i++) {
-        element = design.model.symbol_table[i];
-            if (element.type === "equationset" && element.input) { // Only Independent Variable, skip Dependent and Calc Input
-                if (!(element.lmin & FIXED)) { // Only Free
-                    p.push(pc[kd++]);
-                } else {
-                    p.push(element.value);
-                }
+    for (let i = 0; i < local_design.model.symbol_table.length; i++) {
+        element = local_design.model.symbol_table[i];
+        if (element.type === "equationset" && element.input) { // Only Independent Variable, skip Dependent and Calc Input
+            if (!(element.lmin & FIXED)) { // Only Free
+                p.push(pc[kd++]);
+            } else {
+                p.push(element.value);
             }
+        }
     }
-    store.dispatch(changeInputSymbolValues(p, merit));
-    store.dispatch(changeResultTerminationCondition(ncode));
-    
-    design = store.getState();
-    var obj = design.model.result.objective_value;
+    local_store.dispatch(changeInputSymbolValues(p, merit));
+
+    local_design = local_store.getState();
+    var obj = local_design.model.result.objective_value;
+
+    store.dispatch(load(local_store.getState())); // Restore the local_store as the primary store
+
 //    console.log('Exiting search p=',p,'ncode=',ncode,'obj=',obj);
     return obj;
 }
