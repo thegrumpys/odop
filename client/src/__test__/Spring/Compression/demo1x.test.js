5bfbde73f00b5e6a745bde4aeb646f23310d3346
Grumpy In CA <WinTenTester@outlook.com>
2023-03-28 17:10:59 -0700

    Merge remote-tracking branch 'origin/830' into 381

--- client/src/__test__/Spring/Compression/demo1x.test.js
+++ client/src/__test__/Spring/Compression/demo1x.test.js
@@ -33,61 +33,47 @@ it('demo1', () => {
     design = store.getState();
     expect(design.model.result.objective_value).toEqual(0.0);
     
-// title: "Page 02 of 11"
-    store.dispatch(loadInitialState("Spring/Compression","US"));
-    store.dispatch(changeLabelsValue([{"name":"COMMENT","value":"Compression Spring demo1"}]));
-    
-design = store.getState();
-    expect(design.model.result.objective_value).toBeCloseTo(0.0000000,7);
-    
-// title: "Page 03 of 11"
+     // Page 02 of 11
+     // Page 03 of 11
+     // Page 04 of 11
     // No-op
-    
-// title: "Page 04 of 11"
-    // No-op
-    
-// title: "Page 05 of 11"
-    store.dispatch(fixSymbolValue("OD_Free",0.925));
-    store.dispatch(fixSymbolValue("L_Free",1.713));
-    store.dispatch(fixSymbolValue("Force_2",50));
-    store.dispatch(fixSymbolValue("L_2",1.278));
-    store.dispatch(setSymbolFlag("L_Solid",MAX,CONSTRAINED));
-    store.dispatch(changeSymbolConstraint("L_Solid",MAX,1.06));
-    store.dispatch(changeSymbolValue("Material_Type",3));
-    
-design = store.getState();
+
+     // Page 05 of 11
+    store.dispatch( fixSymbolValue("OD_Free",0.925) );
+    store.dispatch( fixSymbolValue("L_Free",1.713) );
+    store.dispatch( fixSymbolValue("Force_2",50) );
+    store.dispatch( fixSymbolValue("L_2",1.278) );
+    store.dispatch( setSymbolFlag("L_Solid",MAX,CONSTRAINED) );
+    store.dispatch( changeSymbolConstraint("L_Solid",MAX,1.06) );
+    store.dispatch( changeSymbolValue("Material_Type",3) );
+    design = store.getState();
     expect(design.model.result.objective_value).toBeCloseTo(2.7011747,7);
-    
-// title: "Page 06 of 11"
-    store.dispatch(changeSymbolConstraint("FS_2",MAX,2));
-    
-design = store.getState();
-    expect(design.model.result.objective_value).toBeCloseTo(2.7011747,7);
-    
-// title: "Page 07 of 11"
-    store.dispatch(search());
-    
-design = store.getState();
+
+     // Page 06 of 11
+    store.dispatch( changeSymbolConstraint("FS_2",MAX,2) );
+
+     // Page 07 of 11
+    store.dispatch( search() );
+    design = store.getState();
     expect(design.model.result.objective_value).toBeCloseTo(0.0000032,7);
-    
-// title: "Page 08 of 11"
+
+     // Page 08 of 11
     store.dispatch(fixSymbolValue("Wire_Dia",0.125)); // Simulate Action: Select Size...
-design = store.getState();
-    expect(design.model.result.objective_value).toBeCloseTo(0.0000032,7);
-    
-// title: "Page 09 of 11"
-    store.dispatch(search());
-    
-design = store.getState();
-    expect(design.model.result.objective_value).toBeCloseTo(0.0000032,7);
-    
-// title: "Page 10 of 11"
+    design = store.getState();
+    expect(design.model.result.objective_value).toBeCloseTo(0.0000408,7);
+
+     // Page 09 of 11
+    store.dispatch( search() );
+    design = store.getState();
+    expect(design.model.result.objective_value).toBeCloseTo(0.0000099,7);
+
+     // Page 10 of 11
     // No-op
-    
-// title: "Page 11 of 11 (last page)"
-    store.dispatch(fixSymbolValue("Force_2",75.1));
-    
-design = store.getState();
+
+     // Page 11 of 11
+    store.dispatch( fixSymbolValue("Force_2",75.1) );
+    design = store.getState();
     expect(design.model.result.objective_value).toBeCloseTo(0.0650272,7);
+
 });
 
