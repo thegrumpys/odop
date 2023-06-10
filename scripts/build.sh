e4ad5271e41ccedfa602897f9371de8218724c0f
Brian D. Watt <bwatt@1fifoto.com>
2023-06-10 11:58:02 -0500

    Restore build.sh to create docs in SpringDesignSoftware/odop

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "BEGIN Build..."
+echo "BEGIN of Build..."
 npm install
 npm run harp-compile
 npm run heroku-postbuild
 rsync -avz --delete /home/mikmil36/build/odop/client/build/* /home/mikmil36/springdesignsoftware.org/odop
-echo "END Build..."
+echo "END of Build..."
