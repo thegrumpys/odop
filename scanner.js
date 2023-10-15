3328f6b32c0289cb6f9ac81b9a2bf2b8f14e15ae
Brian D. Watt <bwatt@1fifoto.com>
2023-10-15 14:08:44 -0500

    862 Replace value "null" in saved models

--- scanner.js
+++ scanner.js
@@ -26,40 +26,79 @@ connection.query(stmt, function(err, rows, fields) {
         value.forEach((row) => {
 //            console.log('symbol_table=',row.value.symbol_table)
             if (row.value.result.objective_value === null) {
-                console.log('0 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=','Objective Value Null value');
+                console.log('0001 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=','Objective Value Null value');
                 count++;
             }
             row.value.symbol_table.forEach((entry) => {
+                // Undefined Checks
+                if (entry.value === undefined) {
+                    console.log('0101 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No value');
+                    count++;
+                }
                 if (entry.lmin === undefined && entry.lmax === undefined) {
-                    console.log('1 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No lmin and lmax');
+                    console.log('0201 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No lmin and lmax');
                     count++;
                 } else if (entry.lmin === undefined) {
-                    console.log('2 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No lmax');
+                    console.log('0202 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No lmax');
                     count++;
                 } else if (entry.lmax === undefined) {
-                    console.log('3 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No lmax');
+                    console.log('0203 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No lmax');
                     count++;
                 }
                 if (entry.cmin === undefined && entry.cmax === undefined) {
-                    console.log('4 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No cmin and cmax');
+                    console.log('0301 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No cmin and cmax');
                     count++;
                 } else if (entry.cmin === undefined) {
-                    console.log('5 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No cmax');
+                    console.log('0302 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No cmax');
                     count++;
                 } else if (entry.cmax === undefined) {
-                    console.log('6 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No cmax');
+                    console.log('0303 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No cmax');
                     count++;
                 }
+                if ((row.type === 'Spring/Compression' && row.value.version >= 12) ||
+                    (row.type === 'Spring/Extension' && row.value.version >= 8) ||
+                    (row.type === 'Spring/Torsion' && row.value.version >= 8)) {
+                    if (typeof entry.value === 'number' && entry.format === undefined) {
+                        if (entry.validmin === undefined && entry.validmax === undefined) {
+                            console.log('0401 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No validmin and validmax');
+                            count++;
+                        } else if (entry.validmin === undefined) {
+                            console.log('0402 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No validmin');
+                            count++;
+                        } else if (entry.validmax === undefined) {
+                            console.log('0403 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No validmax');
+                            count++;
+                        }
+                    }
+                }
+                if (entry.sdlim === undefined) {
+                    console.log('0501 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'No sdlim');
+                    count++;
+                }
+
+                // NULL Checks
                 if (entry.value === null) {
-                    console.log('7 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null value');
+                    console.log('1101 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null value');
                     count++;
                 }
                 if (entry.cmin === null) {
-                    console.log('8 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null cmin');
+                    console.log('1301 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null cmin');
                     count++;
                 }
                 if (entry.cmax === null) {
-                    console.log('9 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null cmax');
+                    console.log('1302 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null cmax');
+                    count++;
+                }
+                if (entry.validmin === null) {
+                    console.log('1401 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null validmin');
+                    count++;
+                }
+                if (entry.validmax === null) {
+                    console.log('1402 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null validmax');
+                    count++;
+                }
+                if (entry.sdlim === null) {
+                    console.log('1501 filetype=',row.type,'filename=',row.name,'user=',row.user,'version=',row.value.version,'name=',entry.name,'input=',entry.input,'Symbol Table Entry Null sdlim');
                     count++;
                 }
             })
