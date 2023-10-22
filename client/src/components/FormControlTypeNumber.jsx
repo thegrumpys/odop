9268ed3823c226f3c64557b532cdfc3bd1534cf7
Brian D. Watt <bwatt@1fifoto.com>
2023-10-22 12:05:50 -0500

    863 Display Infinity & NaN in FormControlTypeNumber

--- client/src/components/FormControlTypeNumber.jsx
+++ client/src/components/FormControlTypeNumber.jsx
@@ -23,23 +23,58 @@ class FormControlTypeNumber extends Component {
         this.onFocus = this.onFocus.bind(this);
         this.onBlur = this.onBlur.bind(this);
         this.onContextMenu = this.onContextMenu.bind(this);
-        var value = parseFloat(this.props.value);
-        this.state = {
-            value: value,
-            valueString: value.toString(), // Update the display
-            isInvalid: false,
-            focused: false,
-        };
+        if (Number.isFinite(this.props.value)) {
+            var value = parseFloat(this.props.value);
+            this.state = {
+                value: value,
+                valueString: value.toString(), // Update the display
+                isInvalid: false,
+                focused: false,
+            };
+	    } else {
+            this.state = {
+                value: this.props.value,
+                valueString: this.props.value.toString(), // Update the display
+                isInvalid: true,
+                focused: false,
+            };
+        }
     }
 
     componentDidUpdate(prevProps) {
-        if (Number.isFinite(this.props.value) && prevProps.value !== this.props.value) {
-//            console.log('In FormControlTypeNumber.componentDidUpdate prevProps.value=',prevProps.value,'props.value=',this.props.value,'value=',this.state.value,'valueString=',this.state.valueString);
-            if (this.props.value !== this.state.value) { // Only update our internal state value if they differ leaving the valueString unchanged.
-                var value = parseFloat(this.props.value);
+        if (Number.isFinite(this.props.value)) {
+            if (prevProps.value !== this.props.value) {
+//                console.log('In FormControlTypeNumber.componentDidUpdate prevProps.value=',prevProps.value,'props.value=',this.props.value,'value=',this.state.value,'valueString=',this.state.valueString);
+                if (this.props.value !== this.state.value) { // Only update our internal state value if they differ leaving the valueString unchanged.
+                    console.log('In FormControlTypeNumber.componentDidUpdate 0 prevProps.value=',prevProps.value,'props.value=',this.props.value,'value=',this.state.value,'valueString=',this.state.valueString);
+                    var value = parseFloat(this.props.value);
+                    this.setState({
+                        value: value,
+                        valueString: value.toString(), // Update the display
+                    });
+                }
+            }
+        } else {
+            if (Number.isNaN(this.props.value) && this.state.valueString !== 'NaN') {
+                console.log('In FormControlTypeNumber.componentDidUpdate 1 prevProps.value=',prevProps.value,'props.value=',this.props.value,'value=',this.state.value,'valueString=',this.state.valueString);
                 this.setState({
-                    value: value,
-                    valueString: value.toString(), // Update the display
+                    value: this.props.value,
+                    valueString: this.props.value.toString(), // Update the display
+                    isInvalid: true,
+                });
+            } else if (this.state.value === Number.POSITIVE_INFINITY && this.state.valueString !== 'Infinity') {
+                console.log('In FormControlTypeNumber.componentDidUpdate 2 prevProps.value=',prevProps.value,'props.value=',this.props.value,'value=',this.state.value,'valueString=',this.state.valueString);
+                this.setState({
+                    value: this.props.value,
+                    valueString: this.props.value.toString(), // Update the display
+                    isInvalid: true,
+                });
+            } else if (this.state.value === Number.NEGATIVE_INFINITY && this.state.valueString !== '-Infinity') {
+                console.log('In FormControlTypeNumber.componentDidUpdate 3 prevProps.value=',prevProps.value,'props.value=',this.props.value,'value=',this.state.value,'valueString=',this.state.valueString);
+                this.setState({
+                    value: this.props.value,
+                    valueString: this.props.value.toString(), // Update the display
+                    isInvalid: true,
                 });
             }
         }
@@ -53,7 +88,7 @@ class FormControlTypeNumber extends Component {
     onChange(event) {
 //        console.log('In FormControlTypeNumber.onChange event.target.value=',event.target.value,'state=',this.state);
         var value = parseFloat(event.target.value);
-        if (!isNaN(value)) {
+        if (Number.isFinite(value)) {
 //            console.log('In FormControlTypeNumber.onChange Valid event.target.value=',event.target.value,'state=',this.state);
             this.setState({
                 value: value,
@@ -73,18 +108,27 @@ class FormControlTypeNumber extends Component {
     }
 
     onFocus(event) {
-//        console.log('In FormControlTypeNumber.onFocus event.target.value=',event.target.value,'state=',this.state);
         if (!this.props.readOnly) {
-            this.setState({
-                valueString: this.state.value.toString(), // Update the display with unformatted value
-                focused: true,
-            });
+            if (Number.isFinite(this.state.value)) {
+                console.log('In FormControlTypeNumber.onFocus 1 event.target.value=',event.target.value,'state=',this.state);
+                this.setState({
+                    focused: true,
+                    valueString: this.state.value.toString(), // Update the display with unformatted value
+                });
+            } else {
+                console.log('In FormControlTypeNumber.onFocus 2 event.target.value=',event.target.value,'state=',this.state);
+                this.setState({
+                    focused: true,
+                    valueString: '', // Update the display with unformatted value
+                    isInvalid: true,
+                });
+            }
         }
         this.props.onFocus(event);
     }
 
     onBlur(event) {
-//        console.log('In FormControlTypeNumber.onBlur event.target.value=',event.target.value,'state=',this.state);
+        console.log('In FormControlTypeNumber.onBlur event.target.value=',event.target.value,'state=',this.state);
         if (this.state.isInvalid) {
             event.target.value = this.state.value.toString();
             this.props.onChangeValid(event); // Pass valid number onward
@@ -107,8 +151,8 @@ class FormControlTypeNumber extends Component {
 //        console.log('In FormControlTypeNumber.render value=',this.state.value,'valueString=',this.state.valueString);
 //        console.log('In FormControlTypeNumber.render className=',this.props.className);
         var className = (this.props.className !== undefined ? this.props.className : '') + ' text-right';
-        if (this.state.focused && isNaN(parseFloat(this.state.valueString))) {
-            className += ' borders-invalid';
+        if (this.state.focused && !Number.isFinite(parseFloat(this.state.valueString))) {
+            className += ' borders-invalid text-invalid';
         }
         var icon_alerts = this.props.icon_alerts; // start with the icon alerts 
         if (icon_alerts === undefined) {
@@ -143,14 +187,14 @@ class FormControlTypeNumber extends Component {
                 </OverlayTrigger>
             :
             ''}
-            <Form.Control type="number"
+            <Form.Control type={Number.isFinite(this.state.value) ? 'number' : ''}
                 {...p} // Allow OverlayTrigger to pass-in other props
                 onClick={this.onClick}
                 onChange={this.onChange}
                 onFocus={this.onFocus}
                 onBlur={this.onBlur}
                 className={className}
-                value={this.props.disabledText ? '' : this.state.focused ? this.state.valueString : (Number.isFinite(this.state.value) ? this.state.value.toODOPPrecision() : '')} />
+                value={this.props.disabledText ? '' : this.state.focused ? this.state.valueString : (Number.isFinite(this.state.value) ? this.state.value.toODOPPrecision() : this.state.valueString)} />
         </>)
     }
 }
--- client/src/designtypes/Spring/Compression/view.js
+++ client/src/designtypes/Spring/Compression/view.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DesignTable from "../../../components/DesignTable"
+import TestView from "../../../components/TestView"
 import Calculator from "./Calculator"
 import Report1 from "./Report1"
 import Report2 from "./Report2"
@@ -7,6 +8,7 @@ import Report3 from "./Report3"
 
 export function getViewNames() {
     return [
+        { name: "Test", title: 'Test', component: <TestView /> },
         { name: "Advanced", title: 'Advanced', component: <DesignTable /> },
         { name: "Calculator", title: 'Calculator', component: <Calculator /> },
         { name: "Report1", title: 'Report 1 (mini)', component: <Report1 /> },
