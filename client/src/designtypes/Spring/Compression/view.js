7924bde39fbf74565f4c901ad6bfe4d9d4e6ab8d
Brian D. Watt <bwatt@1fifoto.com>
2023-10-22 13:07:24 -0500

    863 Display Infinity & NaN in FormControlTypeNumber

--- client/src/designtypes/Spring/Compression/view.js
+++ client/src/designtypes/Spring/Compression/view.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import DesignTable from "../../../components/DesignTable"
-import TestView from "../../../components/TestView"
 import Calculator from "./Calculator"
 import Report1 from "./Report1"
 import Report2 from "./Report2"
@@ -8,7 +7,6 @@ import Report3 from "./Report3"
 
 export function getViewNames() {
     return [
-        { name: "Test", title: 'Test', component: <TestView /> },
         { name: "Advanced", title: 'Advanced', component: <DesignTable /> },
         { name: "Calculator", title: 'Calculator', component: <Calculator /> },
         { name: "Report1", title: 'Report 1 (mini)', component: <Report1 /> },
