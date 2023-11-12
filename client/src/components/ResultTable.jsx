9b49ccae1f2777a93ba04f03dff4d0fbada6b35d
Brian D. Watt <bwatt@1fifoto.com>
2023-11-12 13:06:02 -0600

    869 Interim enhancements to ResultTable

--- client/src/components/ResultTable.jsx
+++ client/src/components/ResultTable.jsx
@@ -202,12 +202,12 @@ class ResultTable extends Component {
                 <Table className="col-md-8" size="sm">
                     <tbody>
                         <tr>
-                            <th width="33%" id="Feasibility">
+                            <th id="Feasibility">
                                 <OverlayTrigger placement="bottom" overlay={<Tooltip>NOT FEASIBLE: constraints significantly violated; <br />CLOSE TO FEASIBLE: constraints slightly violated; FEASIBLE: constraints not significantly violated; STRICTLY FEASIBLE: no constraints violated</Tooltip>}>
                                     <span>Feasibility</span>
                                 </OverlayTrigger>
                             </th>
-                            <td width="67%" className={feasibility_class + " text-left"}>
+                            <td className={feasibility_class + " text-left"}>
                                 {feasibility_string}
                                 {feasibility_string === 'NOT FEASIBLE' && this.props.search_completed ?
                                     <OverlayTrigger placement="bottom" overlay={<Tooltip>
@@ -222,12 +222,12 @@ class ResultTable extends Component {
                             </td>
                         </tr>
                         <tr>
-                            <th width="33%" id="Message" className="pb-3">
+                            <th width="10%" id="Message" className="pb-3">
                                 <OverlayTrigger placement="top" overlay={<Tooltip>Status feedback message from solution process</Tooltip>}>
-                                    <span>Message:</span>
+                                    <span>Message</span>
                                 </OverlayTrigger>
                             </th>
-                            <td width="67%" className="text-left">{this.props.termination_condition}</td>
+                            <td className="text-left">{this.props.termination_condition}</td>
                        </tr>
                     </tbody>
                 </Table>
