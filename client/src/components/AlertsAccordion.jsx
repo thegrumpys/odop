ed48a1d339db2593c766a0182310b7e7f565a0d1
Mike <Mike@Win11VM2.jardin.local>
2023-12-08 15:01:00 -0800

    890 Improve tooltip formatting
    Repair typo.
    Adjust tooltip text for better readability.

--- client/src/components/AlertsAccordion.jsx
+++ client/src/components/AlertsAccordion.jsx
@@ -157,10 +157,10 @@ class AlertsAccordion extends Component {
                                     <Tooltip className="tooltip-lg">
                                         <p>ALERTS are messages produced in response to design changes.</p>
                                         <p>Severity:<ul>
-	                                        <li>Error alerts <b>(Err)</b> indicate a value outside its validity range.</li>
-	                                        <li>Warning alerts <b>(Warn)</b> are produced if the relationship between two values is incorrect.</li>
-	                                        <li>Notice alerts <b>(Notice)</b> summarize violated minimum or maximum constraints.</li>
-	                                        <li>Informational alerts <b>(Info)</b> highlight other general conditions.</li>
+	                                        <li><b>Err</b> - Error alerts indicate a value outside its validity range.</li>
+	                                        <li><b>Warn</b> - Warning alerts are produced if the relationship between two values is incorrect.</li>
+	                                        <li><b>Notice</b> - Notice alerts summarize violated minimum or maximum constraints.</li>
+	                                        <li><b>Info</b> - Informational alerts highlight other general conditions.</li>
                                         </ul></p>
                                         <p>A red "badge" on each Alert button indicates the total number of alerts of this type.
                                         Close the Alerts panel by a second click on the same Alerts button.</p>
@@ -198,7 +198,7 @@ class AlertsAccordion extends Component {
                                             </th>
                                             <th>
                                                 <OverlayTrigger placement="bottom" overlay={<Tooltip className="tooltip-lg">
-                                 <p> className="text-left">Severity<ul>
+				                                    <p className="text-left">Severity:<ul>
                                                         <li>Error (Err):</li>
                                                         value outside valid range
                                                         <li>Warning (Warn):</li>
