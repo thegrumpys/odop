630a186b6032f2323093909f635ea3db1229b2d1
Brian D. Watt <bwatt@1fifoto.com>
2024-02-11 11:43:53 -0600

    787 Investigate creating a worker thread

--- client/public/x.js
+++
@@ -1,6 +0,0 @@
-export function x(design) {
-    console.log('In x design=',design);
-    design.name = "Mike";
-    console.log('In x design.model.symbol_table[0].value=',design.model.symbol_table[0].value);
-    design.model.symbol_table[0].value=1.2;
-}
