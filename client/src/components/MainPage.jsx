7b2820a66172fa1274fee0d304a5b8359387c6ea
Brian D. Watt <bwatt@1fifoto.com>
2024-03-09 13:04:38 -0600

    441 Migrate to latest version of react...

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -12,6 +12,7 @@ import {
     Row
 } from 'react-bootstrap';
 import { changeView } from '../store/modelSlice';
+import HelpAbout from '../menus/Help/HelpAbout';
 import config from '../config';
 
 export default function MainPage() {
@@ -38,7 +39,7 @@ export default function MainPage() {
 //    console.log('In MainPage.setView view=',view);
     dispatch(changeView(view)); // Update the model
   }
-//
+
 //  console.log('In MainPage.render');
   if (type === null) return null;
   var { getViewNames } = require('../designtypes/'+type+'/view.js'); // Dynamically load getViewNames
@@ -50,14 +51,38 @@ export default function MainPage() {
 
   return (
     <>
-      <Container style={{backgroundColor: '#eee', paddingTop: '60px'}}>
+      <Navbar variant="light" bg="light" expand="md" fixed="top">
+        <OverlayTrigger placement="bottom" overlay={<Tooltip>Reset app.<br />Save your work first!<br />See Help AutoSave.</Tooltip>}>
+          <Navbar.Brand href="/"><img className="d-none d-md-inline" src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon" />ODOP</Navbar.Brand>
+        </OverlayTrigger>
+        <Navbar.Toggle onClick={toggle} />
+        <Navbar.Collapse in={show}>
+          <Nav className="mr-auto">
+            <NavDropdown title="Help">
+              <HelpAbout />
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Item className="d-flex align-items-center">
+              <a href={"/docs/Help/DesignTypes/" + type + "/description.html"} target="_blank" rel="noopener noreferrer">
+                <OverlayTrigger placement="bottom" overlay={<Tooltip>Design type is {type}. Select icon for full description.</Tooltip>}>
+                  <img className="d-none d-md-inline" src={src} alt={alt} height="30px" />
+                </OverlayTrigger>
+              </a>
+              &nbsp;{name}
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container style={{ backgroundColor: '#eee', paddingTop: '60px' }}>
         <Tabs defaultActiveKey={config.url.view} activeKey={activeTab}>
-          {viewNames.map((element) => {return (
-            <Tab title={element.title} key={element.title} eventKey={element.name}>
-              <div id={'main_'+element.name}>
-                {element.component}
-              </div>
-            </Tab>
+          {viewNames.map((element) => {
+            return (
+              <Tab title={element.title} key={element.title} eventKey={element.name}>
+                <div id={'main_' + element.name}>
+                  {element.component}
+                </div>
+              </Tab>
             );
           })}
         </Tabs>
--- client/src/menus/Help/HelpAbout.jsx
+++ client/src/menus/Help/HelpAbout.jsx
@@ -1,132 +1,113 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Modal, NavDropdown } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { version } from '../../version';
+import { version as release_version } from '../../version';
 import { logUsage } from '../../logUsage';
-import { withOktaAuth } from '@okta/okta-react';
+//import { withOktaAuth } from '@okta/okta-react';
 import config from '../../config';
 import { displayMessage } from '../../components/MessageModal';
 import { displaySpinner } from '../../components/Spinner';
 
-class HelpAbout extends Component {
+export default function HelpAbout() {
+  console.log("HelpAbout - Mounting...");
+  const [show, setShow] = useState(false);
+  const [sizes, setSizes] = useState('');
+  const [size, setSize] = useState('');
+  const user = useSelector((state) => state.model.user);
+  const type = useSelector((state) => state.model.model.type);
+  const version = useSelector((state) => state.model.model.version);
+  const jsontype = useSelector((state) => state.model.model.jsontype);
+  const units = useSelector((state) => state.model.model.units);
+  const dispatch = useDispatch();
 
-    constructor(props) {
-//        console.log("In HelpAbout.constructor props=",props);
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.getDBSize = this.getDBSize.bind(this);
-        this.state = {
-            modal: false,
-            sizes: '',
-            size: ''
-        };
-    }
+  useEffect(() => {
+    console.log("HelpAbout - Mounted");
+    getDBSize(user);
+    return () => console.log("HelpAbout - Unmounting ...");
+//    return () => {};
+  }, []);
 
-    componentDidMount() {
-//        console.log('In HelpAbout.componentDidMount this=',this);
-        this.getDBSize(this.props.user);
-    }
+  const toggle = () => {
+    console.log('In HelpAbout.toggle');
+    setShow(!show);
+    if (show) logUsage('event', 'HelpAbout', { event_label: 'HelpAbout' });
+  }
 
-    toggle() {
-        this.setState({
-            modal: !this.state.modal
-        });
-        if (this.state.modal) logUsage('event', 'HelpAbout', { event_label: 'HelpAbout'});
-    }
+  const getDBSize = (user) => {
+    console.log('In HelpAbout.getDBSize');
+    displaySpinner(true);
+    fetch('/api/v1/db_size', {
+      headers: {
+        Authorization: 'Bearer ' + user
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw Error(res.statusText);
+        }
+        return res.json()
+      })
+      .then(sizes => {
+        console.log('In HelpAbout.getSize sizes=', sizes);
+        setSizes(sizes);
+        var size = '';
+        if (sizes.length > 0) {
+          size = sizes[0]; // Default to first name
+        }
+        console.log('In HelpAbout.getSize size=', size);
+        setSize(size);
+        logUsage('event', 'HelpAbout', { event_label: 'getDBSize: ' + size });
+      })
+      .catch(error => {
+        displayMessage('GET of DB Size failed with message: \'' + error.message + '\'');
+      })
+      .finally(() => {
+        displaySpinner(false);
+      });
+  }
 
-    getDBSize(user) {
-//        console.log('In HelpAbout.getSize');
-        displaySpinner(true);
-        fetch('/api/v1/db_size', {
-            headers: {
-                Authorization: 'Bearer ' + this.props.user
-            }
-        })
-        .then(res => {
-            if (!res.ok) {
-                throw Error(res.statusText);
-            }
-            return res.json()
-        })
-        .then(sizes => {
-//            console.log('In HelpAbout.getSize sizes=',sizes);
-            this.setState({
-                sizes: sizes
-            });
-            var size = '';
-            if (this.state.sizes.length > 0)
-                size = this.state.sizes[0]; // Default to first name
-//            console.log('In HelpAbout.getSize size=',size);
-            this.setState({ 
-                size: size
-            });
-            logUsage('event', 'HelpAbout', { event_label: 'getDBSize: ' + size});
-        })
-        .catch(error => {
-            displayMessage('GET of DB Size failed with message: \''+error.message+'\'');
-        })
-        .finally(() => {
-            displaySpinner(false);
-        });
-    }
+                                /* User Authenticated: {this.props.authState.isAuthenticated ? 'true' : 'false'}<br /> */
+                                /* User Email: {this.props.authState.isAuthenticated ? this.props.authState.idToken.claims.email : 'Unknown'}<br /> */
 
-    render() {
-//        console.log('In HelpAbout.render this=',this);
-        return (
+  return (
+    <>
+      <NavDropdown.Item onClick={toggle}>
+        About
+      </NavDropdown.Item>
+      <Modal show={show} onHide={toggle}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon" />  &nbsp; About ODOP
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Link to <a href="/docs/About/" target="_blank" rel="noopener noreferrer">About</a> topics
+          <br />
+          Link to <a href={'/docs/Help/DesignTypes/' + type + '/description.html'} target="_blank" rel="noopener noreferrer">{type} Design Type</a> description
+          <br />
+          Link to <a href="https://www.springdesignsoftware.org/" target="_blank" rel="noopener noreferrer">Spring Design Software Website</a> home page
+          <hr />
+          This is <a href="https://en.wikipedia.org/wiki/Open-source_software" target="_blank" rel="noopener noreferrer">Open Source </a> software.
+          <br />
+          About <a href="https://en.wikipedia.org/wiki/MIT_License" target="_blank" rel="noopener noreferrer">MIT License</a>
+          &nbsp; &nbsp; &nbsp; &nbsp;
+          <a href="https://github.com/thegrumpys/odop/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">ODOP License</a>
+          <hr />
+          ODOP Software Version: {release_version()}<br />
+          {config.node.env !== "production" &&
             <>
-                <NavDropdown.Item onClick={this.toggle}>
-                    About
-                </NavDropdown.Item>
-                <Modal show={this.state.modal} onHide={this.toggle}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>
-                            <img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon"/>  &nbsp; About ODOP
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        Link to <a href="/docs/About/" target="_blank" rel="noopener noreferrer">About</a> topics
-                        <br/>
-                        Link to <a href={'/docs/Help/DesignTypes/' + this.props.type + '/description.html'} target="_blank" rel="noopener noreferrer">{this.props.type} Design Type</a> description
-                        <br/>
-                        Link to <a href="https://www.springdesignsoftware.org/" target="_blank" rel="noopener noreferrer">Spring Design Software Website</a> home page
-                        <hr/>
-                        This is <a href="https://en.wikipedia.org/wiki/Open-source_software" target="_blank" rel="noopener noreferrer">Open Source </a> software.
-                        <br/>
-                        About <a href="https://en.wikipedia.org/wiki/MIT_License" target="_blank" rel="noopener noreferrer">MIT License</a>
-                        &nbsp; &nbsp; &nbsp; &nbsp;
-                        <a href="https://github.com/thegrumpys/odop/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">ODOP License</a>
-                        <hr/>
-                        ODOP Software Version: {version()}<br />
-                        {config.node.env !== "production" &&
-                            <>
-                                User Authenticated: {this.props.authState.isAuthenticated ? 'true' : 'false'}<br />
-                                User Email: {this.props.authState.isAuthenticated ? this.props.authState.idToken.claims.email : 'Unknown'}<br />
-                                User ClientId: {this.props.user === null ? 'Unknown' : this.props.user}<br />
-                            </>
-                        }
-                        Model: {this.props.jsontype} {this.props.type}<br />
-                        Model Units: {this.props.units}<br />
-                        Model Version: {this.props.version}<br />
-                        {config.node.env !== "production" && <span>DB Size: {this.state.size} MB</span>}
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="primary" onClick={this.toggle}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
+              User ClientId: {user === null ? 'Unknown' : user}<br />
             </>
-        );
-    }
+          }
+          Model: {jsontype} {type}<br />
+          Model Units: {units}<br />
+          Model Version: {version}<br />
+          {config.node.env !== "production" && <span>DB Size: {size} MB</span>}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={toggle}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
-
-const mapStateToProps = state => ({
-    user: state.user,
-    type: state.model.type,
-    version: state.model.version,
-    jsontype: state.model.jsontype,
-    units: state.model.units,
-});
-
-export default withOktaAuth(
-    connect(
-        mapStateToProps)(HelpAbout)
-);
