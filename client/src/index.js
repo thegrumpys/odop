3869f4f8c500a2062e8a2d8c4d07d8a436171717
Brian D. Watt <bwatt@1fifoto.com>
2024-10-20 07:05:22 -0500

    921 Improve Performance of our use of Redux Tool Kit (RTK)

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App2 from "./components/App2";
+import App from "./components/App";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import './odop.css';
 import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { initialSystemControls } from './initialSystemControls';
-import config from './config';
 
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App2 />
+      <App />
     </BrowserRouter>
   </Provider>
 );
