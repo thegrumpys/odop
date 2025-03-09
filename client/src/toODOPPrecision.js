b672d7c49ccabbccac839e55077e882cd01fdc27
Brian D. Watt <bwatt@1fifoto.com>
2025-03-09 12:12:06 -0500

    978 Scrollable catalog listing - Developer View menu

--- client/src/toODOPPrecision.js
+++ client/src/toODOPPrecision.js
@@ -1,19 +1,23 @@
 export function toODOPPrecision(value) {
     var odopValue;
-    if (Math.abs(value) < 10000.0 || Math.abs(value) >= 1000000.0) {
-        if (value === Number.POSITIVE_INFINITY) {
-            odopValue = value.toPrecision(4);
-        } else if (value >= 1.7975e+308) {
-            odopValue = "1.797e+308";
-        } else if (value === Number.NEGATIVE_INFINITY) {
-            odopValue = value.toPrecision(4);
-        } else if (value <= -1.7975e+308) {
-            odopValue = "-1.797e+308";
-        } else {
-            odopValue = value.toPrecision(4);
-        }
+    if (typeof value === 'number') {
+      if (Math.abs(value) < 10000.0 || Math.abs(value) >= 1000000.0) {
+          if (value === Number.POSITIVE_INFINITY) {
+              odopValue = value.toPrecision(4);
+          } else if (value >= 1.7975e+308) {
+              odopValue = "1.797e+308";
+          } else if (value === Number.NEGATIVE_INFINITY) {
+              odopValue = value.toPrecision(4);
+          } else if (value <= -1.7975e+308) {
+              odopValue = "-1.797e+308";
+          } else {
+              odopValue = value.toPrecision(4);
+          }
+      } else {
+          odopValue = value.toFixed(0);
+      }
     } else {
-        odopValue = value.toFixed(0);
+      odopValue = value;
     }
     return odopValue;
 };
