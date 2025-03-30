3e6dc7249ec11f30c0eeebf5c0a591a9740f5ff5
Brian D. Watt <bwatt@1fifoto.com>
2025-03-29 19:35:06 -0500

    964 Single script/batch_file for multiple DB ops; procedure doc update

--- scripts/dump_db_startup_files.sh
+++ scripts/dump_db_startup_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Perform dump [backup] operation on Startup designs to create load.sql files.
-# Hard coded for B. Watt oktauserid 'oktauserid'
+# Hard coded for B. Watt oktauserid '00u1itcx44XGp65ln357'
 
 if [ -z "$1" ]; then
   ECHO "USAGE:  dump_db_startup_files type"
   ECHO "        where \"type\" is the system type: \"local\", \"development\", \"test\", \"staging\" or \"production\""
   ECHO ""
   ECHO "Perform dump [backup] operation on Startup designs to create load.sql files."
-  ECHO "Hard coded for B. Watt oktauserid 'oktauserid'
+  ECHO "Hard coded for B. Watt oktauserid '00u1itcx44XGp65ln357'"
   exit 1
 fi
 
@@ -29,19 +29,19 @@ esac
 # Info@SpringDesignSoftware.org
 oktauserid=00u1g7vr21d7yajY4357
 # bwatt@1fifoto.com
-oktauserid=oktauserid
+oktauserid=00u1itcx44XGp65ln357
 
-mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where="(user='$oktauserid' AND type='Piston-Cylinder' AND name='Startup') OR (user='$oktauserid' AND type='Piston-Cylinder' AND name='Startup_Metric')" > ./designtypes/Piston-Cylinder/load.sql
+mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where="(user=$oktauserid AND type='Piston-Cylinder' AND name='Startup') OR (user=\'$oktauserid\' AND type='Piston-Cylinder' AND name='Startup_Metric')" > ./designtypes/Piston-Cylinder/load.sql
 if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
 
-mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where="(user='$oktauserid' AND type='Solid' AND name='70PoundGoldBar') OR (user='$oktauserid' AND type='Solid' AND name='StandardGoldBar') OR (user='$oktauserid' AND type='Solid' AND name='Startup') OR (user='$oktauserid' AND type='Solid' AND name='Startup_Metric') OR (user='$oktauserid' AND type='Solid' AND name='USPS_MachinableParcels') OR (user='$oktauserid' AND type='Solid' AND name='USPS_MaxVolume')" > ./designtypes/Solid/load.sql
-if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
+#mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where=\"(user='$oktauserid' AND type='Solid' AND name='70PoundGoldBar') OR (user='$oktauserid' AND type='Solid' AND name='StandardGoldBar') OR (user='$oktauserid' AND type='Solid' AND name='Startup') OR (user='$oktauserid' AND type='Solid' AND name='Startup_Metric') OR (user='$oktauserid' AND type='Solid' AND name='USPS_MachinableParcels') OR (user='$oktauserid' AND type='Solid' AND name='USPS_MaxVolume')\" > ./designtypes/Solid/load.sql
+#if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
 
-mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where="(user='$oktauserid' AND type='Spring/Compression' AND name='Demo') OR (user='$oktauserid' AND type='Spring/Compression' AND name='HotWound') OR (user='$oktauserid' AND type='Spring/Compression' AND name='HotWoundMetric') OR (user='$oktauserid' AND type='Spring/Compression' AND name='Startup') OR (user='$oktauserid' AND type='Spring/Compression' AND name='Startup_Metric')" > ./designtypes/Spring/Compression/load.sql
-if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
+#mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where=\"(user='$oktauserid' AND type='Spring/Compression' AND name='Demo') OR (user='$oktauserid' AND type='Spring/Compression' AND name='HotWound') OR (user='$oktauserid' AND type='Spring/Compression' AND name='HotWoundMetric') OR (user='$oktauserid' AND type='Spring/Compression' AND name='Startup') OR (user='$oktauserid' AND type='Spring/Compression' AND name='Startup_Metric')\" > ./designtypes/Spring/Compression/load.sql
+#if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
 
-mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where="(user='$oktauserid' AND type='Spring/Extension' AND name='Startup') OR (user='$oktauserid' AND type='Spring/Extension' AND name='Startup_Metric')" > ./designtypes/Spring/Extension/load.sql
-if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
+#mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where=\"(user='$oktauserid' AND type='Spring/Extension' AND name='Startup') OR (user='$oktauserid' AND type='Spring/Extension' AND name='Startup_Metric')\" > ./designtypes/Spring/Extension/load.sql
+#if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
 
-mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where="(user='$oktauserid' AND type='Spring/Torsion' AND name='Startup') OR (user='$oktauserid' AND type='Spring/Torsion' AND name='Startup_Metric')" > ./designtypes/Spring/Torsion/load.sql
-if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
+#mysqldump --user=$user --password=$password --host=$host --skip-ssl $database design --skip-opt --complete-insert --no-create-info --compact --no-tablespaces --where=\"(user='$oktauserid' AND type='Spring/Torsion' AND name='Startup') OR (user='$oktauserid' AND type='Spring/Torsion' AND name='Startup_Metric')\" > ./designtypes/Spring/Torsion/load.sql
+#if [ $? -ne 0 ]; then echo "dump_db_startup_files: mysqldump returned $?"; else echo "mysqldump returned SUCCESS"; fi
