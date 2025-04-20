6c4d139881dd5877f20c4afbe705329a498971c0
Brian D. Watt <bwatt@1fifoto.com>
2025-04-20 13:47:26 -0500

    880 Implement Auto Search

--- client/src/components/NameValueUnitsRowCalcInput.jsx
+++ client/src/components/NameValueUnitsRowCalcInput.jsx
@@ -82,8 +82,8 @@ export default function NameValueUnitsRowCalcInput({ element, index, onChangeVal
     var selectedIndex = parseFloat(event.target.value);
     dispatch(changeSymbolValue(element.name, selectedIndex));
     logValue(element.name, selectedIndex, 'TableIndex');
-    console.log('In NameValueUnitsRowCalcInput.onSelectLocal','model_enable_auto_search=', model_enable_auto_search,'model_objective_value >= model_objmin=',model_objective_value >= model_objmin);
-    if (model_enable_auto_search && model_objective_value >= model_objmin) {
+    console.log('In NameValueUnitsRowCalcInput.onSelectLocal','model_enable_auto_search=', model_enable_auto_search,'valueChanged=',value !== element.value,'model_objective_value >= model_objmin=',model_objective_value >= model_objmin);
+    if (model_enable_auto_search && value !== element.value && model_objective_value >= model_objmin) {
       dispatch(search());
     }
     if (typeof onSelect === "function") onSelect(event);
