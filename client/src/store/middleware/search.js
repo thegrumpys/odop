b7a8eb62575572719469f8a36d26795f70f92d2b
Brian D. Watt <bwatt@1fifoto.com>
2025-06-01 17:55:58 -0500

    986 Auto Standard Size

--- client/src/store/middleware/search.js
+++ client/src/store/middleware/search.js
@@ -153,7 +153,7 @@ export function search(store, objmin, merit) {
     //        console.log('In 2','shadow_store_state_2.model.result.objective_value=',shadow_store_state_2.model.result.objective_value);
             if (shadow_store_state_2.model.result.objective_value <= shadow_store_state_2.model.system_controls.objmin) { // If feasible
               p = [...p_2];
-              ncode = ncode_2 + ' Wire_Dia set to Standard Size ' + nearest_sizes[1].toString();
+              ncode = ncode_2 + ' Wire_Dia set to Standard Size ' + nearest_sizes[0].toString();
     //          console.log('After 2','p=',p,'ncode=',ncode);
             }
           } else { // If feasible to begin with then no need to call patsh, just set p from symbol table
