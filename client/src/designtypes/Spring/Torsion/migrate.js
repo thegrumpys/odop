910c825adaa28719609ef2f20f05e65606e29a9b
Brian D. Watt <bwatt@1fifoto.com>
2025-06-07 16:31:47 -0500

    880 Implement Auto Search

--- client/src/designtypes/Spring/Torsion/migrate.js
+++ client/src/designtypes/Spring/Torsion/migrate.js
@@ -323,22 +323,16 @@ export function migrate(design) {
               "Default values in the internal materials table have changed to allow higher stresses in torsion springs. If your previously \"FEASIBLE\" design is now \"NOT FEASIBLE\", use the Help button below for more information.",
               'info', '', '/docs/Help/DesignTypes/Spring/Torsion/description.html#t_allowableStressUpdate');
         }
-        migrated_design.version = '10'; // last thing... set the migrated model version
-
-    case '10':
-        // console.log('Convert from 10 to 11');
-        // Nothing to do
-        migrated_design.version = '11';
-
-    case '11':
-        // console.log('Convert from 11 to 12');
         design.system_controls.enable_auto_search = 1; // Default to auto search on
-        // migrated_design.version = '12'; // uncomment when there is a case below this line
+        migrated_design.version = '10';
+    case '10':
 
-    // case 'N':
-        // console.log('Convert from N to N+1');
-        // Write the migration code here, but leave the version change line commented
-        // migrated_design.version = 'N+1'; // uncomment when there is a case below this line
+    //============BLOCK OF CODE TO REPLICATE============
+    //     console.log('Convert from N to N+1');
+    //     Write the migration code here
+    //     migrated_design.version = 'N+1';
+    // case 'N+1':
+    //==================================================
 
         break; // Do not copy this break
     default: // Unknown
