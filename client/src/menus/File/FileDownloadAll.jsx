0e292856006d8c2e244d9bb1c2f4c24f49550945
Brian D. Watt <bwatt@1fifoto.com>
2025-06-22 17:29:07 -0500

    1009 Export all feature

--- client/src/menus/File/FileDownloadAll.jsx
+++ client/src/menus/File/FileDownloadAll.jsx
@@ -31,6 +31,7 @@ export default function FileDownloadAll() {
       return;
     }
     try {
+      displaySpinner(true);
       let currentDate = new Date();
       const year = currentDate.getFullYear();
       const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-indexed
@@ -101,6 +102,8 @@ export default function FileDownloadAll() {
       });
     } catch (error) {
       displayMessage('Error fetching and downloading files for download: ' + error);
+    } finally {
+      displaySpinner(false);
     }
   }
 
