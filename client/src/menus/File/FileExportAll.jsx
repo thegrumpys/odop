122d9bf47dcf51f7122b36d824e102244eddf942
Brian D. Watt <bwatt@1fifoto.com>
2025-06-22 10:56:42 -0500

    1009 Export all feature

--- client/src/menus/File/FileExportAll.jsx
+++ client/src/menus/File/FileExportAll.jsx
@@ -13,21 +13,33 @@ export default function FileExportAll() {
   const user = useSelector((state) => state.user);
   console.log('user=',user);
 
-  const exportFile = (model, name, file_type) => {
-//        console.log('In FileExportAll.exportFile model=',model);
+  const downloadFile = (model, file_name, file_type) => {
+//    console.log('In FileExportAll.downloadFile','model=',model,'file_name=',file_name,'file_type=',file_type);
     const url = window.URL.createObjectURL(model);
-//        console.log('In FileExportAll.exportFile','url=', url);
+//    console.log('In FileExportAll.downloadFile','url=', url);
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', name + '.' + file_type);
-//        console.log('In FileExportAll.exportFile','link=', link);
+    link.setAttribute('download', file_name + '.' + file_type);
+//    console.log('In FileExportAll.downloadFile','link=', link);
     document.body.appendChild(link);
     link.click();
   }
 
-
   const onExportAll = async () => {
+    if (user === null) {
+      displayMessage('You are not signed in so your files to export are not accessible, terminating');
+      return;
+    }
     try {
+      let currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-indexed
+      const day = currentDate.getDate().toString().padStart(2, '0');
+      const hours = currentDate.getHours().toString().padStart(2, '0');
+      const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+      const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+      const currentDateString = `${year}${month}${day}${hours}${minutes}${seconds}`;
+
       const allFileContentPromises = [];
 
       var zip = new JSZip();
@@ -61,7 +73,7 @@ export default function FileExportAll() {
           const fileContent = await response.json(); // or use .arrayBuffer(), etc.
           console.log('fileContent=',fileContent);
           return {
-            fileName: `${type}/${fileName.name}`,
+            fileName: `ODOP_${currentDateString}/${type}/${fileName.name}`,
             fileContent
           };
         });
@@ -72,19 +84,23 @@ export default function FileExportAll() {
       // Step 3: resolve all fetches
       const fileData = await Promise.all(allFileContentPromises);
       console.log('fileData=',fileData);
+      if (fileData.length === 0) {
+        displayMessage('No files to export were found, terminating');
+        return;
+      }
       for (const fileDatum of fileData) {
         zip.file(fileDatum.fileName + '.json', new Blob([JSON.stringify(fileDatum.fileContent, null, 2)]));
       }
+
       // Download the Blob
       zip.generateAsync({type:"blob"})
       .then(function(content) {
         console.log('content=',content);
-        exportFile(content, 'odop_export_all', 'zip');
+        downloadFile(content, 'odop_export_all_'+currentDateString, 'zip');
         logUsage('event', 'FileExportAll', { event_label: '' });
       });
     } catch (error) {
-      console.error('Error fetching files:', error);
-      throw error;
+      displayMessage('Error fetching and downloading files to export: ' + error);
     }
   }
 
