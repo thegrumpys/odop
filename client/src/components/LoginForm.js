5553e09bbd2e70d317563460b03dd3cda1c22190
Brian D. Watt <bwatt@1fifoto.com>
2025-07-01 13:15:22 -0500

    1008 Okta migration

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -31,48 +31,14 @@ export default function LoginForm() {
 
   return (
     <form onSubmit={handleLogin}>
-      <Container className="pt-5">
-        <Row>
-          <Col lg="4" />
-          <Col lg="4">
-            <Table border="1" borderless className="p-5">
-              <tbody>
-                <tr>
-                  <td className="text-center pt-3 px-5"><img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon" /></td>
-                </tr>
-                <tr>
-                  <td className="text-center"><h3>Sign in to ODOP</h3></td>
-                </tr>
-                <tr>
-                  <td className="px-5 text-start">Username<br /><Form.Control type="text" value={email} onChange={e => setEmail(e.target.value)} /></td>
-                </tr>
-                <tr>
-                  <td className="px-5 text-start">Password<br /><Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} /></td>
-                </tr>
-                <tr>
-                  <td className="text-center"><Button variant="primary" type="submit">Sign In</Button></td>
-                </tr>
-                <tr>
-                  <td className="text-start px-5 pt-3"><Link to="/reset-password">Forgot Password?</Link></td>
-                </tr>
-                <tr>
-                  <td className="text-start px-5"><a href="/docs/About/userAccounts.html" target="_blank">About ODOP user accounts</a></td>
-                </tr>
-                <tr>
-                  <td className="text-start px-5"><a href="https://thegrumpys.github.io/odop/About/messageOfTheDay.html" target="_blank">ODOP Message Of The Day</a></td>
-                </tr>
-                <tr>
-                  <td className="text-start px-5"><a href="/docs/About" target="_blank">Learn about ODOP</a></td>
-                </tr>
-                <tr>
-                  <td className="text-center p-3">Don't have an account? <Link to="/register">Sign up</Link></td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-          <Col lg="4" />
-        </Row>
-      </Container>
+      <h2>Login</h2>
+      <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+      <button type="submit">Login</button>
+      <br />
+      Don't have an account? <Link to="/register">Sign up</Link>
+      <br />
+      <Link to="/reset-password">Forgot Password?</Link>
     </form>
   );
 }
