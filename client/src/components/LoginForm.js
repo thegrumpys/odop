ec7dc698d0f4e35e3804e179345bfe161e54ca66
Brian D. Watt <bwatt@1fifoto.com>
2025-06-30 21:45:49 -0500

    1008 Okta migration

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -32,44 +32,46 @@ export default function LoginForm() {
   return (
     <form onSubmit={handleLogin}>
       <Container className="pt-5">
-      <Row>
-      <Col lg="4" />
-      <Col lg="4">
-      <Table border="1" className="p-5">
-      <tr>
-        <td className="text-center pt-3 px-5"><img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon" /><br /><hr /></td>
-      </tr>
-      <tr>
-        <td className="text-center"><h3>Sign in to ODOP</h3></td>
-      </tr>
-        <tr>
-          <td className="px-5 text-start">Username<br /><Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} /></td>
-        </tr>
-        <tr>
-          <td className="px-5 text-start">Password<br /><Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} /></td>
-        </tr>
-        <tr>
-          <td className="text-center"><Button variant="primary" type="submit">Sign In</Button></td>
-        </tr>
-        <tr>
-          <td className="text-start px-5 pt-3"><Link to="/reset-password">Forgot Password?</Link></td>
-        </tr>
-        <tr>
-          <td className="text-start px-5"><a href="/docs/About/userAccounts.html" target="_blank">About ODOP user accounts</a></td>
-        </tr>
-        <tr>
-          <td className="text-start px-5"><a href="https://thegrumpys.github.io/odop/About/messageOfTheDay.html" target="_blank">ODOP Message Of The Day</a></td>
-        </tr>
-        <tr>
-          <td className="text-start px-5"><a href="/docs/About" target="_blank">Learn about ODOP</a></td>
-        </tr>
-        <tr>
-          <td className="text-start px-5 pb-5"><hr /><br />Don't have an account? <Link to="/register">Sign up</Link></td>
-        </tr>
-      </Table>
-      </Col>
-      <Col lg="4" />
-      </Row>
+        <Row>
+          <Col lg="4" />
+          <Col lg="4">
+            <Table border="1" className="p-5">
+              <tbody>
+                <tr>
+                  <td className="text-center pt-3 px-5"><img src="favicon.ico" alt="Open Design Optimization Platform (ODOP) icon" /></td>
+                </tr>
+                <tr>
+                  <td className="text-center"><h3>Sign in to ODOP</h3></td>
+                </tr>
+                <tr>
+                  <td className="px-5 text-start">Username<br /><Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} /></td>
+                </tr>
+                <tr>
+                  <td className="px-5 text-start">Password<br /><Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} /></td>
+                </tr>
+                <tr>
+                  <td className="text-center"><Button variant="primary" type="submit">Sign In</Button></td>
+                </tr>
+                <tr>
+                  <td className="text-start px-5 pt-3"><Link to="/reset-password">Forgot Password?</Link></td>
+                </tr>
+                <tr>
+                  <td className="text-start px-5"><a href="/docs/About/userAccounts.html" target="_blank">About ODOP user accounts</a></td>
+                </tr>
+                <tr>
+                  <td className="text-start px-5"><a href="https://thegrumpys.github.io/odop/About/messageOfTheDay.html" target="_blank">ODOP Message Of The Day</a></td>
+                </tr>
+                <tr>
+                  <td className="text-start px-5"><a href="/docs/About" target="_blank">Learn about ODOP</a></td>
+                </tr>
+                <tr>
+                  <td className="text-start px-5 pb-3">Don't have an account? <Link to="/register">Sign up</Link></td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+          <Col lg="4" />
+        </Row>
       </Container>
     </form>
   );
