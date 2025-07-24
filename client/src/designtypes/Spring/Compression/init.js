91a176ea04cac30573ed8426fea6981a4cc3c647
Brian D. Watt <bwatt@1fifoto.com>
2025-07-24 06:31:35 -0500

    989 Improve handling of Material_Type when Prop_Calc_Method not 1

--- client/src/designtypes/Spring/Compression/init.js
+++ client/src/designtypes/Spring/Compression/init.js
@@ -144,6 +144,7 @@ export function init(store, p, x) {
         x[o.Inactive_Coils] = et_tab[j][eto.inactive_coils];
         x[o.Add_Coils_Solid] = et_tab[j][eto.add_coils_solid];
     }
+
     store.dispatch(changeSymbolHidden("Material_Type", false));
     store.dispatch(changeSymbolHidden("ASTM/Fed_Spec", false));
     store.dispatch(changeSymbolHidden("Process", false));
@@ -165,56 +166,58 @@ export function init(store, p, x) {
 //     console.log('case 2 - Specify Tensile, %_Tensile_Stat & %_Tensile_Endur');
     if (!matState || matState.oldvalue === undefined) {
         store.dispatch(saveSymbolValue('Material_Type'));
+        store.dispatch(changeSymbolValue('Material_Type', 1));
     }
-    store.dispatch(changeSymbolValue('Material_Type', 1));
     x[o.Material_Type] = 1;
+
     store.dispatch(changeSymbolHidden("Material_Type", false));
     store.dispatch(changeSymbolHidden("ASTM/Fed_Spec", true));
     store.dispatch(changeSymbolHidden("Process", true));
     store.dispatch(changeSymbolHidden("Life_Category", true));
-     store.dispatch(changeSymbolHidden("%_Tensile_Endur", false));
-     store.dispatch(changeSymbolHidden("%_Tensile_Stat", false));
+    store.dispatch(changeSymbolHidden("%_Tensile_Endur", false));
+    store.dispatch(changeSymbolHidden("%_Tensile_Stat", false));
 
-     store.dispatch(changeSymbolInput("Density", true));
-     store.dispatch(changeSymbolInput("Torsion_Modulus", true));
-     store.dispatch(changeSymbolInput("Hot_Factor_Kh", true));
-     store.dispatch(changeSymbolInput("Tensile", true));
-     store.dispatch(changeSymbolInput("%_Tensile_Endur", true));
-     store.dispatch(changeSymbolInput("%_Tensile_Stat", true));
-     store.dispatch(changeSymbolInput("Stress_Lim_Endur", false));
-     store.dispatch(changeSymbolInput("Stress_Lim_Stat", false));
-     break;
+    store.dispatch(changeSymbolInput("Density", true));
+    store.dispatch(changeSymbolInput("Torsion_Modulus", true));
+    store.dispatch(changeSymbolInput("Hot_Factor_Kh", true));
+    store.dispatch(changeSymbolInput("Tensile", true));
+    store.dispatch(changeSymbolInput("%_Tensile_Endur", true));
+    store.dispatch(changeSymbolInput("%_Tensile_Stat", true));
+    store.dispatch(changeSymbolInput("Stress_Lim_Endur", false));
+    store.dispatch(changeSymbolInput("Stress_Lim_Stat", false));
+    break;
 
  case 3:     // Prop_Calc_Method = 3 - Specify Stress_Lim_Stat & Stress_Lim_Endur
 //     console.log('case 3 - Specify Stress_Lim_Stat & Stress_Lim_Endur');
     if (!matState || matState.oldvalue === undefined) {
         store.dispatch(saveSymbolValue('Material_Type'));
+        store.dispatch(changeSymbolValue('Material_Type', 1));
     }
-    store.dispatch(changeSymbolValue('Material_Type', 1));
     x[o.Material_Type] = 1;
+
     store.dispatch(changeSymbolHidden("Material_Type", false));
     store.dispatch(changeSymbolHidden("ASTM/Fed_Spec", true));
     store.dispatch(changeSymbolHidden("Process", true));
     store.dispatch(changeSymbolHidden("Life_Category", true));
-     store.dispatch(changeSymbolHidden("%_Tensile_Endur", true));
-     store.dispatch(changeSymbolHidden("%_Tensile_Stat", true));
+    store.dispatch(changeSymbolHidden("%_Tensile_Endur", true));
+    store.dispatch(changeSymbolHidden("%_Tensile_Stat", true));
 
-     store.dispatch(changeSymbolInput("Density", true));
-     store.dispatch(changeSymbolInput("Torsion_Modulus", true));
-     store.dispatch(changeSymbolInput("Hot_Factor_Kh", true));
-     store.dispatch(changeSymbolInput("Tensile", true));
-     store.dispatch(changeSymbolInput("%_Tensile_Endur", false));
-     store.dispatch(changeSymbolInput("%_Tensile_Stat", false));
-     store.dispatch(changeSymbolInput("Stress_Lim_Stat", true));
-     store.dispatch(changeSymbolInput("Stress_Lim_Endur", true));
+    store.dispatch(changeSymbolInput("Density", true));
+    store.dispatch(changeSymbolInput("Torsion_Modulus", true));
+    store.dispatch(changeSymbolInput("Hot_Factor_Kh", true));
+    store.dispatch(changeSymbolInput("Tensile", true));
+    store.dispatch(changeSymbolInput("%_Tensile_Endur", false));
+    store.dispatch(changeSymbolInput("%_Tensile_Stat", false));
+    store.dispatch(changeSymbolInput("Stress_Lim_Stat", true));
+    store.dispatch(changeSymbolInput("Stress_Lim_Endur", true));
  }
 
  if (et_tab[j][eto.end_type] === "User_Specified") {
-     store.dispatch(changeSymbolInput("Inactive_Coils", true));
-     store.dispatch(changeSymbolInput("Add_Coils@Solid", true));
+    store.dispatch(changeSymbolInput("Inactive_Coils", true));
+    store.dispatch(changeSymbolInput("Add_Coils@Solid", true));
  } else {
-     store.dispatch(changeSymbolInput("Inactive_Coils", false));
-     store.dispatch(changeSymbolInput("Add_Coils@Solid", false));
+    store.dispatch(changeSymbolInput("Inactive_Coils", false));
+    store.dispatch(changeSymbolInput("Add_Coils@Solid", false));
  }
 //    console.log('init p=',p,' x=',x);
     return x;
