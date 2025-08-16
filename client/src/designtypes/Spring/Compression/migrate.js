87679a61a6e55d18cb46c65924dee5d82452bdae
Brian D. Watt <bwatt@1fifoto.com>
2025-08-16 12:42:42 -0500

    1043 Auto Search not working for torsion springs

--- client/src/designtypes/Spring/Compression/migrate.js
+++ client/src/designtypes/Spring/Compression/migrate.js
@@ -388,13 +388,14 @@ export function migrate(design) {
         }
         design.system_controls.enable_auto_search = 1; // Default to auto search on
         migrated_design.version = '13';
+    case '13':
 
-        //============BLOCK OF CODE TO REPLICATE============
-        // case 'N':
-        //     // console.log('Convert from N to N+1');
-        //     Write the migration code here
-        //     migrated_design.version = 'N+1';
-        //==================================================
+    //============BLOCK OF CODE TO REPLICATE============
+    //     console.log('Convert from N to N+1');
+    //     Write the migration code here
+    //     migrated_design.version = 'N+1';
+    // case 'N+1':
+    //==================================================
 
         break; // Do not copy this break
     default: // Unknown
--- client/src/designtypes/Spring/Extension/migrate.js
+++ client/src/designtypes/Spring/Extension/migrate.js
@@ -342,13 +342,14 @@ export function migrate(design) {
         }
         design.system_controls.enable_auto_search = 1; // Default to auto search on
         migrated_design.version = '10';
+    case '10':
 
-        //============BLOCK OF CODE TO REPLICATE============
-        // case 'N':
-        //     // console.log('Convert from N to N+1');
-        //     Write the migration code here
-        //     migrated_design.version = 'N+1';
-        //==================================================
+    //============BLOCK OF CODE TO REPLICATE============
+    //     console.log('Convert from N to N+1');
+    //     Write the migration code here
+    //     migrated_design.version = 'N+1';
+    // case 'N+1':
+    //==================================================
 
         break; // Do not copy this break
     default: // Unknown
