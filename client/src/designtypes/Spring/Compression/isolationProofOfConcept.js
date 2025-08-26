ed904f0bbc4d867aee7513399c1eacdf08512c5a
Mike <Mike@Win11VM3.jardin.local>
2025-08-26 16:01:23 -0700

    1045 Provide (optional) isolation for script execution
    Proof of concept extended

--- client/src/designtypes/Spring/Compression/isolationProofOfConcept.js
+++ client/src/designtypes/Spring/Compression/isolationProofOfConcept.js
@@ -5,16 +5,44 @@ export const execute = {
             text: (
                 <>
                     <p>
-                    Proof of concept to demonstrate running an (unmodified) ODOP execute file in isolation:
+                    <b>Proof of concept to demonstrate running an (unmodified) ODOP Execute file in isolation:</b>
                     </p>
 
                     <p>
-                    <a href="https://odop.springdesignsoftware.org/?type=Spring%2FCompression&name=Startup_Metric&view=Calculator&execute=tutor4" target="_blank" rel="noopener noreferrer">Run isolated tutor4</a> 
+                    <table>
+                    <thead>
+                    <tr>
+                    <th>Script Name</th><th>&nbsp;</th><th>Description</th><th>&nbsp;</th><th>Options Used</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                    <td><a href="https://odop.springdesignsoftware.org/?type=Spring%2FCompression&name=Startup_Metric&view=Calculator&execute=tutor4" target="_blank" rel="noopener noreferrer">tutor4</a></td>
+                    <td>&nbsp;</td>
+                    <td>Save, print description of a finished design</td>
+                    <td>&nbsp;</td>
+                    <td>Metric compression spring, Calculator View.</td>
+                    </tr>
+                    <tr>
+                    <td><a href="https://odop.springdesignsoftware.org/?type=Spring%2FExtension&name=Startup&view=Advanced&execute=demo6" target="_blank" rel="noopener noreferrer">demo6</a></td>
+                    <td>&nbsp;</td>
+                    <td>Spring Mfgrs. Inst. Handbook - extension spring example&nbsp;1</td>
+                    <td>&nbsp;</td>
+                    <td>US units extension spring, Advanced View</td>
+                    </tr>
+                    <tr>
+                    <td><a href="https://odop.springdesignsoftware.org/?type=Spring%2FCompression&name=Startup&view=Advanced&execute=demoNewDesign" target="_blank" rel="noopener noreferrer">demo&nbsp;NewDesign</a></td>
+                    <td>&nbsp;</td>
+                    <td>Design a compression spring from original specifications</td>
+                    <td>&nbsp;</td>
+                    <td>US units compression spring, Advanced View</td>
+                    </tr>
+                    </tbody>
+                    </table>
                     </p>
 
                     <p>
                     <b>Notes:</b> <br/>
-                    - Metric compression spring, Calculator view. <br/> 
                     - When finished, Close the script. Close the new tab.  You should return here. Push the "Close" button above. This session continues without modification.
                     </p>
                 </>
