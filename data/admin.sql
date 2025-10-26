89791e0a8cd08621b01e79f5a98e0ca8256f9738
Brian D. Watt <bwatt@1fifoto.com>
2025-10-26 12:17:04 -0500

    1066 Create SQL file to inject an admin into database

---
+++ data/admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (`email`, `password`, `first_name`, `last_name`, `role`, `token`, `status`, `created_at`, `last_login_at`) VALUES
+('admin@SpringDesignSoftware.org', '$2b$12$4Ez2A6DXWkTpKb4thNGrjOy54k92P6M3XvigCrgIQMor9an43fyx6', 'Admin', 'Admin', 'admin', '2bt5kJ3S434VRl9ofg6pb3sCl4urXw26', 'active', '2025-10-26 17:13:23', NULL);
